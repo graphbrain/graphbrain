18f9fcf6aa6fb5ab65d5559140950f4076e4fce3
Telmo Menezes <telmo@telmomenezes.com>
2011-08-13 23:51:58 +0100

    first commit

---
+++ graphbrain.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    nodes_json = '[{"id":0, "parent":"", "text":"Hello World!"}]'
+    return render_template('node.html', nodes_json=nodes_json, links_json='[]')
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
