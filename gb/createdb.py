67b87c919659e8100e786471e932d6af0b9092b8
Telmo Menezes <telmo@telmomenezes.net>
2011-08-14 02:07:24 +0100

    working on db schema

--- gb/createdb.py
+++ gb/createdb.py
@@ -1,27 +1,17 @@
 #!/usr/bin/env python
 
 
-import MySQLdb as mdb
-import sys
+import db
 
 
-try:
-    conn = mdb.connect('localhost', 'root', '', 'graphbrain');
+if __name__ == '__main__':
 
-    cursor = conn.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS \
-        Writers(Id INT PRIMARY KEY AUTO_INCREMENT, Name VARCHAR(25))")
-    cursor.execute("INSERT INTO Writers(Name) VALUES('Jack London')")
-    cursor.execute("INSERT INTO Writers(Name) VALUES('Honore de Balzac')")
-    cursor.execute("INSERT INTO Writers(Name) VALUES('Lion Feuchtwanger')")
-    cursor.execute("INSERT INTO Writers(Name) VALUES('Emile Zola')")
-    cursor.execute("INSERT INTO Writers(Name) VALUES('Truman Capote')")
+    # user table
+    db.execute("CREATE TABLE user(id INT PRIMARY KEY AUTO_INCREMENT)")
+    db.execute("ALTER TABLE user ADD COLUMN name VARCHAR(50)")
+    
+    # node table
 
-    conn.commit()
+    # link table
 
-    cursor.close()
-    conn.close()
-
-except mdb.Error, e:
-    print "Error %d: %s" % (e.args[0],e.args[1])
-    #sys.exit(1)
+    db.connection().close()
