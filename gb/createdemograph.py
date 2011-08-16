a1ce70cdb7d29452d22bebdcb714eb8e8ff133fe
Telmo Menezes <telmo@telmomenezes.com>
2011-08-16 19:55:31 +0100

    improving createdemograph

--- gb/createdemograph.py
+++ gb/createdemograph.py
@@ -15,3 +15,5 @@ if __name__ == '__main__':
     g = Graph().create('Demo', u)
     root = Node().create('Graphbrain', g)
     g.set_root(root)
+    n = Node().create('Computer Program', g)
+    l = Link().create(root, n, 'is')
