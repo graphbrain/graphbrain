5ed47b51906f76824d31a1101b41e205ec73a6e5
Telmo Menezes <telmo@telmomenezes.com>
2011-08-16 02:18:30 +0100

    started to define User class

--- gb/user.py
+++ gb/user.py
@@ -1,6 +1,27 @@
+import MySQLdb as mdb
+import bcrypt
+
 import db
 
 
 class User:
     def __init__(self):
         pass
+
+    def create(email, name, password, role):
+        self.email = email
+        self.name = name
+        self.role = role
+        self.pwdhash = bcrypt.hashpw(password, bcrypt.gensalt(12))
+
+        cur = db.cursor()
+
+        cur.execute("INSERT INTO user (name, email, pwdhash, role) VALUES (%s, %s, %s, %d)",
+                        (name, email, self.pwdhash, role))
+
+        self.id = db.connection().insert_id
+
+        db.connection().commit()
+        cur.close()
+
+        return self.id
