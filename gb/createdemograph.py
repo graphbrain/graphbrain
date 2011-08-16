b3948b71315eed2bacb8f9d7d505790ca095c26b
Telmo Menezes <telmo@telmomenezes.net>
2011-08-16 16:11:01 +0100

    createdemograph script

---
+++ gb/createdemograph.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+
+import sys
+
+from user import User
+from graph import Graph
+from node import Node
+from link import Link
+
+
+if __name__ == '__main__':
+    email = sys.argv[1] 
+    u = User().get_by_email(email)
+    g = Graph().create('Demo', u)
+    root = Node().create('Graphbrain', g)
