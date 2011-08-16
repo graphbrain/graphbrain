26c24d302878b61887d589adfe9f981bf06faf8b
Telmo Menezes <telmo@telmomenezes.com>
2011-08-16 11:45:25 +0100

    user get_by_email

--- gb/user.py
+++ gb/user.py
@@ -25,3 +25,15 @@ class User:
         cur.close()
 
         return self.id
+
+    def get_by_email(email):
+        cur = db.cursor
+        cur.excute("SELECT id, name, pwdhash, role, creation_ts, session, session_ts FROM user WHERE email=%s", (email,))
+        row = cur.fetchone()
+        self.id = row[0]
+        self.name = row[1]
+        self.pwdhash = row[2]
+        self.role = row[3]
+        self.creation_ts = row[4]
+        self.session = row[5]
+        self.session_ts = row[6]
