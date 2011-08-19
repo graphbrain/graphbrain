95182c0239880b63c414e89320420c465b9c93f5
Telmo Menezes <telmo@telmomenezes.net>
2011-08-19 14:24:08 +0100

    woking on login/auth system

--- graphbrain.py
+++ graphbrain.py
@@ -4,6 +4,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import redirect
 
 from gb.user import User
 from gb.graph import Graph
@@ -13,6 +14,29 @@ from gb.node import Node
 app = Flask(__name__)
 
 
+def reirect2login():
+    redirect_to_index = redirect('/login')
+    response = app.make_response(redirect_to_index)   
+    return response
+
+
+def requirelogin():
+    def decorator(f):
+        user_id = request.cookies.get('user_id')
+        session = request.cookies.get('session')
+        if user_id is None:
+            return redirect2login
+        if session is None:
+            return redirect2login
+        u = User().get_by_id(user_id)
+        if u.check_session(session):
+            return f
+        else
+            return redirect2login
+    return decorator
+
+
+@requirelogin
 @app.route("/")
 def main():
     # TODO: these values are just temporary
@@ -36,9 +60,14 @@ def login():
 
         u = User().get_by_email(email)
         if u.check_password(password):
-            pass
+            session = u.create_session()
+            redirect_to_index = redirect('/')
+            response = app.make_response(redirect_to_index)  
+            response.set_cookie('user_id',value=u.id)
+            response.set_cookie('session',value=session)
+            return response
         else:
-            pass
+            return render_template('login.html', message='Sorry, wrong username and/or password.')
         return email
 
 
