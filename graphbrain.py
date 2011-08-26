9388ed3dc2d913f2642796388d59b08f7bc2e51f
Telmo Menezes <telmo@telmomenezes.net>
2011-08-26 09:58:42 +0100

    refactored requirelogin decorator to curuser function

--- graphbrain.py
+++ graphbrain.py
@@ -21,26 +21,26 @@ def redirect2login():
     return response
 
 
-def requirelogin(f):
-    def fun():
-        user_id = request.cookies.get('user_id')
-        session = request.cookies.get('session')
-        if user_id is None:
-            return redirect2login()
-        if session is None:
-            return redirect2login()
-        u = User().get_by_id(user_id)
-        if u.check_session(session):
-            return f(u)
-        else:
-            return redirect2login()
-    fun.__name__ = f.__name__
-    return fun
+def curuser():
+    user_id = request.cookies.get('user_id')
+    session = request.cookies.get('session')
+    if user_id is None:
+        return None
+    if session is None:
+        return None
+    u = User().get_by_id(user_id)
+    if u.check_session(session):
+        return u
+    else:
+        return None
 
 
 @app.route("/")
-@requirelogin
-def main(u):
+def main():
+    u = curuser()
+    if u is None:
+        return redirect2login()
+
     # TODO: these values are just temporary
     #u = User().get_by_email('telmo@telmomenezes.com')
     g = Graph().get_by_owner_and_name(u, 'Demo')
@@ -54,6 +54,10 @@ def main(u):
 
 @app.route('/node/<node_id>')
 def node(node_id):
+    u = curuser()
+    if u is None:
+        return redirect2login()
+    
     n = Node().get_by_id(int(node_id))
 
     nodes_json, links_json = n.neighbours_json()
@@ -84,8 +88,11 @@ def login():
 
 
 @app.route("/logout")
-@requirelogin
-def logout(u):
+def logout():
+    u = curuser()
+    if u is None:
+        return redirect2login()
+    
     redirect_to_index = redirect('/')
     response = app.make_response(redirect_to_index)  
     response.delete_cookie('user_id')
@@ -94,8 +101,11 @@ def logout(u):
 
 
 @app.route("/input", methods=['POST',])
-@requirelogin
-def input(u):
+def input():
+    u = curuser()
+    if u is None:
+        return redirect2login()
+    
     input_str = request.form['input']
     graph_id = request.form['graph_id']
 
