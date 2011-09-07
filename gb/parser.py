bc7b5b96b763e6e0df187a5f865e01da398e4b26
Telmo Menezes <telmo@telmomenezes.com>
2011-09-07 12:35:25 +0200

    parser improved, and supporting quotes

--- gb/parser.py
+++ gb/parser.py
@@ -2,49 +2,47 @@ import nltk
 import sys
 
 
-def parse_orig(tokens, start, end):
+class ParseError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return 'Parse error: %s' % self.msg
+
+
+def parse_argument(tokens):
     phrase = ''
-    pos = start
     first = True
-    while pos < end:
+    for t in tokens:
         if first:
             first = False
         else:
             phrase += ' '
-        phrase += tokens[pos][0]
-        pos += 1
+        phrase += t[0]
 
     return phrase
 
 
-def parse_rel(tokens, start, end):
+def parse_predicate(tokens):
     phrase = ''
-    pos = start
     first = True
-    while pos < end:
+    for t in tokens:
         if first:
             first = False
         else:
             phrase += ' '
-        phrase += tokens[pos][0]
-        pos += 1
+        phrase += t[0]
 
     return phrase
 
 
-def parse_targ(tokens, start, end):
-    phrase = ''
-    pos = start
-    first = True
-    while pos < end:
-        if first:
-            first = False
-        else:
-            phrase += ' '
-        phrase += tokens[pos][0]
-        pos += 1
+def remove_ponctuation(tagged_tokens):
+    res = []
+    for t in tagged_tokens:
+        if t[0] not in '.,;!':
+            res.append(t)
 
-    return phrase
+    return res
 
 
 def parse(sentence):
@@ -53,24 +51,61 @@ def parse(sentence):
 
     print tagged_tokens
 
-    rel_index = -1
-    targ_index = -1
+    tagged_tokens = remove_ponctuation(tagged_tokens)
+
+    if len(tagged_tokens) == 0:
+        raise ParseError('Empty sentence.')
+
+    orig_tokens = []
+    rel_tokens = []
+    targ_tokens = []
 
     pos = 0
 
-    while (pos < len(tagged_tokens)) and (rel_index <= 0):
-        if tagged_tokens[pos][1][:2] == 'VB':
-            rel_index = pos
+    # parsing origin
+    quoted = False
+    if tagged_tokens[0][0] == '"':
+        quoted = True
+        pos += 1
+    while pos < len(tagged_tokens):
+        if quoted:
+            if tagged_tokens[pos][0] == '"':
+                pos += 1
+                break
+        else:
+            if tagged_tokens[pos][1][:2] == 'VB':
+                break
+        
+        orig_tokens.append(tagged_tokens[pos])
         pos += 1
 
-    while (pos < len(tagged_tokens)) and (targ_index <= 0):
+    # parsing relationship
+    quoted = False
+    while pos < len(tagged_tokens):
+        if tagged_tokens[pos][0] == '"':
+            pos += 1
+            quoted = True
+            break
         if tagged_tokens[pos][1][:2] == 'NN':
-            targ_index = pos
+            break
+        rel_tokens.append(tagged_tokens[pos])
         pos += 1
 
-    orig = parse_orig(tagged_tokens, 0, rel_index)
-    rel = parse_rel(tagged_tokens, rel_index, targ_index)
-    targ = parse_targ(tagged_tokens, targ_index, len(tagged_tokens))
+    # parsing target
+    while pos < len(tagged_tokens):
+        if quoted:
+            if tagged_tokens[pos][0] == '"':
+                break
+        
+        targ_tokens.append(tagged_tokens[pos])
+        pos += 1
+
+    if (len(orig_tokens) == 0) or (len(rel_tokens) == 0) or (len(targ_tokens) == 0):
+        raise ParseError('Could not understand sentence.')
+
+    orig = parse_argument(orig_tokens)
+    rel = parse_predicate(rel_tokens)
+    targ = parse_argument(targ_tokens)
 
     return orig, rel, targ
 
@@ -79,5 +114,9 @@ if __name__ == '__main__':
     while True:
         print '?'
         line = sys.stdin.readline()
-        orig, rel, targ = parse(line)
-        print '%s [%s] %s' % (orig, rel, targ)
+        try:
+            orig, rel, targ = parse(line)
+        except ParseError, p:
+            print p
+            continue
+        print "'%s' [%s] '%s'" % (orig, rel, targ)
