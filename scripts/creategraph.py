efd7a8bc850abb73be679af7e053f2d478c0b32e
Telmo Menezes <telmo@telmomenezes.net>
2011-09-09 11:02:34 +0200

    created creategraph.py

---
+++ scripts/creategraph.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+
+import sys
+
+from user import User
+from graph import Graph
+from node import Node
+from link import Link
+
+
+if __name__ == '__main__':
+    email = sys.argv[1] 
+    graph_name = sys.argv[2]
+    root_node = sys.argv[3]
+
+    u = User().get_by_email(email)
+    g = Graph().create(graph_name, u)
+    root = Node().create(root_node, g)
+    g.set_root(root)
