8cdf809c68466114bf62949a6a3fabe66ed8e5cc
Telmo Menezes <telmo@telmomenezes.com>
2011-09-13 15:24:09 +0200

    link directed field

--- gb/link.py
+++ gb/link.py
@@ -8,15 +8,16 @@ class Link(DbObj):
     def __init__(self):
         DbObj.__init__(self)
 
-    def create(self, orig, targ, rel, rel_raw, sentence):
+    def create(self, orig, targ, rel, rel_raw, sentence, directed=1):
         self.orig = orig
         self.targ = targ
         self.relation = rel
         self.relation_raw = rel_raw
         self.sentence = sentence
+        self.directed = directed
 
-        self.cur.execute("INSERT INTO link (orig, targ, relation, relation_raw, sentence) VALUES (%s, %s, %s, %s, %s)",
-                         (orig.id, targ.id, self.relation, self.relation_raw, self.sentence))
+        self.cur.execute("INSERT INTO link (orig, targ, relation, relation_raw, sentence, directed) VALUES (%s, %s, %s, %s, %s, %s)",
+                         (orig.id, targ.id, self.relation, self.relation_raw, self.sentence, self.directed))
         self.id = self.insert_id()
         self.commit()
 
@@ -24,11 +25,12 @@ class Link(DbObj):
 
     def get_by_id(self, link_id):
         self.id = link_id
-        self.cur.execute("SELECT orig, targ, relation FROM link WHERE id=%s", (link_id,))
+        self.cur.execute("SELECT orig, targ, relation, directed FROM link WHERE id=%s", (link_id,))
         row = self.cur.fetchone()
         self.orig = row[0]
         self.targ = row[1]
         self.relation = row[2]
+        self.directed = row[3]
 
         return self
         
