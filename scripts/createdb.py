148619b373a32b1f39f73362b18dd0258aa814e8
Telmo Menezes <telmo@telmomenezes.com>
2011-09-13 12:34:08 +0200

    added timestamps to all tables

--- scripts/createdb.py
+++ scripts/createdb.py
@@ -7,9 +7,9 @@ import gb.db as db
 if __name__ == '__main__':
     # user table
     db.execute("CREATE TABLE user(id INT PRIMARY KEY AUTO_INCREMENT)")
+    db.execute("ALTER TABLE user ADD COLUMN creation_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     db.execute("ALTER TABLE user ADD COLUMN name VARCHAR(100)")
     db.execute("ALTER TABLE user ADD COLUMN email VARCHAR(100)")
-    db.execute("ALTER TABLE user ADD COLUMN creation_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     db.execute("ALTER TABLE user ADD COLUMN pwdhash VARCHAR(60)")
     db.execute("ALTER TABLE user ADD COLUMN role INT")
     # 0: user; 1: admin
@@ -18,12 +18,14 @@ if __name__ == '__main__':
 
     # graph table
     db.execute("CREATE TABLE graph(id INT PRIMARY KEY AUTO_INCREMENT)")
+    db.execute("ALTER TABLE graph ADD COLUMN creation_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     db.execute("ALTER TABLE graph ADD COLUMN name VARCHAR(100)")
     db.execute("ALTER TABLE graph ADD COLUMN owner INT")
     db.execute("ALTER TABLE graph ADD COLUMN root INT")
 
     # graph_user table
     db.execute("CREATE TABLE graph_user(id INT PRIMARY KEY AUTO_INCREMENT)")
+    db.execute("ALTER TABLE graph_user ADD COLUMN creation_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     db.execute("ALTER TABLE graph_user ADD COLUMN graph INT")
     db.execute("ALTER TABLE graph_user ADD COLUMN user INT")
     db.execute("ALTER TABLE graph_user ADD COLUMN perm INT")
@@ -31,11 +33,13 @@ if __name__ == '__main__':
     
     # node table
     db.execute("CREATE TABLE node(id INT PRIMARY KEY AUTO_INCREMENT)")
+    db.execute("ALTER TABLE node ADD COLUMN creation_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     db.execute("ALTER TABLE node ADD COLUMN data VARCHAR(250)")
     db.execute("ALTER TABLE node ADD COLUMN graph INT")
 
     # link table
     db.execute("CREATE TABLE link(id INT PRIMARY KEY AUTO_INCREMENT)")
+    db.execute("ALTER TABLE link ADD COLUMN creation_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     db.execute("ALTER TABLE link ADD COLUMN orig INT")
     db.execute("ALTER TABLE link ADD COLUMN targ INT")
     db.execute("ALTER TABLE link ADD COLUMN relation VARCHAR(100)")
