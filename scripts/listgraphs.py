3a74d2d955374bd2b5e978267a0a88005242d1af
Telmo Menezes <telmo@telmomenezes.net>
2011-10-27 11:33:59 +0200

    fixing listgraphs.py
    Former-commit-id: d190eb06bc7b6f08613921f806b808877535b8b8

--- scripts/listgraphs.py
+++ scripts/listgraphs.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python
 
 
-import gb.db as db
+import gb.mongodb
 from gb.user import User
 
 
 if __name__ == '__main__':
-    cur = db.cursor()
+    db = gb.mongodb.getdb()
 
-    cur.execute("SELECT id, name, owner FROM graph")
-    for row in cur:
-        owner = User().get_by_id(row[2]).email
-        print row[0], row[1], owner
-
-    cur.close()
+    cur = db.nodes.find({'type': 'graph'})
+    for g in cur:
+        owner = User().get_by_id(g['owner']).d['email']
+        print g['_id'], g['label'], owner
