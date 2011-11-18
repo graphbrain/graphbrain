184f406f7ee4c2605e3c2cdc4177416c892e5198
Telmo Menezes <telmo@telmomenezes.net>
2011-11-18 16:24:42 +0100

    improving cinema crawler's synch2gb
    Former-commit-id: b190406c3b9c2386382a28565613cd33c3224b7c

--- crawlers/cinema/synch2gb.py
+++ crawlers/cinema/synch2gb.py
@@ -18,15 +18,15 @@ def get_person_node(db, graph, person_id):
         eid = 'cinema:person:%s' % person['name']
     person_node = Node().create_or_get_by_eid(label=person['name'], graph=graph, eid=eid, crawler='cinema')
     if 'image' in person.keys():
-        image_node = Node().create_or_get_by_eid(label=person['image'], graph=graph, eid=person['image'], crawler='cinema')
-        graph.add_link(person_node, image_node, 'photo', 'photo')
+        image_node = Node().create_or_get_by_eid(label=person['image'], graph=graph, eid=person['image'], crawler='cinema', node_type='image')
+        graph.add_link(image_node, person_node, 'photo of', 'photo of')
     return person_node
 
 
 def link_film_to_people(db, graph, film_node, people, rel):
     for person in people:
         person_node = get_person_node(db, graph, person)
-        graph.add_link(film_node, person_node, rel, rel)    
+        graph.add_link(person_node, film_node, rel, rel)    
 
 
 def process_film(db, graph, film):
@@ -50,15 +50,15 @@ def process_film(db, graph, film):
     if 'musicians' in film.keys():
         musicians = film['musicians']
 
-    link_film_to_people(db, graph, film_node, actors, 'actor')
-    link_film_to_people(db, graph, film_node, producers, 'producer')
-    link_film_to_people(db, graph, film_node, writers, 'writer')
-    link_film_to_people(db, graph, film_node, directors, 'director')
-    link_film_to_people(db, graph, film_node, musicians, 'musician')
+    link_film_to_people(db, graph, film_node, actors, 'actor in')
+    link_film_to_people(db, graph, film_node, producers, 'producer of')
+    link_film_to_people(db, graph, film_node, writers, 'writer of')
+    link_film_to_people(db, graph, film_node, directors, 'director of')
+    link_film_to_people(db, graph, film_node, musicians, 'musician for')
 
     if 'poster' in film.keys():
-        poster_node = Node().create_or_get_by_eid(label=film['poster'], graph=graph, eid=film['poster'], crawler='cinema')
-        graph.add_link(film_node, poster_node, 'poster', 'poster')
+        poster_node = Node().create_or_get_by_eid(label=film['poster'], graph=graph, eid=film['poster'], crawler='cinema', node_type='image')
+        graph.add_link(poster_node, film_node, 'poster of', 'poster of')
 
 
 def synch(graph_owner, graph_name):
@@ -72,14 +72,14 @@ def synch(graph_owner, graph_name):
     mfilms = db.films
     total = mfilms.count()
     count = 1
-    q = mfilms.find()
+    q = mfilms.find(timeout=False)
     for film in q:
         print 'Synching film: %s [%d/%d] (%f%%)' % (film['title'], count, total, (float(count) / float(total)) * 100)
         process_film(db, graph, film)
         count += 1
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     graph_owner = 'gb@graphbrain.com'
     graph_name = 'Main'
     synch(graph_owner, graph_name)
