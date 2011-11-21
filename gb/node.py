ee95986bd8f0a196452578dc0531e0ee40a9c56b
Telmo Menezes <telmo@telmomenezes.net>
2011-11-21 12:43:14 +0100

    working on supernodes
    Former-commit-id: 5d725fb0c692c0e4114e29d7d3a535eef6955e32

--- gb/node.py
+++ gb/node.py
@@ -36,19 +36,24 @@ class Node(DbObj):
         self.d = self.db.nodes.find_one({'label': label, 'graph': graph_id})
         return self
 
-    def _neighbors(self, nodes, nodeids, depth=0):
+    def _neighbors(self, nodes, nodeids, depth=0, count=0):
+        maxnodes = 25
+
         if self.d is None:
             return
 
         next_nodes = []
 
-        if (depth < 1):
+        if (depth < 2):
             if 'targs' in self.d:
                 for n in self.d['targs'].keys():
                     n = str(n)
                     if n not in nodeids:
                         nnode = Node().get_by_id(n)
                         if not nnode.d is None:
+                            count += 1
+                            if count > maxnodes:
+                                return
                             nnode.parent = self.d['_id']
                             nodes.append(nnode)
                             nodeids.append(n)
@@ -60,29 +65,112 @@ class Node(DbObj):
                     if n not in nodeids:
                         nnode = Node().get_by_id(n)
                         if not nnode.d is None:
+                            count += 1
+                            if count > maxnodes:
+                                return
                             nnode.parent = self.d['_id']
                             nodes.append(nnode)
                             nodeids.append(n)
                             next_nodes.append(nnode)
 
             for n in next_nodes:
-                n._neighbors(nodes, nodeids, depth + 1) 
-
+                n._neighbors(nodes, nodeids, depth + 1, count) 
 
     def _internal_links(self, nodes, nodeids):
-        ilinks = {}
-        for node in nodes:
-            ilinks[node.d['_id']] = []
-    
+        ilinks = []
         for orig in nodes:
             if 'targs' in orig.d:
                 targs = orig.d['targs']
                 for targ_id, targ_list in targs.items():
                     if targ_id in nodeids:
                         for targ in targ_list:
-                            ilinks[orig.d['_id']].append({'_id': targ_id, 'relation': targ['relation'], 'directed': targ['directed']})
+                            ilinks.append({'orig': orig.d['_id'], 'targ': targ_id, 'relation': targ['relation'], 'directed': targ['directed']})
 
         return ilinks
+
+    def _reldict(self, links):
+        rd = {}
+        for l in links:
+            orig_id = str(l['orig'])
+            targ_id = str(l['targ'])
+            rel = l['relation']
+            dkey = (True, orig_id, rel)
+            rkey = (False, rel, targ_id)
+            if dkey in rd:
+                rd[dkey].append(targ_id)
+            else:
+                rd[dkey] = [targ_id,]
+            if rkey in rd:
+                rd[rkey].append(orig_id)
+            else:
+                rd[rkey] = [orig_id,]
+        return rd
+
+    def _maxrel(self, rd):
+        maxlen = -1
+        maxkey = None
+        for key in rd.keys():
+            l = len(rd[key])
+            if (maxlen < 0) or (l > maxlen):
+                maxlen = l
+                maxkey = key
+        return maxkey
+
+    def _delrel(self, rd, key):
+        nodes = rd[key]
+        del rd[key]
+
+        for k in rd.keys():
+            newrelnodes = []
+            relnodes = rd[k]
+            for n in relnodes:
+                if not n in nodes:
+                    newrelnodes.append(n)
+            if len(newrelnodes) > 0:
+                rd[k] = newrelnodes
+            else:
+                del rd[k]
+
+    def _supernodes(self, links):
+        snodes = {}
+        newlinks = links
+        rd = self._reldict(links)
+        key = self._maxrel(rd)
+        super_id = 0
+        while not key is None:
+            nodes = rd[key]
+            direction = key[0]
+            orig_id = -1
+            targ_id = -1
+            rel = ''
+            if direction:
+                orig_id = key[1]
+                rel = key[2]
+            else:
+                rel = key[1]
+                targ_id = key[2]
+            
+            superkey = 'sn%d' % super_id
+            snodes[superkey] = nodes
+
+            # adjust links
+            auxlinks = []
+            if direction:
+                for l in links:
+                    if (l['orig'] != orig_id) or (l['relation'] != rel):
+                        auxlinks.append(l)
+                auxlinks.append({'orig': orig_id, 'targ': superkey, 'relation': rel, 'directed': '1'})
+            else:
+                for l in links:
+                    if (l['targ'] != targ_id) or (l['relation'] != rel):
+                        auxlinks.append(l)
+                auxlinks.append({'orig': superkey, 'targ': targ_id, 'relation': rel, 'directed': '1'})
+            newlinks = auxlinks
+
+            self._delrel(rd, key)
+            key = self._maxrel(rd)
+
+        return snodes, newlinks
 
     def neighbours_json(self):
         self.parent = ''
@@ -90,6 +178,10 @@ class Node(DbObj):
         nodeids = [str(self.d['_id']),]
         self._neighbors(nodes, nodeids)
         links = self._internal_links(nodes, nodeids)
+
+        #snodes, newlinks = self._supernodes(links)
+        #print snodes
+        #print newlinks
 
         nodes_json = '['
         for node in nodes:
@@ -99,11 +191,10 @@ class Node(DbObj):
 
         link_id = 0
         links_json = '['
-        for orig_id in links.keys():
-            for targ in links[orig_id]:
-                links_json = '%s{"id":"%d", "orig": "%s", "targ":"%s", "type":"%s", "dir":"%s"},' % (
-                            links_json, link_id, str(orig_id), str(targ['_id']), targ['relation'], targ['directed'])
-                link_id += 1
+        for l in links:
+            links_json = '%s{"id":"%d", "orig": "%s", "targ":"%s", "type":"%s", "dir":"%s"},' % (
+                            links_json, link_id, str(l['orig']), str(l['targ']), l['relation'], l['directed'])
+            link_id += 1
         links_json = '%s]' % links_json
 
         return nodes_json, links_json
