b37794f9bcfe2c1f1c0415f6a7b5428c799e8123
Telmo Menezes <telmo@telmomenezes.com>
2012-01-18 12:18:14 +0000

    started work on Link place() function
    Former-commit-id: 53951a340b1307e9b730a7d991a8d2cc2c40c158

--- js/link.js
+++ js/link.js
@@ -205,3 +205,13 @@ Link.prototype.intersectsLink = function(link2) {
 Link.prototype.intersectsSNode = function(snode) {
     return lineRectOverlap(this.x0, this.y0, this.x1, this.y1, snode.rect);
 }
+
+Link.prototype.place = function() {
+    var line = document.createElement('div');
+    
+    snode.setAttribute('class', 'linkLine');
+    snode.setAttribute('id', 'linkline' + this.id);
+    
+    var nodesDiv = document.getElementById("nodesDiv");
+    nodesDiv.appendChild(snode);
+}
