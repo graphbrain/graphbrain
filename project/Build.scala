d68251d4addcd5202b49a33a5d1421dc1d930727
Telmo Menezes <telmo@telmomenezes.com>
2012-02-01 17:44:59 +0100

    organizing web project
    Former-commit-id: 27bee579652503bc311526103f6387b84903bb7e

--- project/Build.scala
+++ project/Build.scala
@@ -1,13 +1,15 @@
 import sbt._
 import Keys._
 
+
 object GraphbrainBuild extends Build {
 	lazy val root = Project(id = "gb",
                             base = file(".")) aggregate(inference, web)
 
-    lazy val inference = Project(id = "gb-inference",
+    lazy val inference = Project(id = "inference",
                            base = file("inference"))
 
-    lazy val web = Project(id = "gb-web",
+    lazy val web = Project(id = "web",
                            base = file("web")) dependsOn(RootProject(uri("git://github.com/unfiltered/unfiltered-scalate.git")))
 }
+
