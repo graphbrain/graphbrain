38659b148b6d1cb1c9e9e1bbe625a718206af039
Telmo Menezes <telmo@telmomenezes.com>
2012-02-05 19:11:37 +0100

    created core project
    Former-commit-id: e3d279bec7a24d92a8afd6d51ed0693015de67ae

---
+++ core/build.sbt
@@ -0,0 +1,5 @@
+name := "gb-core"
+
+version := "0.1.0-SNAPSHOT"
+
+scalaVersion := "2.9.1"
--- project/Build.scala
+++ project/Build.scala
@@ -1,25 +1,17 @@
 import sbt._
 import Keys._
-import PlayProject._
 
 
 object GraphbrainBuild extends Build {
-	lazy val root = Project(id = "gb",
-                            base = file(".")) aggregate(inference, webapp)
+	lazy val core = Project(id = "core",
+                           base = file("core"))
 
-    lazy val inference = Project(id = "inference",
+  lazy val inference = Project(id = "inference",
                            base = file("inference"))
 
-    // Play web app project
-    val appName         = "GraphBrain Web App"
-    val appVersion      = "1.0"
+  lazy val root = Project(id = "gb",
+                            base = file(".")) aggregate(core, inference)
 
-    val appDependencies = Seq(
-      // Add your project dependencies here,
-    )
-
-    val webapp = PlayProject(appName, appVersion, appDependencies).settings(defaultScalaSettings:_*).settings(
-      // Add your own project settings here      
-    )
+  
 }
 
