7f5438cde015bdb52b291af1bbd0c994299a7040
Telmo Menezes <telmo@telmomenezes.com>
2012-02-08 21:55:25 +0100

    Edge etype
    Former-commit-id: 90e70caab137cd1202cb905f84def45befbb3a26

--- core/src/main/scala/Edge.scala
+++ core/src/main/scala/Edge.scala
@@ -1,9 +1,13 @@
 package com.graphbrain
 
-class Edge(_id: String, links: List[String]) extends Vertex(_id, links) {
+class Edge(_id: String, links: List[String], val etype: String) extends Vertex(_id, links) {
   override val vtype = "edge"
+
+  override def toMap = super.toMap ++ Map(("etype" -> etype))
+
+  override def toString: String = super.toString + " " + etype
 }
 
 object Edge {
-  def apply(_id: String, links: List[String] = List[String]()) = new Edge(_id, links) 
+  def apply(_id: String, links: List[String], etype:String) = new Edge(_id, links, etype) 
 }
--- core/src/main/scala/HGDB.scala
+++ core/src/main/scala/HGDB.scala
@@ -3,12 +3,16 @@ package com.graphbrain
 object HGDB { 
   def get(_id: String) = {
     val map = Store.get(_id)
+    val rid = map.getOrElse("_id", "").toString
     val links: List[String] = map.getOrElse("links", List[String]()).asInstanceOf[List[String]]
     map("vtype") match {
-      case "vertex" => Vertex(map("_id").toString, links)
-      case "node" => Node(map("_id").toString, links)
-      case "edge" => Edge(map("_id").toString, links)
-      case _  => Vertex(map("_id").toString, links)
+      case "vertex" => Vertex(rid, links)
+      case "node" => Node(rid, links)
+      case "edge" => {
+        val etype = map.getOrElse("etype", "").toString
+        Edge(rid, links, etype)
+      }
+      case _  => Vertex(rid, links)
     }
   }
 
@@ -18,8 +22,8 @@ object HGDB {
   }
 
   def main(args: Array[String]) = {
-    println(store(Edge("e11")))
-    println(get("e11"))
+    println(store(Edge("e14", List[String](), "is")))
+    println(get("e14"))
     //println(Vertex("wikipedia/alan_ball_(screenwriter)"))
   }
 }
