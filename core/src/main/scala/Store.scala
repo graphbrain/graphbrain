a5d52a2313866910c83d799b33b0b66799bf6647
Telmo Menezes <telmo@telmomenezes.net>
2012-02-08 15:35:03 +0100

    improved Store and Vertex
    Former-commit-id: 8f87e1eecbbbbc99a77e25f8560ff3ec39e70e6c

--- core/src/main/scala/Store.scala
+++ core/src/main/scala/Store.scala
@@ -6,7 +6,7 @@ import com.mongodb.DBCollection
 import com.mongodb.BasicDBObject
 import com.mongodb.DBObject
 import com.mongodb.DBCursor
-import scala.collection.JavaConverters._
+import scala.collection.JavaConversions._
 
 object Store {
 	val conn = new Mongo()
@@ -16,19 +16,12 @@ object Store {
   def get(_id: String) = {
     val query = new BasicDBObject();
     query.put("_id", _id);
-    coll.findOne(query).asInstanceOf[java.util.Map[String, AnyRef]].asScala
+    coll.findOne(query).asInstanceOf[java.util.Map[String, Any]].toMap
   }
 
   def put(doc: Map[String, Any]) = {
     val dbobj = new BasicDBObject()
     for ((key, value) <- doc) dbobj.put(key, value)
     coll.insert(dbobj)
-  }
-
-  def main(args: Array[String]) = {
-    //val doc = Map(("_id" -> "test5"), ("list" -> Array(55, 1, 2, 3, 4, 77)))
-    //put(doc)
-    println(get("test5"))
-    println("Done.")
   }
 }
