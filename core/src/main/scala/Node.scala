4af60551ccb2b581b2ae8658a3576d21bfad76d0
Telmo Menezes <telmo@telmomenezes.com>
2012-02-09 23:18:10 +0100

    Store and HGDB are now classes instead of objects
    Former-commit-id: 66ad39bd2bf2b21c3cb40c239625a20bef65cdf3

--- core/src/main/scala/Node.scala
+++ core/src/main/scala/Node.scala
@@ -1,11 +1,13 @@
 package com.graphbrain
 
-class Node(_id: String, val links: Set[String]) extends Vertex(_id) {
+class Node(_id: String, val edges: Set[String]) extends Vertex(_id) {
   override val vtype = "node"
 
-  override def toMap: Map[String, Any] = super.toMap ++ Map(("links" -> links))
+  override def toMap: Map[String, Any] = super.toMap ++ Map(("edges" -> edges))
+
+  def addEdge(edge: Edge): Node = Node(_id, edges + edge._id)
 }
 
 object Node {
-  def apply(_id: String, links: Set[String] = Set[String]()) = new Node(_id, links) 
+  def apply(_id: String, edges: Set[String] = Set[String]()) = new Node(_id, edges) 
 }
