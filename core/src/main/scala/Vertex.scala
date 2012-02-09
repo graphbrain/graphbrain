f7c18884a70ff15c415193e1086c4e75ef8c161a
Telmo Menezes <telmo@telmomenezes.com>
2012-02-09 18:10:37 +0100

    restricting links to Node
    Former-commit-id: b6452270b48e1876b46faa356f13c6f1695c2ac3

--- core/src/main/scala/Vertex.scala
+++ core/src/main/scala/Vertex.scala
@@ -1,13 +1,13 @@
 package com.graphbrain
 
-class Vertex(val _id: String, val links: Set[String]) {
+class Vertex(val _id: String) {
   val vtype = "vertex"
 
-  def toMap = Map(("_id" -> _id), ("vtype" -> vtype), ("links" -> links))
+  def toMap: Map[String, Any] = Map(("_id" -> _id), ("vtype" -> vtype))
 
   override def toString: String = vtype + " [" + _id + "]"
 }
 
 object Vertex {
-  def apply(_id: String, links: Set[String] = Set[String]()) = new Vertex(_id, links) 
+  def apply(_id: String) = new Vertex(_id) 
 }
