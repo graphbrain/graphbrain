ecb22297cd46c2d972343b5bff7fe8d89740e3c5
Telmo Menezes <telmo@telmomenezes.net>
2012-02-10 19:23:25 +0100

    Node remove edge; NodeTests
    Former-commit-id: 8a450701bc21ee7307cac3b0c6e61f02d80dccb0

--- core/src/test/scala/HGDBTest.scala
+++ core/src/test/scala/HGDBTest.scala
@@ -1,16 +1,40 @@
 import org.scalatest.FunSuite
 import com.graphbrain.HGDB
 import com.graphbrain.Vertex
+import com.graphbrain.Node
 
 class HGDBTest extends FunSuite {
-	val hgdb = HGDB("testhgdb.testcoll")
+  val hgdb = HGDB("testhgdb.testcoll")
 
-	test("put Vertex") {
-		val vertex = Vertex("vertex0")
-		hgdb.remove(vertex)
-		hgdb.put(vertex)
-		
-		val vertexOut = hgdb.get("vertex0")
-		assert(vertex._id == vertexOut._id)
-	}
+  test("put Vertex") {
+    val vertex = Vertex("vertex0")
+    hgdb.remove(vertex)
+    hgdb.put(vertex)
+    
+    val vertexOut = hgdb.get("vertex0")
+    assert(vertex._id == vertexOut._id)
+  }
+
+  test("put Node") {
+    val node = Node("node0")
+    hgdb.remove(node)
+    hgdb.put(node)
+    
+    val nodeOut = hgdb.get("node0")
+    assert(node._id == nodeOut._id)
+  }
+
+  test("two node relationship") {
+    val node0 = Node("node0")
+    val node1 = Node("node1")
+    hgdb.remove(node0)
+    hgdb.remove(node1)
+    hgdb.put(node0)
+    hgdb.put(node1)
+    hgdb.addrel("test", Array[Node](node0, node1))
+
+    val eid = "test " + node0._id + " " + node1._id
+    val edge = hgdb.get(eid)
+    assert(edge._id == eid)
+  }
 }
---
+++ core/src/test/scala/NodeTest.scala
@@ -0,0 +1,16 @@
+import org.scalatest.FunSuite
+import com.graphbrain.Edge
+import com.graphbrain.Node
+
+class NodeTest extends FunSuite {
+	test("add Edge") {
+		val node0 = Node("node0")
+		val node1 = Node("node1")
+		
+		val edge = Edge("test", Array[Node](node0, node1))
+
+		val node = node0.addEdge(edge)
+
+		assert(node.edges == Set[String](edge._id))
+	}
+}
