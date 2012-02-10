ba988c035f8d5201c189e898e821de6ecd65f254
Telmo Menezes <telmo@telmomenezes.net>
2012-02-10 16:06:57 +0100

    debugging and creating tests for Node's auto _id and participants stuff
    Former-commit-id: d3ea742db0d87a236be8bc632834e5b220a5c206

--- core/src/main/scala/Edge.scala
+++ core/src/main/scala/Edge.scala
@@ -9,7 +9,7 @@ class Edge(_id: String, val etype: String) extends Vertex(_id) {
 
   def participantIds = {
   	val tokens = _id.split(' ')
-  	for (i <- 1 to tokens.length) yield tokens(i)
+  	for (i <- 1 until tokens.length) yield tokens(i)
   }
 }
 
@@ -17,7 +17,8 @@ object Edge {
   def apply(_id: String, etype:String) = new Edge(_id, etype)
 
   def apply(edgeType:String, participants: Array[Node]) = {
-  	val eid = edgeType + (for (node <- participants) yield (" " + node._id))
+  	val tokens = List[String](edgeType) ++ (for (node <- participants) yield node._id)
+  	val eid = tokens.reduceLeft(_ + " " + _)
   	new Edge(eid, edgeType)
   } 
 }
