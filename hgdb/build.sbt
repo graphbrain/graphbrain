1da8a7315f23cd40d07a16cc73d28d386ae68996
Telmo Menezes <telmo@telmomenezes.com>
2012-02-14 23:47:26 +0100

    renamed core to hgdb
    Former-commit-id: eeb3729dcc6806df60a581b435462676e4765199

--- hgdb/build.sbt
+++ hgdb/build.sbt
@@ -1,4 +1,4 @@
-name := "core"
+name := "hgdb"
 
 version := "0.1.0-SNAPSHOT"
 
--- project/Build.scala
+++ project/Build.scala
@@ -3,15 +3,13 @@ import Keys._
 
 
 object GraphbrainBuild extends Build {
-	lazy val core = Project(id = "core",
-                           base = file("core"))
+	lazy val hgdb = Project(id = "hgdb",
+                           base = file("hgdb"))
 
   lazy val inference = Project(id = "inference",
                            base = file("inference"))
 
   lazy val root = Project(id = "gb",
-                            base = file(".")) aggregate(core, inference)
-
-  
+                            base = file(".")) aggregate(hgdb, inference)
 }
 
