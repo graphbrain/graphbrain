d39195e6fed091e3728cd6c91390cf8b8e5bcca7
Telmo Menezes <telmo@telmomenezes.net>
2012-02-16 22:11:00 +0100

    created ImageNode
    Former-commit-id: 2c2c7b2f79aa9a2ab092b596c6922162cfc075c7

---
+++ hgdb/src/main/scala/ImageNode.scala
@@ -0,0 +1,15 @@
+package com.graphbrain.hgdb
+
+
+class ImageNode(id: String, val url: String, edges: Set[String]) extends Node(id, edges) {
+  override val vtype = "imagenode"
+
+  override def toMap: Map[String, Any] = super.toMap ++ Map(("url" -> url))
+
+  override def toString: String = super.toString + "; url: " + url
+}
+
+object ImageNode {
+  def apply(id: String, url: String, edges: Set[String] = Set[String]()) = new TextNode(id, url, edges)
+  def apply(id: String, url: String, edgesStr: String) = new TextNode(id, url, Node.str2set(edgesStr))
+}
