006e7fb2039dee3680050b296ae32d6f18e8816a
Telmo Menezes <telmo@telmomenezes.com>
2012-02-17 21:31:27 +0100

    moving webapp stuff to the com.graphbrain.webapp package
    Former-commit-id: b3573162681953abb023ef77308d699625e8f15a

--- webapp/src/main/scala/Server.scala
+++ webapp/src/main/scala/Server.scala
@@ -1,6 +1,5 @@
-package com.example
+package com.graphbrain.webapp
 
-/** embedded server */
 object Server {
   val logger = org.clapper.avsl.Logger(Server.getClass)
   val http = new dispatch.nio.Http
