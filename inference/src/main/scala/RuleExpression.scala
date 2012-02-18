789b4d66f8464fc8087b2ceff00fd225bca96a1f
Chih-Chun Chen <chihchun_chen@yahoo.co.uk>
2012-02-18 02:44:15 +0100

    Created new brain-generators project, added list crawler.
    Former-commit-id: a7865799fda2c3b2c636ba1b4fcee272f27c225a

--- inference/src/main/scala/RuleExpression.scala
+++ inference/src/main/scala/RuleExpression.scala
@@ -4,5 +4,6 @@ case class POS(exp:String) extends RuleExpression //A POS expression is a string
 case class REGEX(exp:String) extends RuleExpression
 case class GRAPH2(source:String, relation:String, target:String) extends RuleExpression
 case class GRAPH2Pair(g1:GRAPH2, g2:GRAPH2)//This data structure is used to pass two graphs in to merge.
+case class StringPair(s1:StringExpression, s2:StringExpression)
 case class RULE(condition:RuleExpression, input:RuleExpression, output:RuleExpression) extends RuleExpression
 case class COMPOSITE(exp1:RuleExpression, operator:String, exp2:RuleExpression) extends RuleExpression
