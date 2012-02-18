1238bd36b355e77621c27b8d67580621bc4fb765
Telmo Menezes <telmo@telmomenezes.com>
2012-02-18 02:12:46 +0100

    login page working in new webapp
    Former-commit-id: ce6b29bb181e0c84524b9c2c0e4e6f9ee303e6cb

--- webapp/src/main/scala/Server.scala
+++ webapp/src/main/scala/Server.scala
@@ -1,5 +1,7 @@
 package com.graphbrain.webapp
 
+import java.net.URL
+
 object Server {
   val logger = org.clapper.avsl.Logger(Server.getClass)
   val http = new dispatch.nio.Http
@@ -7,11 +9,10 @@ object Server {
   def main(args: Array[String]) {
     unfiltered.netty.Http(8080)
       .handler(GBPlan)
+      .resources(new URL(getClass().getResource("/robots.txt"), "."))
       .run { s =>
-        logger.info("starting GraphBrain webapp at localhost on port %s"
-                    .format(s.port))
-        unfiltered.util.Browser.open(
-          "http://127.0.0.1:%d/".format(s.port))
+        logger.info("starting GraphBrain webapp at localhost on port %s".format(s.port))
+        unfiltered.util.Browser.open("http://127.0.0.1:%d/".format(s.port))
       }
     http.shutdown()
   }
