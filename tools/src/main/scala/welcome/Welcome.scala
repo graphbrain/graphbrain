c3b2df533b7c28019465d0d3809d243952bbe214
Telmo Menezes <telmo@telmomenezes.net>
2012-03-03 16:20:42 +0100

    basic welcome graph
    Former-commit-id: 2b382e6f5e5bc3e3a56be502052177a9f5727077

--- tools/src/main/scala/welcome/Welcome.scala
+++ tools/src/main/scala/welcome/Welcome.scala
@@ -1,16 +1,55 @@
 package com.graphbrain.tools.welcome
 
 import com.graphbrain.hgdb.VertexStore
+import com.graphbrain.hgdb.TextNode
+import com.graphbrain.hgdb.EdgeType
+import com.graphbrain.hgdb.SourceNode
 
 
 class Welcome(val store: VertexStore) {
+  var nodes = 0
+  var edges = 0
 
+  def addTextNode(id: String, text: String) = {
+    val node = TextNode(id, text)
+    store.update(node)
+    addBinaryRel("source/welcome", "source", id)
+    nodes += 1
+  }
+
+  def addEdgeType(id: String, text:String) = {
+    val edgeType = EdgeType(id, text, List[String](text))
+    store.update(edgeType)
+    addBinaryRel("source/welcome", "source", id)
+  }
+
+  def addBinaryRel(node1: String, rel:String, node2: String) = {
+    store.addrel(rel, Array(node1, node2))
+    edges += 1
+  }
+
+  def createGraph() = {
+    store.put(SourceNode("source/welcome"))
+
+    // root node
+    addTextNode("welcome/graphbrain", "GraphBrain")
+
+    // GraphBrain is ...
+    addTextNode("welcome/knowledge_base", "Knowledge Base")
+    addTextNode("welcome/search_engine", "Search Engine")
+    addTextNode("welcome/artificial_brain", "Artificial Brain")
+    addBinaryRel("welcome/graphbrain", "is", "welcome/knowledge_base")
+    addBinaryRel("welcome/graphbrain", "is", "welcome/search_engine")
+    addBinaryRel("welcome/graphbrain", "is", "welcome/artificial_brain")
+  }
 }
 
 object Welcome {
-  def apply(args: Array[String]) {
-  	val store: VertexStore = new VertexStore("gb")
-  	val welcome = new Welcome(store)
-  	//addTextNode("welcome/graphbrain", "GraphBrain")
+  def apply(args: Array[String]) = {
+    val store: VertexStore = new VertexStore("gb")
+    val welcome = new Welcome(store)
+    welcome.createGraph()
+    println("Nodes created: " + welcome.nodes)
+    println("Edges create: " + welcome.edges)
   }
 }
