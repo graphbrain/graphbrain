f4c1a97ad895d1e56c5c15e26bb4d3a5fa7f6822
Telmo Menezes <telmo@telmomenezes.com>
2012-03-03 17:05:39 +0100

    added neighbors tool
    Former-commit-id: 2f1a6a6d9d58539657815a77f72b6dcdb1a2517c

---
+++ tools/src/main/scala/neighbors/Neighbors.scala
@@ -0,0 +1,17 @@
+package com.graphbrain.tools.neighbors
+
+import com.graphbrain.hgdb.VertexStore
+
+
+object Neighbors {
+  def apply(args: Array[String]) = {
+    if (args.size == 0) {
+      println("Error: too few parameters for neighbors. You need to specify the vertex id.")
+    }
+    else {
+      val store: VertexStore = new VertexStore("gb")
+      val neighbors = store.neighbors(args(0))
+      for (n <- neighbors) println(n._1 + " (parent: " + n._2 + ")")
+    }
+  }
+}
