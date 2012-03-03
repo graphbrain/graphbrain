cbad6f7d21262dfc4fdd78745cfbe3d34def6b2f
Telmo Menezes <telmo@telmomenezes.net>
2012-03-03 14:17:24 +0100

    working on DemoGraph tool
    Former-commit-id: 3c1bdc6592fc12959be480ddefc76cfe7269eef6

--- tools/src/main/scala/demograph/DemoGraph.scala
+++ tools/src/main/scala/demograph/DemoGraph.scala
@@ -1,6 +1,13 @@
 package com.graphbrain.tools.demograph
 
+import com.graphbrain.hgdb.VertexStore
+
+
 object DemoGraph {
+
   def run(args: Array[String]) {
+  	val store: VertexStore = new VertexStore("gb")
+
+  	//addTextNode("welcome/graphbrain", "GraphBrain")
   }
 }
