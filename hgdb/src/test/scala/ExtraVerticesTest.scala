b7a841bdcbff13738888094554da447510a0998b
Telmo Menezes <telmo@telmomenezes.net>
2012-03-09 13:19:27 +0100

    delrel deals with extra vertices
    Former-commit-id: cb4b81494c79fd0754284b524b8333dbe096a495

--- hgdb/src/test/scala/ExtraVerticesTest.scala
+++ hgdb/src/test/scala/ExtraVerticesTest.scala
@@ -38,4 +38,43 @@ class ExtraVerticesTest extends FunSuite {
     val extra10 = store.getExtraEdges("node0/10")
     assert(extra10.id == "")
   }
+
+  test("delrel with extra vertices") {
+    val node0 = TextNode("node0", "?")
+    store.remove(node0)
+    store.put(node0)
+    for (n <- 1 to 100) {
+      store.remove(TextNode("node" + n, "?"))
+      store.put(TextNode("node" + n, "?"))
+      store.addrel("test", Array("node0", "node" + n))
+    }
+
+    store.delrel("test", Array("node0", "node100"))
+    val extra9 = store.getExtraEdges("node0/9")
+    assert(!extra9.edges.contains("test node0 node100"))
+    assert(store.get("node0").extra == 9)
+
+    store.delrel("test", Array("node0", "node11"))
+    assert(!store.getExtraEdges("node0/1").edges.contains("test node0 node11"))
+    assert(store.get("node0").extra == 1)
+
+    store.put(TextNode("node101", "?"))
+    store.addrel("test", Array("node0", "node101"))
+    assert(store.getExtraEdges("node0/1").edges.contains("test node0 node101"))
+    assert(store.get("node0").extra == 1)
+
+    store.put(TextNode("node102", "?"))
+    store.addrel("test", Array("node0", "node102"))
+    assert(store.getExtraEdges("node0/9").edges.contains("test node0 node102"))
+    assert(store.get("node0").extra == 9)
+
+    store.put(TextNode("node103", "?"))
+    store.addrel("test", Array("node0", "node103"))
+    assert(store.getExtraEdges("node0/10").edges.contains("test node0 node103"))
+    assert(store.get("node0").extra == 10)
+
+    store.delrel("test", Array("node0", "node1"))
+    assert(!store.get("node0").edges.contains("test node0 node1"))
+    assert(store.get("node0").extra == 0)
+  }
 }
