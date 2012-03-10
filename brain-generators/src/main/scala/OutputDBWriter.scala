1a35e8e6cc7a335766b117628697b262dd6ac2fe
Chih-Chun Chen <chihchun_chen@yahoo.co.uk>
2012-03-10 22:58:06 +0100

    Exception handling in OutputDBWriter
    Former-commit-id: 63ad29ac28978ff6cae711674d98ba24940cd5b1

--- brain-generators/src/main/scala/OutputDBWriter.scala
+++ brain-generators/src/main/scala/OutputDBWriter.scala
@@ -17,40 +17,48 @@ class OutputDBWriter(storeName:String, source:String) {
 
 	def writeOutDBInfo(node1: String, rel: String, node2: String, resource: String):Unit=
 	{
-		val sourceNode=store.getSourceNode(ID.source_id(source))
-		val N1Wiki=ID.wikipedia_id(node1)
-		val N2Wiki=ID.wikipedia_id(node2)
-		val n1 = TextNode(id=N1Wiki, text=node1);
-		val n2 = TextNode(id=N2Wiki, text=node2);
 		
-		val n1RNode = URLNode(ID.url_id(wikiURL+N1Wiki), wikiURL+N1Wiki)
-		val n2RNode = URLNode(ID.url_id(wikiURL+N2Wiki), wikiURL+N2Wiki)
-		store.put(n1)
-		store.put(n2)
-		store.put(n1RNode)
-		store.put(n2RNode)
 
-		store.addrel("en_wikipage", Array[String](n1RNode.id, n1.id))
-		store.addrel("en_wikipage", Array[String](n2RNode.id, n2.id))
-		store.addrel("source", Array[String](sourceNode.id, n1.id))
-		store.addrel("source", Array[String](sourceNode.id, n2.id))
+		try{
+			val sourceNode=store.getSourceNode(ID.source_id(source))
+			val N1Wiki=ID.wikipedia_id(node1)
+			val N2Wiki=ID.wikipedia_id(node2)
+			val n1 = TextNode(id=N1Wiki, text=node1);
+			val n2 = TextNode(id=N2Wiki, text=node2);
+		
+			val n1RNode = URLNode(ID.url_id(wikiURL+N1Wiki), wikiURL+N1Wiki)
+			val n2RNode = URLNode(ID.url_id(wikiURL+N2Wiki), wikiURL+N2Wiki)
+			store.put(n1)
+			store.put(n2)
+			store.put(n1RNode)
+			store.put(n2RNode)
+
+			store.addrel("en_wikipage", Array[String](n1RNode.id, n1.id))
+			store.addrel("en_wikipage", Array[String](n2RNode.id, n2.id))
+			store.addrel("source", Array[String](sourceNode.id, n1.id))
+			store.addrel("source", Array[String](sourceNode.id, n2.id))
 		
 		
 						
-		//The id for the relationship between two nodes
-		val relID = getRelID(rel, n1.id, n2.id)
+			//The id for the relationship between two nodes
+			val relID = getRelID(rel, n1.id, n2.id)
 		
 
-		store.addrel(rel, Array[String](ID.wikipedia_id(node1), ID.wikipedia_id(node2)))
+			store.addrel(rel, Array[String](ID.wikipedia_id(node1), ID.wikipedia_id(node2)))
 		
-		if(resource!="")
-		{
-			val resourceNode = URLNode(ID.url_id(resource), resource)	
-			store.put(resourceNode)
-			//Only the explicit reference in the DBPedia record is included - the Wikipedia pages to the nodes are not.
-			store.addrel("source", Array[String](sourceNode.id, resourceNode.id))
-			store.addrel("en_wikipage_line", Array[String](resourceNode.id, relID))
+			if(resource!="")
+			{
+				val resourceNode = URLNode(ID.url_id(resource), resource)	
+				store.put(resourceNode)
+				//Only the explicit reference in the DBPedia record is included - the Wikipedia pages to the nodes are not.
+				store.addrel("source", Array[String](sourceNode.id, resourceNode.id))
+				store.addrel("en_wikipage_line", Array[String](resourceNode.id, relID))
+			}	
 		}
+		catch {
+			case e => e.printStackTrace()
+		}
+		
 		
 
 	}
@@ -59,9 +67,14 @@ class OutputDBWriter(storeName:String, source:String) {
   	{
   		val sourceNode=SourceNode(id=ID.source_id(sourceID))
 	    val urlNode=URLNode(ID.url_id(sourceURL), sourceURL)
-	    store.put(sourceNode)
-	    store.put(urlNode)
-	    store.addrel("source", Array[String](sourceNode.id, urlNode.id))
+	    try{
+	    	store.put(sourceNode)
+	    	store.put(urlNode)
+	    	store.addrel("source", Array[String](sourceNode.id, urlNode.id))
+	    }
+	    catch{
+	    	case e => e.printStackTrace()
+	    }
   	}
 
 
