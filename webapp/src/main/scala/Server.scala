4977cc298d184fde692dd7689490fa0e2bc9492d
Telmo Menezes <telmo@telmomenezes.net>
2012-03-14 01:07:31 +0100

    prepared webapp for deploy
    Former-commit-id: cb69c218aa83dad11fbe62c02161b8e094e42e80

--- webapp/src/main/scala/Server.scala
+++ webapp/src/main/scala/Server.scala
@@ -12,7 +12,7 @@ object Server {
       .resources(new URL(getClass().getResource("/robots.txt"), "."))
       .run { s =>
         logger.info("starting GraphBrain webapp at localhost on port %s".format(s.port))
-        unfiltered.util.Browser.open("http://127.0.0.1:%d/".format(s.port))
+        //unfiltered.util.Browser.open("http://127.0.0.1:%d/".format(s.port))
       }
     http.shutdown()
   }
