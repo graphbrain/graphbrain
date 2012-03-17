7326fce3e239600c1aa50985117b52b262615d0b
Telmo Menezes <telmo@telmomenezes.com>
2012-03-18 00:24:31 +0100

    SearchInterfave.indexVertex
    Former-commit-id: a732263c2b81fddc6f3a73df0fa445cc832e1fe4

--- hgdb/src/main/scala/SearchInterface.scala
+++ hgdb/src/main/scala/SearchInterface.scala
@@ -5,4 +5,11 @@ abstract trait SearchInterface {
   def initIndex(): Unit
   def index(key: String, text: String): Unit
   def query(text: String): SearchResults
+
+  def indexVertex(vertex: Vertex) = {
+    vertex match {
+      case t: TextNode => index(t.id, t.text)
+      case _ =>
+    }
+  }
 }
