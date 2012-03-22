17f7cb57bde2b7da3548ad84464cc146c0bfbbae
Telmo Menezes <telmo@telmomenezes.com>
2012-03-22 23:25:54 +0100

    working on interface
    Former-commit-id: 5a667ae18895a93e98279a92eac484f7751d26c3

--- webapp/src/main/scala/Server.scala
+++ webapp/src/main/scala/Server.scala
@@ -4,16 +4,20 @@ import java.net.URL
 
 object Server {
   val logger = org.clapper.avsl.Logger(Server.getClass)
-  val http = new dispatch.nio.Http
+  var http: unfiltered.netty.Http = null
 
-  def main(args: Array[String]) {
-    unfiltered.netty.Http(8080)
+  def start() = {
+    http = unfiltered.netty.Http(8080)
       .handler(GBPlan)
       .resources(new URL(getClass().getResource("/robots.txt"), "."))
-      .run { s =>
+      
+      http.run { s =>
         logger.info("starting GraphBrain webapp at localhost on port %s".format(s.port))
         //unfiltered.util.Browser.open("http://127.0.0.1:%d/".format(s.port))
       }
-    http.shutdown()
+  }
+
+  def main(args: Array[String]) {
+    start()
   }
 }
