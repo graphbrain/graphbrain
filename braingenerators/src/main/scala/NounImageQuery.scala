19b8dee4f4ed6ddd3ba736760ca4f76159f52614
Chih-Chun Chen <chihchun_chen@yahoo.co.uk>
2012-04-06 10:35:33 +0200

    Extract Contributor (need to agree on node/relationship insertions)
    Former-commit-id: 9a93b7b00e15b277aecf21e28a7d808f604326ad

--- braingenerators/src/main/scala/NounImageQuery.scala
+++ braingenerators/src/main/scala/NounImageQuery.scala
@@ -13,16 +13,39 @@ object NounImageQuery {
 
   val nounProjectURL = "http://thenounproject.com/noun/"
   val svgRegex="""[^\s^\']+(\.(?i)(svg))""".r
+  val contributorRegex="""(<span class="attribution">\s*.*\s*</span>)""".r
+  val contRegex="""(<a href=)(.+?)(>)(.+?)(</a>)""".r
+  val contURLRegex="""(<a href=.+?>)""".r
+  val contTagRegex="""(<a href=.+?>)(.+?)(</a>)"""
+
+  def getContributor(noun:String):(String, String)={
+    try{
+          val imageURL=NounImageQuery.nounProjectURL + noun + "/";
+          val result=NounImageQuery.readPage(imageURL)
+          val contributorData=contRegex.findAllIn(contributorRegex.findAllIn(result).next).next;
+          val contributorURL=contURLRegex.findAllIn(contributorData).next.replace("<a href=", "").replace("\"", "").replace(">", "");
+          val contributorName=contributorData.replaceAll("<a href=.+?>", "").replace("</a>", "")
+          return (contributorName, contributorURL)
+      }
+      catch
+      {
+        case e => e.printStackTrace(); return ("", "")
+      }
+
+    
+    
+  }
 
   def getImageResource(noun:String):String={
     val imageURL = NounImageQuery.nounProjectURL + noun + "/";
     val result=NounImageQuery.readPage(imageURL)
     val svgImage=svgRegex.findFirstIn(result);
+
     return(svgImage.getOrElse("Not found"))
     
   }
 
-  def getSVGImage(imageURL:String):String={
+  def getSVGImagePage(imageURL:String):String={
     return NounImageQuery.readPage(imageURL);
   }
 
@@ -51,12 +74,17 @@ object NounImageQuery {
   }
 
   def main(args : Array[String]) : Unit = {
+    //val imageResource=NounImageQuery.getImageResource("tree")
+      //println(NounImageQuery.imageResource); 
+      //println(NounImageQuery.getSVGImagePage(imageResource));
+      //println(NounImageQuery.getContributor("tree"));
     args(0) match
     {
-      case a:String => 
-      val imageResource=NounImageQuery.getImageResource(a); 
+      case a:String => val imageResource=NounImageQuery.getImageResource(a); 
       println(imageResource); 
-      println(NounImageQuery.getSVGImage(imageResource))
+      println(NounImageQuery.getSVGImagePage(imageResource));
+      println(NounImageQuery.getContributor(a));
+      case _ => 
 
     }
     
