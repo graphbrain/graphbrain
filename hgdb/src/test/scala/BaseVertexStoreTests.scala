005ee9f89ce15a767e7100467c3a8a370575eb8f
Telmo Menezes <telmo@telmomenezes.com>
2012-04-08 14:55:50 +0200

    rootNeighbourEdges and fixing tests
    Former-commit-id: ad1d4037200c82b69e3dd478b3caf6d08167f189

--- hgdb/src/test/scala/BaseVertexStoreTests.scala
+++ hgdb/src/test/scala/BaseVertexStoreTests.scala
@@ -206,6 +206,30 @@ trait BaseVertexStoreTests { this: FunSuite =>
     assert(store.neighborEdges(store.neighbors("node5", 2)) == Set[String](e01, e02, e03, e045, e56, e67))
   }
 
+  test("rootNeighborEdges for a few neighbors [" + label + "]") {
+    val node0 = TextNode("node0", ""); store.remove(node0); store.put(node0)
+    val node1 = TextNode("node1", ""); store.remove(node1); store.put(node1)
+    val node2 = TextNode("node2", ""); store.remove(node2); store.put(node2)
+    val node3 = TextNode("node3", ""); store.remove(node3); store.put(node3)
+    val node4 = TextNode("node4", ""); store.remove(node4); store.put(node4)
+    val node5 = TextNode("node5", ""); store.remove(node5); store.put(node5)
+    val node6 = TextNode("node6", ""); store.remove(node6); store.put(node6)
+    val node7 = TextNode("node7", ""); store.remove(node7); store.put(node7)
+    
+    store.addrel("test", Array[String]("node0", "node1")); val e01 = "test node0 node1"
+    store.addrel("test", Array[String]("node0", "node2")); val e02 = "test node0 node2"
+    store.addrel("test", Array[String]("node0", "node3")); val e03 = "test node0 node3"
+    store.addrel("test", Array[String]("node0", "node4", "node5")); val e045 = "test node0 node4 node5"
+    store.addrel("test", Array[String]("node5", "node6")); val e56 = "test node5 node6"
+    store.addrel("test", Array[String]("node6", "node7")); val e67 = "test node6 node7"
+
+    assert(store.rootNeighborEdges("node0", store.neighbors("node0", 0)) == Set[String]())
+    assert(store.rootNeighborEdges("node0", store.neighbors("node0", 1)) == Set[String](e01, e02, e03, e045))
+    assert(store.rootNeighborEdges("node0", store.neighbors("node0", 2)) == Set[String](e01, e02, e03, e045))
+    assert(store.rootNeighborEdges("node0", store.neighbors("node0", 3)) == Set[String](e01, e02, e03, e045))
+    assert(store.rootNeighborEdges("node5", store.neighbors("node5", 2)) == Set[String](e045, e56))
+  }
+
   test("addrel with inexistant vertex [" + label + "]") {
     intercept[KeyNotFound] {
       store.addrel("test", Array("dummy1", "dummy2"))
