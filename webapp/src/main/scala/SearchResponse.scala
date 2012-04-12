458ffa801e0b61ca8742f5144f677b1fa9b9d73b
Telmo Menezes <telmo@telmomenezes.com>
2012-04-12 21:40:59 +0200

    working on search UI
    Former-commit-id: cc34cd0da3bee752f3d57a7422190bd4ee58417b

--- webapp/src/main/scala/SearchResponse.scala
+++ webapp/src/main/scala/SearchResponse.scala
@@ -3,15 +3,26 @@ package com.graphbrain.webapp
 import unfiltered.response._
 import java.io.Writer
 import com.graphbrain.searchengine.RiakSearchInterface
+import com.graphbrain.hgdb.VertexStore
 
 
-case class SearchResponse(query: String) extends ResponseWriter {
+case class SearchResponse(store: VertexStore, query: String) extends ResponseWriter {
 	def write(writer: Writer) {
 		val si = RiakSearchInterface("gbsearch")
-      	val results = si.query(query)
-      	//println("number of matches: " + results.numResults)
-      	//for (id <- results.ids) println(id)
+      	val results = si.query(query + "*")
+      	var html = ""
+      	if (results.numResults == 0) {
+      		html = "<p>Sorry, no results found.</p>"
+      	}
+      	else {
+      		for (id <- results.ids) {
+      			val vertex = store.get(id)
+      			if (vertex.toString != "") {
+      				html += "<p><a href=\"/node/" + id + "\">" + vertex + "</a></p>"
+      			}
+      		}
+      	}
 
-		writer.write(results.toString)
+		writer.write(html)
 	} 
 }
