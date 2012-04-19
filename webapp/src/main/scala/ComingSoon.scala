fdec15a02157258714e56edd32ecd1d0f00701f6
Telmo Menezes <telmo@telmomenezes.com>
2012-04-19 19:28:37 +0200

    X-Real-Ip on webapp logs
    Former-commit-id: d6208c459e7694b4dac4b27a3c5b426fe976feb0

--- webapp/src/main/scala/ComingSoon.scala
+++ webapp/src/main/scala/ComingSoon.scala
@@ -1,14 +1,24 @@
 package com.graphbrain.webapp
 
-case class ComingSoon() extends Page {
+case class ComingSoon(prod: Boolean) extends Page {
+	def cssAndJs = {
+      if (prod) {
+        """<link href="/css/soon.css" type="text/css" rel="Stylesheet" />""" +
+        analyticsJs
+      }
+      else {
+        """<link href="/css/soon.css" type="text/css" rel="Stylesheet" />"""
+      }
+    }
+
 	override def html = {
 
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>GraphBrain</title>
-<link href="/css/soon.css" type="text/css" rel="Stylesheet" />  
+<title>GraphBrain</title>  
 <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+{scala.xml.Unparsed(cssAndJs)}
 </head>
 <body>
 
