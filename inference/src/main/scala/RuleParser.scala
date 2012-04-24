f00604eb4515b5d0508dc3ef5c1558a8cf0ec94d
Chih-Chun Chen <chihchun_chen@yahoo.co.uk>
2012-04-25 00:43:06 +0200

    Working on RuleParser
    Former-commit-id: d3d20972838da043c23c9e0b7b9a1f73a911fd3b

--- inference/src/main/scala/RuleParser.scala
+++ inference/src/main/scala/RuleParser.scala
@@ -2,7 +2,7 @@ package com.graphbrain.inference
 
 object RuleParser{
 
-	val RuleRegex=""".+\(.+\):.+\(.+\)>.+\(.+\)""".r
+	/*val RuleRegex=""".+\(.+\):.+\(.+\)>.+\(.+\)""".r
 	val POSRegex="""POS\(.+\)""".r
 	val StringExpressionRegex="""STRING\(.+\)""".r
 	val REGEXRegex="""REGEX\(.+\)""".r
@@ -13,19 +13,78 @@ object RuleParser{
 
 
 	
-
-	def parse(ruleString:String):Rule={
-		val ruleString = RuleRegex.findAllIn(ruleString);
-		if(ruleString.length==1)
-		{
-			val rule = ruleString.next;
-			val cond = rule.split(":")(0)
-			val source = rule.split(":")(1).split(">")(0)
-			val target = rule.split(":")(1).split(">")(1)
-
-			
+	def parse(expString:String):RuleExpression={
+		
+		if(RuleRegex.findAllIn(ruleString).length==1){
+			return parseRule(expString);
+		}
+		else if(POSRegex.findAllIn(ruleString).length==1){
+			return parsePOS(expString);
+		}
+		else if(StringExpressionRegex.findAllIn(ruleString).length==1){
+			return parseString(expString);
+		}
+		else if(REGEXRegex.findAllIn(ruleString).length==1){
+			return parseREGEX(expString);
+		}
+		else if(PLACEHOLDERRegex.findAllIn(ruleString).length==1){
+			return parsePLACEHOLDER(expString);
+		}
+		else if(GRAPH2Regex.findAllIn(ruleString).length==1){
+			return parseGRAPH2(expString);
+		}
+		else if(COMPOSITERegex.findAllIn(ruleString).length==1){
+			return parseCOMPOSITE(expString);
+		}
+		else if(GRAPH2PAIRRegex.findAllIn(ruleString).length==1){
+			return parseGRAPH2PAIR(expString);
 		}
 
+	}
+
+	def parseRule(ruleString:String):Rule={
+		
+		
+		
+		val rule = ruleString.next;
+		val condString = rule.split(":")(0)
+		val sourceString = rule.split(":")(1).split(">")(0)
+		val targetString = rule.split(":")(1).split(">")(1)	
+		val condition =
+		val source = 
+		val target =
+		return Rule(condition, source, target)
+
   }
+
+  def parseREGEX(expString:String):REGEX={
+  	
+  }
+
+  def parsePOS(expString:String):POS={
+  	
+  }
+
+  def parseGRAPH2(expString:String):GRAPH2={
+  	
+  }
+
+  def parsePLACEHOLDER(expString:String):PLACEHOLDER={
+  	
+  }
+
+  def parseCOMPOSITE(expString:String):COMPOSITE={
+  	
+  }
+
+  def parseString(expString:String):StringExpression={
+  	
+  }
+
+  def parseGRAPH2PAIR(expString:String):GRAPH2PAIR={
+  	
+  }*/
+
+
 	
 }
