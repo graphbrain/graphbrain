14b67ac3489c19dd24b9a192a42ea50fd7b8e579
Chih-Chun Chen <chihchun_chen@yahoo.co.uk>
2012-05-28 17:59:54 +0200

    emails working
    Former-commit-id: 0c995abe3325b45305c66a7113ef5e4b78a0eda4

--- email/src/main/scala/SendMail.scala
+++ email/src/main/scala/SendMail.scala
@@ -18,6 +18,8 @@ class SendMail (uname:String, pwd:String, apiKey:String) {
 	
 	val registrationEmailFile = "RegistrationMessage.txt";
 	val referralEmailFile = "ReferralMessage.txt";
+	val registrationEmail = "Welcome #r_e_c_i_p_i_e_n_t,\n \n You've successuly registered as a member of GraphBrain! You can create as many brains as you like. Simply log in with your username: #u_s_e_r_n_a_m_e and password.\n \n As GraphBrain is still very much in its infancy, we would be grateful for any feedback or suggestions. E-mail us at: contact@graphbrain.com\n \n Happy brain-building,\n \n The GraphBrain team\n \n GraphBrain: a beautiful way to organise, explore and share information";
+	val referralEmail = "Hello #r_e_c_i_p_i_e_n_t, \n \n #s_e_n_d_e_r would like to invite you to use GraphBrain, a beautiful way to organize and explore information. To find out more and register, simply go to http://graphbrain.com and sign up! \n \n Looking forward to having you in GraphBrain,\n \n The GraphBrain team";
 
 	def sendEmail(recipient: String, subject: String, messageText: String, from:String = "contact@graphbrain.com"):Boolean= {
 
@@ -54,22 +56,25 @@ class SendMail (uname:String, pwd:String, apiKey:String) {
 	}
 
 	//For marketing campaigns:
-	def sendEmails(recipientEmails: List[String], subject:String, messageBaseText: String, from:String = "contact@graphbrain.com", defaultRecipientName: String = "", defaultSenderName: String = "", recipientNames: List[String]=Nil, senderNames: List[String] = Nil): Boolean = {
+	def sendEmails(recipientEmails: List[String], subject:String, messageBaseText: String, from:String = "contact@graphbrain.com",defaultRecipientName: String = "", defaultSenderName: String = "", recipientNames: List[String]=Nil, senderNames: List[String] = Nil, userNames: List[String] = Nil): Boolean = {
 		var success = 0;
 		
 		for(i <- 0 to recipientEmails.length-1) {
 			var messageText = messageBaseText;
 			if(recipientNames != Nil) {
-				messageText = messageText.replace("r_e_c_i_p_i_e_n_t", recipientNames(i))
+				messageText = messageText.replace("#r_e_c_i_p_i_e_n_t", recipientNames(i))
 			}
 			else {
-				messageText = messageText.replace("r_e_c_i_p_i_e_n_t", defaultRecipientName)	
+				messageText = messageText.replace("#r_e_c_i_p_i_e_n_t", defaultRecipientName)	
 			}
 			if(senderNames != Nil) {
-				messageText = messageText.replace("s_e_n_d_e_r", senderNames(i))
+				messageText = messageText.replace("#s_e_n_d_e_r", senderNames(i))
 			}
 			else {
-				messageText = messageText.replace("s_e_n_d_e_r", defaultSenderName)
+				messageText = messageText.replace("#s_e_n_d_e_r", defaultSenderName)
+			}
+			if(userNames != Nil) {
+				messageText = messageText.replace("#u_s_e_r_n_a_m_e", userNames(i))
 			}
 
 			if(sendEmail(recipientEmails(i), subject, messageText, from)) {success +=1}
@@ -79,14 +84,14 @@ class SendMail (uname:String, pwd:String, apiKey:String) {
 		return true;
 	}
 
-	def sendRegistrationEmail(recipientEmails: List[String], recipientNames: List[String]): Boolean = {
+	def sendRegistrationEmail(recipientEmails: List[String], recipientNames: List[String], userNames: List[String]): Boolean = {
 
-		return sendEmails(recipientEmails, subject = "Welcome to GraphBrain", messageBaseText = fromFile(registrationEmailFile).mkString, defaultRecipientName = "", defaultSenderName = "", recipientNames = recipientNames)
+		return sendEmails(recipientEmails, subject = "Welcome to GraphBrain", messageBaseText = registrationEmail, defaultRecipientName = "", defaultSenderName = "", recipientNames = recipientNames, userNames = userNames)
 
 	}
 
 	def sendReferralEmail(recipientEmails: List[String], recipientNames: List[String], senderNames: List[String]): Boolean = {
-		return sendEmails(recipientEmails, subject = "Invitation to GraphBrain", messageBaseText = fromFile(referralEmailFile).mkString, defaultRecipientName = "", defaultSenderName = "", recipientNames = recipientNames, senderNames = senderNames)
+		return sendEmails(recipientEmails, subject = "Invitation to GraphBrain", messageBaseText = referralEmail, defaultRecipientName = "", defaultSenderName = "", recipientNames = recipientNames, senderNames = senderNames)
 	}
 }
 
@@ -105,13 +110,14 @@ object SendMail {
       val m = new SendMail(uname, pwd, apiKey);
       //m.sendEmail("chihchun_chen@yahoo.co.uk", "testing from scala code", "If this gets to you, it's working. \n")
       
-      val recipients = List("chihchun_chen@yahoo.co.uk", "c.chen@abmcet.net")
-      val recipientNames = List("Aliana Hepwood", "Chih-Chun")
-      val senderNames = List("Ch-Ch", "Ali Hepwood")
+      val recipients = List("chihchun_chen@yahoo.co.uk")
+      val recipientNames = List("Aliana Hepwood")
+      val userNames = List("chihchun_chen")
+      val senderNames = List("Ch-Ch")
       val message = fromFile("TestMessage.txt").mkString
       //m.sendEmails(recipients, "testing", message, "contact@graphbrain.com", recipientNames, senderNames)
-      m.sendRegistrationEmail(recipients, recipientNames);
-      //m.sendReferralEmail(recipients, recipientNames, senderNames);
+      m.sendRegistrationEmail(recipients, recipientNames, userNames);
+      m.sendReferralEmail(recipients, recipientNames, senderNames);
       
         
     }
