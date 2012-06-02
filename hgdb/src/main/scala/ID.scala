9c477abdf9f5708c18b7e8fbbbef1f9191ce07b4
Telmo Menezes <telmo@telmomenezes.com>
2012-06-02 19:20:41 +0200

    separating HSV code from GraphInterface
    Former-commit-id: 2374ffb154c78313263d5b4642e5419bdf3cef98

--- hgdb/src/main/scala/ID.scala
+++ hgdb/src/main/scala/ID.scala
@@ -8,33 +8,27 @@ object ID {
   def brainId(name: String, userName: String) =
     "brain/" + userName + "/" + sanitize(name)
 
-   def usergenerated_id(userName:String, thing:String, brainName:String)=
-	brainId(brainName, userName) + "/" + thing
+  def usergenerated_id(userName:String, thing:String, brainName:String) =
+	  brainId(brainName, userName) + "/" + thing
 
-   def image_id(image_url:String)=
+  def image_id(image_url:String) =
     "image/" + sanitize(image_url)
-   def image_id(image_name:String, image_url:String="")=
-	"image/" + sanitize(image_name) + "/" + sanitize(image_url)
   
+  def image_id(image_name:String, image_url:String="") =
+	  "image/" + sanitize(image_name) + "/" + sanitize(image_url)
 
-   def video_id(video_url:String)=
+  def video_id(video_url:String) =
     "video/" +  sanitize(video_url)
 
-   def video_id(video_name:String, video_url:String="")=
-	"video/" + sanitize(video_name) + "/" + sanitize(video_url)
+  def video_id(video_name:String, video_url:String="") =
+	  "video/" + sanitize(video_name) + "/" + sanitize(video_url)
 
-  
+  def text_id(thing:String):String =
+	  sanitize(thing)
 
-   def text_id(thing:String):String=
-	sanitize(thing)
+  def relation_id(relation:String):String =
+	  sanitize(relation)
 
-   def relation_id(relation:String):String=
-	sanitize(relation)
-
-   def url_id(url:String):String=
+  def url_id(url:String):String =
     "web/"+sanitize(url)
-  
-  
-  
-  
 }
