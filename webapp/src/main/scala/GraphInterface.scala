c21fb29332576bc99a6e121ea176eac58ecbb8e0
Telmo Menezes <telmo@telmomenezes.net>
2012-06-02 19:20:41 +0200

    separating HSV code from GraphInterface
    Former-commit-id: 2374ffb154c78313263d5b4642e5419bdf3cef98

--- webapp/src/main/scala/GraphInterface.scala
+++ webapp/src/main/scala/GraphInterface.scala
@@ -14,30 +14,6 @@ import com.codahale.jerkson.Json._
 import scala.math
 
 
-class PoorLong (val l:Long) {
-    /** a "mathematical" modulo that always maps to 0...n-1 (for positive n) */
-    def mod(n:Long) = {
-        val m = l % n
-        if (m<0) m+n else m
-    }
-}
-
-class PoorDouble (val d:Double) {
-    /**the "decimals" of this double */
-    def fraction:Double = d - d.floor
-
-    /** a "pseudo-mod", 7.5 mod 5 would map to 2.5, -0.5 mod 5 would map to 4.5 */
-    def mod(n:Long) = new PoorLong(d.floor.longValue).mod(n).toDouble + fraction
-}
-
-object Conversions {
-    implicit def longToPoorLong(l:Long) = new PoorLong(l)
-    implicit def doubleToPoorDouble(d:Double) = new PoorDouble(d)
-} 
-
-import Conversions._ //put this import before your hsv converter code
-
-
 class GraphInterface (val rootId: String, val store: VertexStore, val user: UserNode) {
   val neighbors = store.neighbors(rootId)
   val edgeIds = store.rootNeighborEdges(rootId, neighbors)
@@ -161,23 +137,6 @@ class GraphInterface (val rootId: String, val store: VertexStore, val user: User
     generate(json)
   }
 
-  /** Converts an hsv triplet in the range ([0,360],[0,1],[0,1])
-   *  to an rgb triplet in the range ([0,1],[0,1],[0,1])
-   */
-  private def hsvToRgb(h:Double, s:Double, v:Double): (Double, Double, Double) = {
-    val c = s * v
-    val h1 = h / 60.0
-    val x  = c * (1.0 - ((h1 mod 2) - 1.0).abs) 
-    val (r,g,b) = if (h1 < 1.0) (c, x, 0.0)
-                else if (h1 < 2.0) (x, c, 0.0)
-                else if (h1 < 3.0) (0.0, c, x)
-                else if (h1 < 4.0) (0.0, x, c)
-                else if (h1 < 5.0) (x, 0.0, c)
-                else (c, 0.0, x) 
-    val m = v - c
-    (r + m, g + m, b + m)
-  }
-
   private def linkColor(label: String) = {
     val code = math.abs(label.hashCode) % 99
     var hue: Double = code.toDouble / 99.0
@@ -187,7 +146,7 @@ class GraphInterface (val rootId: String, val store: VertexStore, val user: User
     hue *= deltaHue
     hue += hue1
     // println("label: " + label + "; hue: " + hue)
-    val rgb = hsvToRgb(hue, 0.75, 1.0)
+    val rgb = HSV.hsvToRgb(hue, 0.75, 1.0)
     val r: Int = (rgb._1 * 255).toInt
     val g: Int = (rgb._2 * 255).toInt
     val b: Int = (rgb._3 * 255).toInt
