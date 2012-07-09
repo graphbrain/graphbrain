6ece085ac845fedfa50a6fe3b496b3fbe5cfbe45
Telmo Menezes <telmo@telmomenezes.com>
2012-07-09 22:23:03 +0200

    created UserOps
    Former-commit-id: 5cb7724c72605e66a575b485c21a52701545d34b

--- hgdb/src/main/scala/UserOps.scala
+++ hgdb/src/main/scala/UserOps.scala
@@ -4,13 +4,27 @@ package com.graphbrain.hgdb
 trait UserOps extends VertexStoreInterface {
 
   def createAndConnectVertices(edgeType: String, participants: Array[Vertex], userid: String) = {
+    val userSpace = participants.exists(v => ID.isInUserSpace(v.id))
+
     for (v <- participants) {
       if (!exists(v.id)) {
         put(v)
       }
+      if (!ID.isInUserSpace(v.id)) {
+      	val userSpaceId = ID.globalToUser(v.id, userid)
+      	if (!exists(userSpaceId)) {
+          put(v.clone(userSpaceId))
+      	}	
+      }
     }
 
-    val ids = for (v <- participants) yield v.id
+    if (!userSpace) {
+      val ids = for (v <- participants) yield v.id
+      addrel(edgeType.replace(" ", "_"), ids)
+    }
+
+    val ids = for (v <- participants) yield
+      if (ID.isInUserSpace(v.id)) v.id else ID.globalToUser(v.id, userid)
     addrel(edgeType.replace(" ", "_"), ids)
   }
 }
