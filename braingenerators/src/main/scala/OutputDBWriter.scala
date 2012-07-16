6c4eccc9de5d358e9601eaa16398b093b6a94450
Telmo Menezes <telmo@telmomenezes.com>
2012-07-16 21:07:11 +0200

    removing indexing from braingenerators
    Former-commit-id: 63fa733613bcea016ecfc84868661385c1f8f633

--- braingenerators/src/main/scala/OutputDBWriter.scala
+++ braingenerators/src/main/scala/OutputDBWriter.scala
@@ -15,13 +15,12 @@ import com.graphbrain.hgdb.SourceNode
 import com.graphbrain.hgdb.URLNode
 import com.graphbrain.hgdb.Vertex
 import com.graphbrain.hgdb.EdgeType
-import com.graphbrain.searchengine.Indexing
 
 
 class OutputDBWriter(storeName:String, source:String, username:String, name:String, role:String) {
 	
 
-	val store = new VertexStore(storeName) with Indexing with BurstCaching with UserManagement with UserOps
+	val store = new VertexStore(storeName) with BurstCaching with UserManagement with UserOps
 	val wikiURL = "http://en.wikipedia.org/wiki/"
 
 	def writeOutDBInfo(node1: String, relin: String, node2: String, resource: String):Unit=
