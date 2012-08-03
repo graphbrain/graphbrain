945b587b8c1193d722eb5c3f777e341e481b833d
chihchunchen <chihchun_chen@yahoo.co.uk>
2012-08-03 12:01:57 +0200

    Disambiguation handling for braingenerators
    Former-commit-id: b237e4bd5326cdf111f390c2d486d11b9bbe5e75

--- braingenerators/src/main/scala/OutputDBWriter.scala
+++ braingenerators/src/main/scala/OutputDBWriter.scala
@@ -25,6 +25,7 @@ class OutputDBWriter(storeName:String, source:String, username:String, name:Stri
 	val wikiURL = "http://en.wikipedia.org/wiki/"
 	val wikiPageET = EdgeType(ID.reltype_id("wikipage"), label = "wikipage")
 	val lineRegex = """#.*?""".r
+	val wikiRel = "sys/wikipedia"
 	
 
 	def writeOutDBInfo(node1: String, relin: String, node2: String, resource: String):Unit=
@@ -33,10 +34,9 @@ class OutputDBWriter(storeName:String, source:String, username:String, name:Stri
 
 		try{
 			val rel=ID.relation_id(relin);
-			
-			val global1 = ID.text_id(node1, "1")
-			val global2 = ID.text_id(node2, "1")
 			val globalRelType = ID.reltype_id(rel, 1)
+			val wikiName1 = ID.wikipedia_id(node1)
+			val wikiName2 = ID.wikipedia_id(node2)
 			
 			
 			//val N1Wiki=ID.wikipedia_id(node1)
@@ -45,29 +45,24 @@ class OutputDBWriter(storeName:String, source:String, username:String, name:Stri
 			//val nw1 = TextNode(id=N1Wiki, text=URLDecoder.decode(node1, "UTF-8"));
 			//val nw2 = TextNode(id=N2Wiki, text=URLDecoder.decode(node2, "UTF-8"));
 
-			val ng1 = TextNode(id = global1, text=URLDecoder.decode(node1, "UTF-8"))
-			val ng2 = TextNode(id = global2, text=URLDecoder.decode(node2, "UTF-8"))
+			val ng1 = insertAndGetWikiDisambigNode(node1)
+			val ng2 = insertAndGetWikiDisambigNode(node2)
 			val relType = EdgeType(id = globalRelType, label = rel);
-			
+			val w1 = TextNode(id = wikiName1, text = URLDecoder.decode(node1, "UTF-8"))
+			val w2 = TextNode(id = wikiName2, text = URLDecoder.decode(node2, "UTF-8"))
 
 			
 			println(store.getOrInsert2(relType, store.idFromUsername(username)).id)
 			println(store.getOrInsert2(ng1, store.idFromUsername(username)).id)
 			println(store.getOrInsert2(ng2, store.idFromUsername(username)).id)
+        	
 
+			store.addrel(wikiRel, Array[String](ng1.id, w1.id))
+        	store.addrel(wikiRel, Array[String](ng2.id, w2.id))
 
 			//Relationship at global level
 			store.addrel2(relType.id, Array[String](ng1.id, ng2.id), store.idFromUsername(username))
 
-			//Wikipedia page
-			val wikiPageURL = lineRegex.split(resource)(0)
-			
-			if(wikiPageURL.startsWith(wikiURL)) {
-				val wikipediaPage = URLNode(ID.url_id(wikiPageURL), url = wikiPageURL, title = node1)
-			
-				println(store.getOrInsert2(wikipediaPage, store.idFromUsername(username)).id)
-				store.addrel2(wikiPageET.id, Array[String](wikipediaPage.id, ng1.id), store.idFromUsername(username))
-			}
 
 			
 			
@@ -93,6 +88,31 @@ class OutputDBWriter(storeName:String, source:String, username:String, name:Stri
 
 		}	
 	}
+
+	def removeWikiDisambig(wikiTitle: String): String = {
+      return wikiTitle.split("""\(""")(0)
+	}
+
+	def insertAndGetWikiDisambigNode(wikiTitle: String): Vertex = {
+		//Check if exists in database
+		val titleSP = removeWikiDisambig(wikiTitle);
+		
+		var i = 1;
+
+		while(store.exists(ID.text_id(titleSP, i.toString))) {
+			val existingNode = store.get(ID.text_id(titleSP, i.toString));
+			existingNode match {
+				case e: TextNode => if(e.text == URLDecoder.decode(wikiTitle, "UTF-8")) {return existingNode}
+				case _ =>
+			}
+			i += 1
+		}
+		val newNode = TextNode(id = ID.text_id(titleSP, i.toString), text=URLDecoder.decode(wikiTitle, "UTF-8"))
+		println(store.put(newNode));
+		return newNode;
+		
+	}
+
 
 	def writeGeneratorSource(sourceID:String, sourceURL:String)
   	{
