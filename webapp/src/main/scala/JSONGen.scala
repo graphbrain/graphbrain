28218ed0d3a3c235514787f5189ffa8a7591ea53
Telmo Menezes <telmo@telmomenezes.net>
2012-08-31 23:08:20 +0100

    fixing JSONGen
    Former-commit-id: 2946a6657e651910eac8447ea0ef194762869054

--- webapp/src/main/scala/JSONGen.scala
+++ webapp/src/main/scala/JSONGen.scala
@@ -14,7 +14,12 @@ object JSONGen {
           "{" + (for (e <- m) yield json(e._1) + ":" + json(e._2)).reduceLeft(_ + "," + _) + "}"
       }
       case Nil => "[]"
-      case l: Iterable[_] => "[" + (for (e <- l) yield json(e)).reduceLeft(_ + "," + _) + "]"
+      case l: Iterable[_] => {
+        if (l.size == 0)
+          "[]"
+        else
+          "[" + (for (e <- l) yield json(e)).reduceLeft(_ + "," + _) + "]"
+      }
       case a: Array[_] => json(a.toList)
       case _ => ""
     }
