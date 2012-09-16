150aee1df254f8bab6a7ca4fda563197f6537a2c
Telmo Menezes <telmo@telmomenezes.net>
2012-09-17 00:10:54 +0200

    removing obsolete functiond from hgdb.ID
    Former-commit-id: 9502fe956738b5523c828862ed1b429cc277fd6b

--- hgdb/src/main/scala/ID.scala
+++ hgdb/src/main/scala/ID.scala
@@ -65,18 +65,6 @@ object ID {
   def usergenerated_id(userName:String, thing:String) =
 	  userName + "/" + thing
 
-  def image_id(image_url:String) =
-    "image/" + sanitize(image_url)
-  
-  def image_id(image_name:String, image_url:String="") =
-	  "image/" + sanitize(image_name) + "/" + sanitize(image_url)
-
-  def video_id(video_url:String) =
-    "video/" +  sanitize(video_url)
-
-  def video_id(video_name:String, video_url:String="") =
-	  "video/" + sanitize(video_name) + "/" + sanitize(video_url)
-
   def text_id(thing:String, which:Int=1):String =
 	  which.toString + "/" + sanitize(thing)
 
