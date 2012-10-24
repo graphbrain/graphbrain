fdd6cda9044004ce257bb15e06d23f98328c4782
Telmo Menezes <telmo@telmomenezes.net>
2012-10-24 23:15:53 +0200

    adding bootstrap library and misc visual changes
    Former-commit-id: 5ef9a2e680ade5401eb503e598f70a2c9d05afd9

--- webapp/src/main/scala/NavBar.scala
+++ webapp/src/main/scala/NavBar.scala
@@ -50,9 +50,8 @@ case class NavBar(user: UserNode, page: String) {
   def tools = {
     if (page == "node") {
       """
-      <li class="divider-vertical"></li>
-      <li><button class="btn btn-inverse" id="ai-chat-button"><i class="icon-asterisk icon-white"></i> Talk to AI</button></li>
-      <li><button id="removeButton" class="btn btn-inverse" data-toggle="button"><i class="icon-remove icon-white"></i> Remove</button></li>
+      <li><button class="btn" id="ai-chat-button"><i class="icon-asterisk icon-black"></i> Talk to AI</button></li>
+      <li><button id="removeButton" class="btn" data-toggle="button"><i class="icon-remove icon-black"></i> Remove</button></li>
       """
     }
     else {
@@ -75,10 +74,9 @@ case class NavBar(user: UserNode, page: String) {
       """
       <div class="pull-right">
       <ul class="nav">
-        """ + tools + """    
-        <li class="divider-vertical"></li>
+        """ + tools + """
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-user icon-white"></i> """ + user.name + """ <b class="caret"></b></a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-user icon-black"></i> """ + user.name + """ <b class="caret"></b></a>
           <ul class="dropdown-menu">
             <li><a href="/node/user/""" + user.username +  """">Home</a></li>
             <li><a href="#" id="logoutLink">Logout</a></li>
@@ -90,6 +88,7 @@ case class NavBar(user: UserNode, page: String) {
     }
   }
 
+  // <div class="navbar navbar-fixed-top">
   def html = {
     if (page == "comingsoon") {
       ""
@@ -104,11 +103,11 @@ case class NavBar(user: UserNode, page: String) {
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </a>
-            <a style="padding:7px 20px 7px" href="/secret"><img src="/images/GB_logo_S.png" class="brand" alt="graphbrain" /></a>
+            <a href="/secret"><img src="/images/GB_logo_XS.png" class="brand" alt="graphbrain" /></a>
             <div class="nav-collapse">
               <ul class="nav">
                 <li><form class="navbar-search" id="search-field">
-                  <input type="text" id="search-input-field" class="search-query" placeholder="Search" />
+                  <input type="text" id="search-input-field" placeholder="Search" />
                 </form></li>
               </ul>
             </div>
@@ -119,7 +118,6 @@ case class NavBar(user: UserNode, page: String) {
         <div id="alert" class="alert" style="visibility:hidden; margin:0px">
           <div id="alertMsg"></div>
         </div>
-        
       </div>
       """
     }
