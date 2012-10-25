abf1c827d0933e91cb7d5e5725dafbb04cefdd6c
Telmo Menezes <telmo@telmomenezes.com>
2012-10-25 19:01:22 +0200

    making SimpleLog whiter
    Former-commit-id: 020223d254189d40eb85d8134b8e92f9611f38e7

--- utils/src/main/scala/SimpleLog.scala
+++ utils/src/main/scala/SimpleLog.scala
@@ -5,14 +5,14 @@ import scala.annotation.elidable._
 
 
 trait SimpleLog {
-    private def slog(msg: String, level: String, color: String=Console.YELLOW) = {
+    private def slog(msg: String, level: String, color: String=Console.WHITE) = {
         val caller = (new Exception).getStackTrace()(3)
         val line = Console.RED + "[" + level + "] " + Console.GREEN + caller + " " + color + msg + Console.WHITE
         Console.out.println(line) 
     }
 
-    @elidable(ALL) def ldebug(msg: String, color: String=Console.YELLOW) = slog(msg, "DEBUG", color)
-    @elidable(INFO) def linfo(msg: String, color: String=Console.YELLOW) = slog(msg, "INFO", color)
-    @elidable(WARNING) def lwarn(msg: String, color: String=Console.YELLOW) = slog(msg, "WARNING", color)
-    @elidable(SEVERE) def lerror(msg: String, color: String=Console.YELLOW) = slog(msg, "ERROR", color)
+    @elidable(ALL) def ldebug(msg: String, color: String=Console.WHITE) = slog(msg, "DEBUG", color)
+    @elidable(INFO) def linfo(msg: String, color: String=Console.WHITE) = slog(msg, "INFO", color)
+    @elidable(WARNING) def lwarn(msg: String, color: String=Console.WHITE) = slog(msg, "WARNING", color)
+    @elidable(SEVERE) def lerror(msg: String, color: String=Console.WHITE) = slog(msg, "ERROR", color)
 }
