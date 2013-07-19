18ebba735218675d7300875f53c8cb4dae30e2bc
Telmo Menezes <telmo@telmomenezes.com>
2013-07-19 12:24:26 +0200

    starting Java project
    Former-commit-id: 3a0b6d5af6b01b7a6479cf6f82e1e309422ca9ed

---
+++ src/main/java/com/graphbrain/webapp/GBHandler.java
@@ -0,0 +1,21 @@
+package com.graphbrain.webapp;
+ 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+public class GBHandler extends AbstractHandler {
+ 
+    public GBHandler() {
+    }
+ 
+    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        response.getWriter().println("<h1>Graphbrain!</h1>");
+    }
+}
