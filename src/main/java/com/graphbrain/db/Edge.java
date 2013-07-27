6f8e4ffebba0e02d46deabcc060b4ba5b588ec90
Telmo Menezes <telmo@telmomenezes.net>
2013-07-27 13:21:58 +0200

    porting gbdb to java
    Former-commit-id: ad855d434250d9bbe8ca8a8c0e0cbbf4c5d0fdac

--- src/main/java/com/graphbrain/db/Edge.java
+++ src/main/java/com/graphbrain/db/Edge.java
@@ -1,84 +1,115 @@
-package com.graphbrain.gbdb
+package com.graphbrain.db;
 
+import java.util.LinkedList;
+import java.util.List;
 
-case class Edge(edgeType: String, extParticipantIds: List[String], originalEdge: Edge = null) {
-  val participantIds = extParticipantIds.map(x => Vertex.cleanId(x))
+public class Edge {
+	
+	private String edgeType;
+	private List<String> participantIds;
+	private Edge originalEdge;
+	
+	public Edge(String edgeType, List<String> extParticipantIds, Edge originalEdge) {
+		this.edgeType = edgeType;
+		participantIds = new LinkedList<String>();
+		for (String pid : extParticipantIds) {
+			participantIds.add(Vertex.cleanId(pid));
+		}
+		this.originalEdge = originalEdge;
+	}
+	
+	public Edge(String edgeType, List<String> extParticipantIds) {
+		this(edgeType, extParticipantIds, null);
+	}
 
-  def negate = Edge("neg/" + edgeType, extParticipantIds)
+	public Edge negate() {
+		edgeType = "neg/" + edgeType;
+		return this;
+	}
 
-  def isPositive = ID.parts(edgeType)(0) != "neg"
+	public Edge fromString(String edgeString) {
+	    String[] tokens = edgeString.split(" ");
+	    edgeType = tokens[0];
+	    
+	    participantIds = new LinkedList<String>();
+	    for (int i = 1; i < tokens.length; i++) {
+	    	participantIds.add(tokens[i]);
+	    }
+	    return this;
+	}
+	
+	public boolean isPositive() {
+		return ID.parts(edgeType)[0] != "neg";
+	}
 
-  def isGlobal: Boolean = {
-    for (p <- participantIds)
-      if (!ID.isUserNode(p) && ID.isInUserSpace(p))
-        return false
+	public boolean isGlobal() {
+		for (String p : participantIds) {
+			if (!ID.isUserNode(p) && ID.isInUserSpace(p)) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-    true
-  }
+	public boolean isInUserSpace() {
+		for (String p : participantIds) {
+			if (ID.isInUserSpace(p)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-  def isInUserSpace: Boolean = {
-    for (p <- participantIds)
-      if (ID.isInUserSpace(p))
-        return true
+  	public boolean isInContextSpace() {
+  		for (String p : participantIds) {
+  			if (!ID.isInContextSpace(p)) {
+  				return false;
+  			}
+  		}
+  		return true;
+  	}
 
-    false
-  }
-
-  def isInContextSpace: Boolean = {
-    for (p <- participantIds)
-      if (!ID.isInContextSpace(p))
-        return false
-
-    true
-  }
-
-  def toUser(userId: String) = {
-    val pids = for (pid <- extParticipantIds) yield ID.globalToUser(pid, userId)
-    Edge(edgeType, pids)
-  }
+  	public Edge makeUser(String userId) {
+  		List<String> pids = new LinkedList<String>();
+  		for (String pid : participantIds) {
+  			pids.add(ID.globalToUser(pid, userId));
+  		}
+  		participantIds = pids;
+  		return this;
+  	}
   
-  def toGlobal = {
-    val pids = for (pid <- extParticipantIds) yield ID.userToGlobal(pid)
-    Edge(edgeType, pids)
-  }
+  	public Edge makeGlobal() {
+  		List<String> pids = new LinkedList<String>();
+  		for (String pid : participantIds) {
+  			pids.add(ID.userToGlobal(pid));
+  		}
+  		participantIds = pids;
+  		return this;
+  	}
 
-  def getOriginalEdge = if (originalEdge == null) this else originalEdge
+  	public Edge getOriginalEdge() {
+  		if (originalEdge == null) {
+  			return this;
+  		}
+  		else {
+  			return originalEdge;
+  		}
+  	}
 
-  override def toString = edgeType + " " + participantIds.reduceLeft(_ + " " + _)
+  	@Override
+  	public String toString() {
+  		StringBuilder sb = new StringBuilder(100);
+  		sb.append(edgeType);
+  		for (String p : participantIds) {
+  			sb.append(" ");
+  			sb.append(p);
+  		}
+  		return sb.toString();
+  	}
 
-  def humanReadable2 = (ID.humanReadable(participantIds(0)) +
-                        " [" +  ID.humanReadable(edgeType) + "] " +
-                        ID.humanReadable(participantIds(1))).replace(",", "")
-}
-
-object Edge {
-  def fromString(edgeString: String) = {
-    val tokens = edgeString.split(" ").toList
-    Edge(tokens.head, tokens.tail)
-  }
-
-  def fromEdgeEntry(nodeId: String, edgeType: String, position: Int, node1: String, node2: String, nodeN: String) = {
-    val preParticipantIds = if (nodeN != null) {
-        List[String](node1, node2) ::: nodeN.split(" ").toList
-      }
-      else if (node2 != null) {
-        List[String](node1, node2)
-      }
-      else {
-        List[String](node1)
-      }
-
-    val participantIds = if (position == 0) {
-        nodeId :: preParticipantIds
-      }
-      else if (position >= preParticipantIds.length) {
-        preParticipantIds :+ nodeId
-      }
-      else {
-        val parts = preParticipantIds.splitAt(position)
-        parts._1 ::: nodeId :: parts._2
-      }
-
-    Edge(edgeType, participantIds)
-  }
+  	public String humanReadable2() {
+  		return (ID.humanReadable(participantIds.get(0))
+  				+ " [" +  ID.humanReadable(edgeType) + "] "
+  				+ ID.humanReadable(participantIds.get(1))).replace(",", "");
+  	}
 }
