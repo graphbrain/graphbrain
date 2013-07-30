d539577292c061b8dce8ec015cc2fbe4d2aa2461
Telmo Menezes <telmo@telmomenezes.com>
2013-07-30 12:01:21 +0200

    working on permutations system for edges
    Former-commit-id: a0732242e7e45cbb66b24b65b77273feb8e37d2c

--- src/main/java/com/graphbrain/db/Graph.java
+++ src/main/java/com/graphbrain/db/Graph.java
@@ -1,8 +1,10 @@
 package com.graphbrain.db;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-public abstract class Graph {
+public class Graph {
 
 	private Backend back;
 	
@@ -126,6 +128,36 @@ public abstract class Graph {
 	public List<Vertex> allUsers() {
 		return back.listByType(VertexType.User);
 	}
+	
+	public Set<Edge> edges(Vertex center) {
+		return back.edges(center);
+	}
+	
+	public Set<Edge> edges(String centerId) {
+		Vertex center = get(centerId);
+		return edges(center);
+	}
+	
+	public Set<String> nodesFromEdgeSet(Set<Edge> edgeSet) {
+		Set<String> nset = new HashSet<String>();
+
+		for (Edge e : edgeSet) {
+			for (String pid : e.getIds()) {
+				nset.add(pid);
+		    }
+		}
+		
+		return nset;
+	}
+
+	public Set<String> neighbors(String centerId) {
+		//ldebug("neighbors " + nodeId)
+
+		Set<Edge> nedges = edges(centerId);
+		Set<String> nodes = nodesFromEdgeSet(nedges);
+		nodes.add(centerId);
+		return nodes;
+	}
 
 /*
 
@@ -165,132 +197,6 @@ public abstract class Graph {
 
 
   def delrel(edgeType: String, participants: List[String]): Unit = delrel(Edge(edgeType, participants))
-
-
-  def neighborEdges(nodeId: String, edgeType: String = "", relPos: Integer = -1): Set[Edge] = {
-    //ldebug("neighborEdges " + nodeId + "; edgeType: " + edgeType + "; pos: " + relPos)
-
-    try {
-      val eset = MSet[Edge]()
-
-      val query = HFactory.createSliceQuery(backend.ksp, StringSerializer.get(),
-                                                      new CompositeSerializer(), StringSerializer.get())
-      
-      query.setKey(nodeId)
-      query.setColumnFamily("edges")
-
-      val minPos: java.lang.Integer = Integer.MIN_VALUE
-      val maxPos: java.lang.Integer = Integer.MAX_VALUE
-
-      val minEdgeType = if (edgeType == "") String.valueOf(Character.MIN_VALUE) else edgeType
-      val maxEdgeType = if (edgeType == "") String.valueOf(Character.MAX_VALUE) else edgeType
-
-      val start = new Composite()
-      start.addComponent(minEdgeType, StringSerializer.get())
-      start.addComponent(minPos, IntegerSerializer.get())
-      start.addComponent(String.valueOf(Character.MIN_VALUE), StringSerializer.get())
-      start.addComponent(String.valueOf(Character.MIN_VALUE), StringSerializer.get())
-      start.addComponent(String.valueOf(Character.MIN_VALUE), StringSerializer.get())
-        
-      val finish = new Composite()
-      finish.addComponent(maxEdgeType, StringSerializer.get())
-      finish.addComponent(maxPos, IntegerSerializer.get())
-      finish.addComponent(String.valueOf(Character.MAX_VALUE), StringSerializer.get())
-      finish.addComponent(String.valueOf(Character.MAX_VALUE), StringSerializer.get())
-      finish.addComponent(String.valueOf(Character.MAX_VALUE), StringSerializer.get())
-
-      val it = new ColumnSliceIterator[String, Composite, String](query, start, finish, false)
-      while (it.hasNext()) {
-        val column = it.next()
-        val pos: Integer = column.getName().get(1, IntegerSerializer.get())
-
-        if ((relPos < 0) || (relPos != pos)) {
-          val edgeType = column.getName().get(0, StringSerializer.get())
-          val node1 = column.getName().get(2, StringSerializer.get())
-          val node2 = column.getName().get(3, StringSerializer.get())
-          val nodeN = column.getName().get(4, StringSerializer.get())
-          val edge = Edge.fromEdgeEntry(nodeId, edgeType, pos, node1, node2, nodeN)
-          eset += edge
-        }
-      }
-
-      eset.toSet
-    }
-    catch {
-      case e => Set[Edge]()
-    }
-  }
-
-
-  def nodesFromEdgeSet(edgeSet: Set[Edge]): Set[String] = {
-    val nset = MSet[String]()
-
-    for (e <- edgeSet) {
-      for (pid <- e.participantIds)
-        nset += pid
-    }
-
-    nset.toSet
-  }
-
-
-  def neighbors(nodeId: String): Set[String] = {
-    ldebug("neighbors " + nodeId)
-
-    val nedges = neighborEdges(nodeId)
-    nodesFromEdgeSet(nedges) + nodeId
-  }
-
-
-  private def edgeEntryKey(nodeId: String, edge: Edge) = {
-    ldebug("edgeEntryKey nodeId: " + nodeId + "; edge: " + edge)
-
-    val pos: java.lang.Integer = edge.participantIds.indexOf(nodeId)    
-    // TODO: throw exception if pos == -1
-
-    val participants = edge.participantIds.filterNot(x => x == nodeId)
-    val numberOfParticipants = participants.length
-
-    val node2 = if (numberOfParticipants > 1) participants(1) else ""
-
-    val extraNodes =
-      if (numberOfParticipants > 2)
-        participants.slice(2, numberOfParticipants).reduceLeft(_ + " " + _)
-      else
-        ""
-
-    val c = new Composite()
-    c.addComponent(edge.edgeType, StringSerializer.get())
-    c.addComponent(pos, IntegerSerializer.get())
-    c.addComponent(participants(0), StringSerializer.get())
-    c.addComponent(node2, StringSerializer.get())
-    c.addComponent(extraNodes, StringSerializer.get())
-
-    c
-  }
-
-
-  private def addEdgeEntry(nodeId: String, edge: Edge) = {
-    ldebug("addEdgeEntry nodeId: " + nodeId + "; edge: " + edge)
-
-    val colKey = edgeEntryKey(nodeId, edge)
-
-    val col = HFactory.createColumn(colKey, "", new CompositeSerializer(), StringSerializer.get())
-    val mutator = HFactory.createMutator(backend.ksp, StringSerializer.get())
-    mutator.addInsertion(nodeId, "edges", col)
-    mutator.execute()
-  }
-
-
-  private def delEdgeEntry(nodeId: String, edge: Edge) = {
-    ldebug("delEdgeEntry nodeId: " + nodeId + "; edge: " + edge)
-
-    val colKey = edgeEntryKey(nodeId, edge)
-
-    val mutator = HFactory.createMutator(backend.ksp, StringSerializer.get())
-    mutator.addDeletion(nodeId, "edges", colKey, new CompositeSerializer())
-    mutator.execute()
-  }
 
 
   private def incVertexEdgeType(nodeId: String, edgeType: String, position: Int) = {
@@ -388,45 +294,6 @@ public abstract class Graph {
     mutator.execute()
   }
 
-
-  def relExistsOnVertex(id: String, edge: Edge): Boolean = {
-    ldebug("relExistsOnVertex id: " + id + "; edge: " + edge)
-
-    val ckey = edgeEntryKey(id, edge)
-    val res = backend.tpEdges.queryColumns(id, Arrays.asList(ckey))
-    res.hasResults()
-  }
-
-  
-  def relExists(edge: Edge): Boolean = {
-    ldebug("relExists: " + edge)
-
-    edge.participantIds.exists(relExistsOnVertex(_, edge))
-  }
-
-
-  def getOrNull(id: String): Vertex = {
-    ldebug("getOrNull: " + id)
-
-    try {
-      get(id)
-    }
-    catch {
-      case e: KeyNotFound => {
-        ldebug("KeyNotFound " + id)
-        null
-      }
-    }
-  }
-
-  def getSourceNode(id: String): SourceNode = {
-  	ldebug("getSourceNode: " + id)
-    get(id) match {
-  		case x: SourceNode => x
-  		case v: Vertex => throw WrongVertexType("on vertex: " + id + " (expected SourceNode)")
-  	}
-  }
-
   def createAndConnectVertices(edgeType: String, participants: Array[Vertex]) = {
     ldebug("createAndConnectVertices edgeType: " + edgeType + "; participants: " + participants)
     for (v <- participants) {
@@ -438,35 +305,5 @@ public abstract class Graph {
     val ids = for (v <- participants) yield v.id
     addrel(edgeType.replace(" ", "_"), ids.toList)
   }
-
-  def createTextNode(namespace: String="", text: String="", summary: String="") = TextNode(this, namespace, text, summary)
-
-  def createEdgeType(id: String="", label: String="") = EdgeType(this, id, label)
-
-  def createSourceNode(id: String="") = SourceNode(this, id)
-
-  def createURLNode(url: String="", userId: String = "", title: String="") = URLNode(this, url, userId, title)
-
-  def createUserNode(id: String="", username: String="", name: String="",
-  email: String="", pwdhash: String="", role: String="", session: String="",
-  sessionTs: Long= -1, lastSeen: Long= -1, contexts: List[ContextNode]=null, summary: String="") = UserNode(this, id, username, name, email, pwdhash, role, session, sessionTs, lastSeen, contexts, summary)
-
-  def createRuleNode(store: VertexStore, id: String="", rule: String="") = RuleNode(this, id, rule)
   */
 }
-
-
-/*
-object VertexStore {
-  def apply(clusterName: String, keyspaceName: String) = new VertexStore(clusterName, keyspaceName)
-
-  def main(args : Array[String]) : Unit = {
-    val store = new VertexStore()
-
-    val edges = store.neighborEdges("user/telmo_menezes")
-    for (e <- edges) println(e)
-
-    var snode = store.getUserNode("user/telmo_menezes").updateSummary
-    println(snode.summary)
-  }
-}*/
