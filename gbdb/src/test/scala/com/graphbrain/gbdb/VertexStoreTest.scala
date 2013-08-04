06d431223df36c98e14dc6a60c8d0727fe465fc0
Telmo Menezes <telmo@telmomenezes.com>
2013-08-04 14:40:25 +0100

    added leveldb.py
    Former-commit-id: 4da5e5b1073dff3df881a607cf6d08076691d3af

--- gbdb/src/test/scala/com/graphbrain/gbdb/VertexStoreTest.scala
+++
@@ -1,9 +0,0 @@
-import org.scalatest.FunSuite
-import com.graphbrain.gbdb.VertexStore
-
-class VertexStoreTest extends FunSuite with BaseVertexStoreTests{
-  val store = new VertexStore("gb", "testhgdb")
-  store.simpleLogOn = false
-
-  testsFor(baseTests(store, "VertexStore"))
-}
