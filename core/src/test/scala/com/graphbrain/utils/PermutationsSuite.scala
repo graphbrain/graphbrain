99848d39fe63ec50b90a05e9883b194a818ea6fa
Telmo Menezes <telmo@telmomenezes.com>
2013-08-11 17:56:17 +0100

    setting up tests
    Former-commit-id: 6063085dee11bbe0c0217e6875e77cbfc243eea9

---
+++ core/src/test/scala/com/graphbrain/utils/PermutationsSuite.scala
@@ -0,0 +1,13 @@
+package com.graphbrain.utils
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+
+@RunWith(classOf[JUnitRunner])
+class PermutationsSuite extends FunSuite {
+
+  test("testingtests") {
+    assert(1 === 1)
+  }
+}
