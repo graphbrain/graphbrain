72eb29c56d1e29febde341e31559d13e3935df79
Telmo Menezes <telmo@telmomenezes.com>
2013-08-11 03:09:03 +0100

    working on core
    Former-commit-id: ef7a4f5d4f9b92be9d38e85cc4209a3be7fc2d79

--- core/src/main/scala/com/graphbrain/db/Consensus.scala
+++ core/src/main/scala/com/graphbrain/db/Consensus.scala
@@ -2,7 +2,7 @@ package com.graphbrain.db
 
 
 object Consensus {
-  def evalEdge(edge: Edge, store: UserOps) = {
+  def evalEdge(edge: Edge, graph: UserOps) = {
     if (edge.isGlobal) {
       var score = 0
 
@@ -11,7 +11,7 @@ object Consensus {
 
       // go through all the user space alt version of the first version in this edge
       // TODO: select edge participant with the lowest degree?
-      val altVertices = store.globalAlts(pids(0))
+      val altVertices = graph.globalAlts(pids(0))
       for (altV <- altVertices) {
         val userId = ID.ownerId(altV)
 
@@ -20,20 +20,20 @@ object Consensus {
         val negUserEdge = negEdge.toUser(userId)
 
         // update score based on postive and negative user space edges
-        if (store.relExists(userEdge)) {
+        if (graph.exists(userEdge)) {
           score += 1
         }
-        if (store.relExists(negUserEdge)) {
+        if (graph.exists(negUserEdge)) {
           score -= 1
         }
       }
 
       // add or delete edge based on consensus score
       if (score > 0) {
-        store.addrel(edge)
+        graph.put(edge)
       }
       else {
-        store.delrel(edge)
+        graph.remove(edge)
       }
     }
   }
