f9f8f41202f348ad3023ff71416a4f788f1d2901
Telmo Menezes <telmo@telmomenezes.com>
2013-09-02 15:02:30 +0200

    URLNode title and icon
    Former-commit-id: ceb8e65fe9f78b365db8d5c074319582e905f3c2

--- core/src/main/scala/com/graphbrain/db/URLNode.scala
+++ core/src/main/scala/com/graphbrain/db/URLNode.scala
@@ -30,62 +30,6 @@ case class URLNode(override val id: String,
 
   override def setTs(newTs: Long): Vertex = copy(ts=newTs)
 
-  private def exists(urlName: String): Boolean = {
-    try {
-      HttpURLConnection.setFollowRedirects(false)
-      // note : you may also need
-      //        HttpURLConnection.setInstanceFollowRedirects(false)
-      val con = new URL(urlName).openConnection().asInstanceOf[HttpURLConnection]
-      con.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2")
-      con.setRequestMethod("HEAD")
-      con.getResponseCode == HttpURLConnection.HTTP_OK
-    }
-    catch {
-       case _: Throwable => false
-    }
-  }
-
-  private def getDomainName = {
-    val uri = new URI(url)
-    uri.getHost
-  }
-
-  private def getTitleAndIcon: (String, String) = {
-    ldebug("getTitleAndIcon " + url)
-
-    try {
-      val doc = Jsoup.connect(url).header("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2").get()
-    
-      val title = doc.title()
-      ldebug("title: " + title)
-    
-      val links = doc.select("link[rel=shortcut icon]")
-      val link = links.first()
-      val icon = if (link != null) {
-        val icoUrl = link.attr("abs:href")
-        icoUrl
-      }
-      else {
-        val icoUrl = "http://" + getDomainName + "/favicon.ico"
-        ldebug("default icon url: " + icoUrl)
-        if (exists(icoUrl))
-          icoUrl
-        else
-          ""
-      }
-
-      ldebug("icon: " + icon)
-
-      (title, icon)
-    }
-    catch {
-      case e: Exception => {
-        ldebug(e.toString)
-        ("", "")
-      }
-    }
-  }
-
   override def raw: String = {
     "type: " + "url<br />" +
     "url: " + url + "<br />" +
@@ -97,13 +41,63 @@ case class URLNode(override val id: String,
 
 
 object URLNode {
-  def fromUrl(url: String, userId: String = "", title: String="", icon: String="") = {
+  def fromUrl(url: String, userId: String = "") = {
     val auxId = urlToId(url)
     val id = if (userId == "") auxId else ID.globalToUser(auxId, userId)
-    URLNode(id, title, icon)
+    val titleAndIcon = getTitleAndIcon(url)
+    URLNode(id, titleAndIcon._1, titleAndIcon._2)
   }
 
   def idToUrl(id: String) = URLDecoder.decode(ID.lastPart(id), "UTF-8")
 
   def urlToId(url: String) = "url/" + URLEncoder.encode(url, "UTF-8")
+
+  private def getTitleAndIcon(url: String): (String, String) = {
+    try {
+      val doc = Jsoup.connect(url).header("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2").get()
+
+      val title = doc.title()
+
+      val links = doc.select("link[rel=shortcut icon]")
+      val link = links.first()
+      val icon = if (link != null) {
+        val icoUrl = link.attr("abs:href")
+        icoUrl
+      }
+      else {
+        val icoUrl = "http://" + getDomainName(url) + "/favicon.ico"
+        if (exists(icoUrl))
+          icoUrl
+        else
+          ""
+      }
+
+      (title, icon)
+    }
+    catch {
+      case e: Exception => {
+        ("", "")
+      }
+    }
+  }
+
+  private def getDomainName(url: String) = {
+    val uri = new URI(url)
+    uri.getHost
+  }
+
+  private def exists(urlName: String): Boolean = {
+    try {
+      HttpURLConnection.setFollowRedirects(false)
+      // note : you may also need
+      //        HttpURLConnection.setInstanceFollowRedirects(false)
+      val con = new URL(urlName).openConnection().asInstanceOf[HttpURLConnection]
+      con.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2")
+      con.setRequestMethod("HEAD")
+      con.getResponseCode == HttpURLConnection.HTTP_OK
+    }
+    catch {
+      case _: Throwable => false
+    }
+  }
 }
