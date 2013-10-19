c9ded94fe7b1167dc5338ab35147f6d648d3828f
Telmo Menezes <telmo@telmomenezes.net>
2013-10-19 15:01:54 +0200

    PTrees
    Former-commit-id: c0a2c7712f468139189e30bb934cd6a860af77fe

---
+++ core/src/main/scala/com/graphbrain/eco/nodes/TreeRule.scala
@@ -0,0 +1,24 @@
+package com.graphbrain.eco.nodes
+
+import com.graphbrain.eco.{Context, Contexts, NodeType}
+
+class TreeRule(params: Array[ProgNode], lastTokenPos: Int= -1) extends FunNode(params, lastTokenPos) {
+  override val label = "tree"
+
+  override def ntype = {
+    params(0).ntype match {
+      case NodeType.Boolean => NodeType.Unknown
+      case _ => {
+        typeError()
+        NodeType.Unknown
+      }
+    }
+  }
+
+  override def booleanValue(ctxts: Contexts, ctxt: Context) = {
+    // incomplete
+    params(1).booleanValue(ctxts, ctxt)
+  }
+
+  override protected def typeError() = error("the first part of a rule must be a boolean")
+}
--- core/src/main/scala/com/graphbrain/eco/nodes/VarNode.scala
+++ core/src/main/scala/com/graphbrain/eco/nodes/VarNode.scala
@@ -1,12 +1,12 @@
 package com.graphbrain.eco.nodes
 
-import com.graphbrain.eco.{Phrase, Context, Contexts}
+import com.graphbrain.eco.{PTree, Context, Contexts}
 
 abstract class VarNode(val name: String, lastTokenPos: Int) extends ProgNode(lastTokenPos) {
   override def stringValue(ctxts: Contexts, ctxt: Context): String = ctxt.getString(name)
   override def numberValue(ctxts: Contexts, ctxt: Context): Double = ctxt.getNumber(name)
   override def booleanValue(ctxts: Contexts, ctxt: Context): Boolean = ctxt.getBoolean(name)
-  override def phraseValue(ctxts: Contexts, ctxt: Context): Phrase = ctxt.getPhrase(name)
+  override def phraseValue(ctxts: Contexts, ctxt: Context): PTree = ctxt.getPhrase(name)
 
   override def toString = name
 }
