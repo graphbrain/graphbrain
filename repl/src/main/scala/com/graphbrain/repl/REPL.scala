ec59509341861945636ffad38ecb9da4a9823ff0
Telmo Menezes <telmo@telmomenezes.com>
2013-10-22 16:23:00 +0200

    working on REPL
    Former-commit-id: d5b7de6de80c4a32479ee7176d4fd67cb35d8691

--- repl/src/main/scala/com/graphbrain/repl/REPL.scala
+++ repl/src/main/scala/com/graphbrain/repl/REPL.scala
@@ -1,7 +1,26 @@
 package com.graphbrain.repl
 
+import com.graphbrain.eco.Parser
+import com.graphbrain.eco.nodes.DummyFun
+
 object REPL {
   def main(args: Array[String]) = {
-    println("Hello REPL!")
+    println("Welcome to the Eco REPL.")
+
+    for(ln <- io.Source.stdin.getLines()) {
+      val p = new Parser(ln)
+
+      p.prog.root match {
+        case d: DummyFun => {
+          d.name match {
+            case "load" => println("loading " + d.params(0).stringValue(null, null))
+            case "exit" => {
+              println("bye.")
+              sys.exit(0)
+            }
+          }
+        }
+      }
+    }
   }
 }
