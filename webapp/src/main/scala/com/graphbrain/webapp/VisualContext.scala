a7f8a9e9e44650b7e333c62f9f0b1291eaf5cc28
Telmo Menezes <telmo@telmomenezes.com>
2013-11-12 14:01:48 +0100

    moving parse rendering to template
    Former-commit-id: 558ca6d8d3a155353b3c9d3e716d6b12d919704e

---
+++ webapp/src/main/scala/com/graphbrain/webapp/VisualContext.scala
@@ -0,0 +1,29 @@
+package com.graphbrain.webapp
+
+import com.graphbrain.eco.{Context, Words, Word}
+
+class VisualContext(ctxt: Context){
+  val htmlWords = renderWords(ctxt.parent.sentence)
+  val htmlContext = renderContext(ctxt)
+  val htmlVertex = ctxt.getTopRetVertex
+
+  private def renderContext(ctxt: Context, indent: Int = 0): String = {
+    var r = ""
+
+    (0 until indent).foreach(x => r += "&nbsp;&nbsp;&nbsp;&nbsp;")
+    r += "<span class=\"text-success\">" + ctxt.parent.rule.name + "</span>"
+    r += " (" + renderWords(ctxt.parent.sentence) + ") <br />"
+
+    for (sctxt <- ctxt.subContexts)
+      r += renderContext(sctxt, indent + 1)
+
+    r
+  }
+
+  private def renderWord(word: Word) =
+    word.word + " <span class=\"text-primary\">[" + word.pos + "]</span>"
+
+  private def renderWords(words: Words) =
+    words.words.map(w => renderWord(w))
+      .reduceLeft(_ + " " + _)
+}
