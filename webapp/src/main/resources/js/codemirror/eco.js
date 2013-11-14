8166b41d9120ff59d2799d2660e34be340853aac
Telmo Menezes <telmo@telmomenezes.net>
2013-11-14 16:25:37 +0100

    eco syntax highlighting
    Former-commit-id: fd7f8c7b190a8f7cdc64e2abdff4a5a8040540ad

--- webapp/src/main/resources/js/codemirror/eco.js
+++ webapp/src/main/resources/js/codemirror/eco.js
@@ -1,7 +1,7 @@
 /**
- * Author: Koh Zi Han, based on implementation by Koh Zi Chun
+ * Author: Telmo Menezes, based on implementation by Koh Zi Han and Koh Zi Chun
  */
-CodeMirror.defineMode("scheme", function () {
+CodeMirror.defineMode("eco", function () {
     var BUILTIN = "builtin", COMMENT = "comment", STRING = "string",
         ATOM = "atom", NUMBER = "number", BRACKET = "bracket";
     var INDENT_WORD_SKIP = 2;
@@ -12,8 +12,9 @@ CodeMirror.defineMode("scheme", function () {
         return obj;
     }
 
-    var keywords = makeKeywords("λ case-lambda call/cc class define-class exit-handler field import inherit init-field interface let*-values let-values let/ec mixin opt-lambda override protect provide public rename require require-for-syntax syntax syntax-case syntax-error unit/sig unless when with-syntax and begin call-with-current-continuation call-with-input-file call-with-output-file case cond define define-syntax delay do dynamic-wind else for-each if lambda let let* let-syntax letrec letrec-syntax map or syntax-rules abs acos angle append apply asin assoc assq assv atan boolean? caar cadr call-with-input-file call-with-output-file call-with-values car cdddar cddddr cdr ceiling char->integer char-alphabetic? char-ci<=? char-ci<? char-ci=? char-ci>=? char-ci>? char-downcase char-lower-case? char-numeric? char-ready? char-upcase char-upper-case? char-whitespace? char<=? char<? char=? char>=? char>? char? close-input-port close-output-port complex? cons cos current-input-port current-output-port denominator display eof-object? eq? equal? eqv? eval even? exact->inexact exact? exp expt #f floor force gcd imag-part inexact->exact inexact? input-port? integer->char integer? interaction-environment lcm length list list->string list->vector list-ref list-tail list? load log magnitude make-polar make-rectangular make-string make-vector max member memq memv min modulo negative? newline not null-environment null? number->string number? numerator odd? open-input-file open-output-file output-port? pair? peek-char port? positive? procedure? quasiquote quote quotient rational? rationalize read read-char real-part real? remainder reverse round scheme-report-environment set! set-car! set-cdr! sin sqrt string string->list string->number string->symbol string-append string-ci<=? string-ci<? string-ci=? string-ci>=? string-ci>? string-copy string-fill! string-length string-ref string-set! string<=? string<? string=? string>=? string>? string? substring symbol->string symbol? #t tan transcript-off transcript-on truncate values vector vector->list vector-fill! vector-length vector-ref vector-set! with-input-from-file with-output-to-file write write-char zero?");
-    var indentKeys = makeKeywords("define let letrec let* lambda");
+    var specialKeywords = makeKeywords("wv ww :wv :ww");
+    var keywords = makeKeywords("pat let ! build max-depth are-pos-pre filter-max contains-pos-pre txt-vert rel-vert len");
+    var indentKeys = makeKeywords("");
 
     function stateStack(indent, type, prev) { // represents a state stack object
         this.indent = indent;
@@ -214,7 +215,11 @@ CodeMirror.defineMode("scheme", function () {
 
                         if (keywords && keywords.propertyIsEnumerable(stream.current())) {
                             returnType = BUILTIN;
-                        } else returnType = "variable";
+                        }
+                        else if (specialKeywords && specialKeywords.propertyIsEnumerable(stream.current())) {
+                            returnType = NUMBER;
+                        }
+                        else returnType = "variable";
                     }
             }
             return (typeof state.sExprComment == "number") ? COMMENT : returnType;
@@ -229,4 +234,4 @@ CodeMirror.defineMode("scheme", function () {
     };
 });
 
-CodeMirror.defineMIME("text/x-scheme", "scheme");
+CodeMirror.defineMIME("text/x-eco", "eco");
