02e830c60d1314ba4e3db841febb379335de57f3
Telmo Menezes <telmo@telmomenezes.com>
2013-11-15 18:53:16 +0100

    words pos
    Former-commit-id: f4cbe687ed1221f229a73f530054d9ffb8ebf4e0

---
+++ core/src/main/scala/com/graphbrain/eco/nodes/PosFun.scala
@@ -0,0 +1,19 @@
+package com.graphbrain.eco.nodes
+
+import com.graphbrain.eco.NodeType.NodeType
+import com.graphbrain.eco.{Contexts, NodeType}
+
+class PosFun(params: Array[ProgNode], lastTokenPos: Int= -1) extends FunNode(params, lastTokenPos) {
+  override val label = "pos"
+
+  override def ntype: NodeType = NodeType.Number
+
+  override def numberValue(ctxts: Contexts) = {
+    val p = params(0)
+    p.wordsValue(ctxts)
+    for (c <- ctxts.ctxts) {
+      val pos = c.getRetWords(p).pos
+      c.setRetNumber(this, pos)
+    }
+  }
+}
