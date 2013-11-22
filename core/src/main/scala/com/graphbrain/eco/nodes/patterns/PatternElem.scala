aea8200565dd8f76b287a7ea160c632973b898ec
Telmo Menezes <telmo@telmomenezes.com>
2013-11-22 20:05:27 +0100

    working on patterns
    Former-commit-id: 14319a581ae7baa3856b5f6d8acd49a270ab2910

--- core/src/main/scala/com/graphbrain/eco/nodes/patterns/PatternElem.scala
+++ core/src/main/scala/com/graphbrain/eco/nodes/patterns/PatternElem.scala
@@ -1,10 +1,70 @@
 package com.graphbrain.eco.nodes.patterns
 
-abstract class PatternElem(val patt: Pattern) {
+import com.graphbrain.eco.Words
+
+abstract class PatternElem {
+  var elemPos: Int = -1
+  var elemCount: Int = -1
+
+  var fixed = false
+  var sentence: Words = null
+
+  var prevElem: PatternElem = null
+  var nextElem: PatternElem = null
+
+  var start: Int = -1
+  var end: Int = -1
+
   var startMin: Int = -1
   var startMax: Int = -1
   var endMin: Int = -1
   var endMax: Int = -1
 
-  def updateWithPrevious(prev: PatternElem)
+  def init(elemPos: Int, elemCount: Int, prev: PatternElem, next: PatternElem) = {
+    this.elemPos = elemPos
+    this.elemCount = elemCount
+    this.prevElem = prev
+    this.nextElem = next
+  }
+
+  protected def onSetSentence()
+
+  def resetSentence() = sentence = null
+
+  private def setSentence(sentence: Words) = {
+    this.sentence = sentence
+    onSetSentence()
+  }
+
+  def rewind() =
+    start = -1
+
+  def next(sentence: Words): Boolean = {
+    if (this.sentence == null)
+      setSentence(sentence)
+
+    onNext()
+  }
+
+  def onNext(): Boolean
+
+  def curStartMin = if ((prevElem != null) && prevElem.fixed)
+    prevElem.end + 1
+  else
+    startMin
+
+  def curStartMax = if ((prevElem != null) && prevElem.fixed)
+    prevElem.end + 1
+  else
+    startMax
+
+  def curEndMin = if ((nextElem != null) && nextElem.fixed)
+    nextElem.start - 1
+  else
+    endMin
+
+  def curEndMax = if ((nextElem != null) && nextElem.fixed)
+    nextElem.start - 1
+  else
+    endMax
 }
--- core/src/main/scala/com/graphbrain/eco/nodes/patterns/StrPatternElem.scala
+++ core/src/main/scala/com/graphbrain/eco/nodes/patterns/StrPatternElem.scala
@@ -1,19 +1,50 @@
 package com.graphbrain.eco.nodes.patterns
 
-class StrPatternElem(patt: Pattern) extends PatternElem(patt) {
+class StrPatternElem(val str: String)
+  extends PatternElem {
 
-  override def updateWithPrevious(prev: PatternElem) = {
-    if (prev == null) {
+  override protected def onSetSentence() = {
+    if (prevElem == null) {
       startMin = 0
       startMax = 0
-      endMin = 0
-      endMax = 0
     }
     else {
-      startMin = prev.endMin + 1
-      startMax = prev.endMax + 1
-      endMin = startMin
-      endMax = startMax
+      startMin = prevElem.endMin + 1
+      startMax = prevElem.endMax + 1
     }
+
+    endMin = startMin
+    endMax = startMax
   }
+
+  private def step: Boolean = if (start < 0) {
+    start = Math.min(curStartMax, curEndMin)
+    true
+  }
+  else {
+    start += 1
+    start > curEndMax
+  }
+
+  override def onNext(): Boolean = {
+    // check if gap to fill is larger than one word
+    if (curStartMax < curEndMin)
+      return false
+
+    var found = false
+
+    while(!found) {
+      if (!step)
+        return false
+
+      if (sentence.words(start).word == str)
+        found = true
+    }
+
+    end = start
+    true
+  }
+
+  override def toString =
+    "\"" + str + "\"" + " = '" + sentence.slice(start, end) + "'"
 }
