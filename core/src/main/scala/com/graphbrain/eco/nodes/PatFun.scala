10d944c42c94b6b98efd053a0c85f1d07d99699a
Telmo Menezes <telmo@telmomenezes.net>
2013-11-23 19:28:18 +0100

    new pattern mathcing engine
    Former-commit-id: 7fc2cce221a1dee6fb3cbec64600d2b54a59c530

--- core/src/main/scala/com/graphbrain/eco/nodes/PatFun.scala
+++ core/src/main/scala/com/graphbrain/eco/nodes/PatFun.scala
@@ -1,41 +1,30 @@
 package com.graphbrain.eco.nodes
 
 import com.graphbrain.eco.NodeType.NodeType
-import com.graphbrain.eco.{Words, Context, Contexts, NodeType}
+import com.graphbrain.eco.{Context, Contexts, NodeType}
+import com.graphbrain.eco.nodes.patterns.{VarPatternElem, StrPatternElem}
 
 class PatFun(params: Array[ProgNode], lastTokenPos: Int= -1) extends FunNode(params, lastTokenPos) {
   override val label = "pat"
 
   override def ntype: NodeType = NodeType.Boolean
 
-  private def stepPointers(pointers: Array[Int], words: Int): Boolean = {
-    val count = pointers.length
+  val elems = params.map(f = {
+    case s: StringNode => new StrPatternElem(s.value)
+    case v: VarNode => new VarPatternElem(v.name, v.possiblePOS, v.necessaryPOS, v.forbiddenPOS)
+    case _ => null // error
+  })
 
-    if (pointers(0) < 0) {
-      for (i <- 0 until count) pointers(i) = i
-    }
-    else {
-      if (count == 1) return false
-
-      var curPointer = count - 1
-      pointers(curPointer) += 1
-
-      while (pointers(curPointer) >= words - count + curPointer + 1) {
-        curPointer -= 1
-
-        if (curPointer < 1) return false
-
-        pointers(curPointer) += 1
-        var pos = pointers(curPointer)
-        for (i <- (curPointer + 1) until count) {
-          pos += 1
-          pointers(i) = pos
-        }
-      }
-    }
-
-    true
+  // init elements
+  for (i <- 0 until elems.length) {
+    val prev = if (i == 0) null else elems(i - 1)
+    val next = if (i == elems.length - 1) null else elems(i + 1)
+    elems(i).init(i, elems.length, prev, next)
   }
+
+  println("\n+++++++++++++++")
+  println(this)
+  printMatch()
 
   override def booleanValue(ctxts: Contexts): Unit = {
     val words = ctxts.sentence.words.length
@@ -43,33 +32,51 @@ class PatFun(params: Array[ProgNode], lastTokenPos: Int= -1) extends FunNode(par
 
     if (count > words) return
 
-    val pointers = new Array[Int](count)
-    pointers(0) = -1
+    elems.foreach(_.resetSentence())
 
-    while(stepPointers(pointers, words)) {
-      val newContext = new Context(ctxts)
-
-      var matches = true
-      for (i <- 0 until count) {
-        val start = pointers(i)
-        val end = if (i < (count - 1)) pointers(i + 1) else words
-
-        val subStr = (for (j <- start until end) yield ctxts.sentence.words(j)).map(_.word).reduceLeft(_ + " " + _)
-        val subPhrase = ctxts.sentence.words.slice(start, end)
-
-        params(i) match {
-          case v: VarNode => newContext.setWords(v.name, new Words(subPhrase, start))
-          case s: StringNode => if (s.value.toLowerCase != subStr.toLowerCase) matches = false // should bail out
-          case _ => // error
-        }
-      }
-
-      if (matches) {
-        ctxts.addContext(newContext)
-        newContext.setRetBoolean(this, value = true)
-      }
-    }
+    matches(ctxts, 0)
 
     ctxts.applyChanges()
   }
+
+  private def matches(ctxts: Contexts, pos: Int): Unit = {
+    //println("+" + pos)
+
+    elems(pos).fixed = true
+
+    elems(pos).rewind()
+    while (elems(pos).next(ctxts.sentence)) {
+      if (pos == elems.length - 1) {
+        // match found
+        println("\n")
+        println(ctxts.sentence)
+        println(this)
+        printMatch()
+        addContext(ctxts)
+      }
+      else {
+        //println(elems(pos))
+        matches(ctxts, pos + 1)
+      }
+    }
+
+    elems(pos).fixed = false
+  }
+
+  def addContext(ctxts: Contexts): Unit = {
+    val newContext = new Context(ctxts)
+
+    for (e <- elems) {
+      e match {
+        case v: VarPatternElem => newContext.setWords(v.name, v.curWords)
+        case _ => // error
+      }
+    }
+
+    ctxts.addContext(newContext)
+    newContext.setRetBoolean(this, value = true)
+  }
+
+  def printMatch() =
+    println(elems.map(_.toString).reduceLeft(_ + "\n" + _))
 }
