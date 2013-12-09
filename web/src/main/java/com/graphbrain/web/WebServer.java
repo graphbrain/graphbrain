592ff9bad651e0d17d947e8d55a3c3bd1e391be5
Telmo Menezes <telmo@telmomenezes.com>
2013-12-09 16:43:39 +0100

    proting webserver to java
    Former-commit-id: 0c04d3027de2092038216f872c46119a731a723a

--- web/src/main/java/com/graphbrain/web/WebServer.java
+++ web/src/main/java/com/graphbrain/web/WebServer.java
@@ -1,6 +1,12 @@
 package com.graphbrain.web;
 
 import com.graphbrain.db.Graph;
+import com.graphbrain.db.UserNode;
+import spark.Request;
+
+import java.text.DateFormat;
+import java.util.Locale;
+
 import static spark.Spark.*;
 
 public class WebServer {
@@ -13,6 +19,15 @@ public class WebServer {
 
         get(new HandleLanding("/"));
 
+        get(new HandleNode("/node/*"));
+
+        post(new HandleUser(HandleUser.HandleUserType.SIGNUP, "/signup"));
+        post(new HandleUser(HandleUser.HandleUserType.CHECK_USERNAME, "/checkusername"));
+        post(new HandleUser(HandleUser.HandleUserType.CHECK_EMAIL, "/checkemail"));
+        post(new HandleUser(HandleUser.HandleUserType.LOGIN, "/login"));
+
+        post(new HandleSearch("/search"));
+
         get(new HandleEco(HandleEco.HandleEcoType.PARSER, "/eco"));
         post(new HandleEco(HandleEco.HandleEcoType.PARSER, "/eco"));
         get(new HandleEco(HandleEco.HandleEcoType.CODE, "/eco/code"));
@@ -21,5 +36,64 @@ public class WebServer {
         post(new HandleEco(HandleEco.HandleEcoType.EDIT_TESTS, "/eco/edittests"));
         get(new HandleEco(HandleEco.HandleEcoType.RUN_TESTS, "/eco/runtests"));
         post(new HandleEco(HandleEco.HandleEcoType.RUN_TESTS, "/eco/runtests"));
+    }
+
+    public static UserNode getUser(Request req) {
+        String username = null;
+
+        if (req.cookies().containsKey("username")) {
+            username = req.cookie("username");
+        }
+
+        String session = null;
+
+        if (req.cookies().containsKey("session")) {
+            session = req.cookie("session");
+        }
+
+        if ((username == null) || (session == null)) {
+            return null;
+        }
+
+        UserNode userNode = graph.getUserNodeByUsername(username);
+        if (userNode == null) {
+            return null;
+        }
+        else {
+            if (userNode.checkSession(session)) {
+                return userNode;
+            }
+            else {
+                return null;
+            }
+        }
+    }
+
+    public static void log(Request req, String msg) {
+        DateFormat df = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM, Locale.ENGLISH);
+
+        String ip = "";
+        if (req != null)
+            ip = req.ip();
+
+        UserNode userNode = getUser(req);
+        String username;
+        if (userNode == null)
+            username = "null";
+        else
+            username = userNode.getUsername();
+
+        StringBuilder sb = new StringBuilder(100);
+        sb.append("[");
+        sb.append(df.format(new java.util.Date()));
+        sb.append("] ");
+        sb.append(ip);
+        sb.append(" ");
+        sb.append(username);
+        sb.append(" - ");
+        sb.append(msg);
+
+        System.out.println(sb.toString());
+        //logger.info(logLine);
     }
 }
