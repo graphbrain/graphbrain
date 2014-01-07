60fcfd41e3f0dd4ed7de022827adb380eb18034d
Telmo Menezes <telmo@telmomenezes.net>
2014-01-07 13:11:49 +0100

    working on sentence chunker
    Former-commit-id: 14989593245137e51ff28304166b428cc212291f

--- core/src/main/java/com/graphbrain/eco/Text.java
+++ core/src/main/java/com/graphbrain/eco/Text.java
@@ -1,35 +1,97 @@
 package com.graphbrain.eco;
 
-import com.graphbrain.db.Vertex;
-
 import java.util.LinkedList;
 import java.util.List;
 
 public class Text {
 
+    private String text;
     private List<String> sentences;
 
-    public Text(String text) {
-        String[] lines = text.split("\\.");
+    private int start;
+    private int end;
+    private char c;
 
-        sentences = new LinkedList<String>();
-        for (String l : lines) {
-            String sent = l.trim();
-            if (!sent.isEmpty()) {
-                sentences.add(sent);
+    public static List<String> toSentences(String text) {
+        return (new Text(text)).getSentences();
+    }
+
+    public Text(String text) {
+        this.text = text;
+        sentences = new LinkedList<>();
+        text2Sentences();
+    }
+
+    private boolean isFinalPonctuation(char ch) {
+        return ((ch == '.') || (ch == '!') || (ch == '?') || (ch == '\n'));
+    }
+
+    private boolean isSentenceEnd() {
+        if (end == text.length() - 1) {
+            return true;
+        }
+
+        if ((c != '.') && (c != '!') && (c != '?') && (c != '\n')) {
+            return false;
+        }
+
+        char nextChar = text.charAt(end + 1);
+
+        if (c == '.') {
+            if (Character.isDigit(nextChar)) {
+                return false;
             }
+        }
+
+        return !isFinalPonctuation(nextChar);
+    }
+
+    private void findSentenceEnd() {
+        while (true) {
+            if (end >= text.length()) {
+                return;
+            }
+
+            c = text.charAt(end);
+
+            if (isSentenceEnd()) {
+                return;
+            }
+            end++;
         }
     }
 
-    public void parse(Prog prog) {
-        for (String s : sentences) {
-            Vertex vertex = prog.parse(s);
-            System.out.println(s);
-            System.out.println(vertex);
+    private void addSentence() {
+        String sentence = text.substring(start, end + 1).trim();
+        if (sentence.length() > 1) {
+            sentences.add(sentence);
+        }
+    }
+
+    private void text2Sentences() {
+        start = 0;
+
+        while (start < text.length()) {
+            end = start;
+            findSentenceEnd();
+            addSentence();
+            start = end + 1;
         }
     }
 
     public List<String> getSentences() {
         return sentences;
+    }
+
+    public static void main(String[] args) {
+        String text = ".Does Telmo like chocolate???    Telmo likes chocolate. He also likes meat. Dogecoin is worth .35 BTC today.";
+        text += "\n\nhello world\n";
+        text += "\n\nxpto...";
+
+        List<String> sentences = Text.toSentences(text);
+
+        for (String s : sentences) {
+            System.out.println("> " + s);
+        }
     }
 }
