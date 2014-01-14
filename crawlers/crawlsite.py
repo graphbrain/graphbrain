0af1f300eb969c6493e211e9dff548a410c48b6f
Telmo Menezes <telmo@telmomenezes.net>
2014-01-14 15:13:30 +0100

    python crawler
    Former-commit-id: 493872f5f06013117e03a24ec3d982ff850202f8

---
+++ crawlers/crawlsite.py
@@ -0,0 +1,74 @@
+
+from bs4 import BeautifulSoup
+import urllib.request, urllib.error, urllib.parse
+from urllib.parse import urlparse
+import os
+
+ 
+class CrawlSite:
+    def __init__(self, baseurl):
+        self.baseurl = baseurl
+
+        self.basedir = "crawldata/" + self.url2path(baseurl)
+
+        self.pages = set()
+        self.count = 0
+
+        self.stop_fragments = ("comment-page", "langswitch_lang", "comments_popup", "wpmp_switcher", "submit=search", "share=")
+        self.stop_endings = (".png", ".gif", ".jpg", ".jpeg", ".pdf", ".r")
+
+        # create base dir if it does not exist
+        if not os.path.exists(self.basedir):
+    	    os.makedirs(self.basedir)
+
+    def crawl(self):
+        self.process_page(self.baseurl)
+
+    def url2path(self, urlstr):
+        return str.replace(urlstr, "/", "_")
+
+    def save_page(self, page_url_str):
+        url = urlparse(page_url_str)
+
+        if url.fragment != '':
+            return False
+
+        urlstr = page_url_str.lower()
+
+        for s in self.stop_fragments:
+        	if s in urlstr:
+        		return False
+
+        for s in self.stop_endings:
+        	if urlstr.endswith(s):
+        		return False
+
+        return True
+
+    def process_page(self, page):
+        self.pages.add(page)
+
+        self.count += 1
+        print(("#{} {}".format(self.count, page)))
+
+        content = urllib.request.urlopen(page).read()
+        soup = BeautifulSoup(content)
+
+        path = self.url2path(page)
+
+        with open(self.basedir + "/" + path, "w") as text_file:
+    	    text_file.write(str(soup))
+
+        for link in soup.find_all('a'):
+            urlstr = link.get('href')
+
+            if (urlstr != '') and (not urlstr is None):
+                if urlstr.startswith(self.baseurl):
+                    if not urlstr in self.pages:
+                        if self.save_page(urlstr):
+                            self.process_page(urlstr)
+                            
+
+if __name__ == "__main__":
+	c = CrawlSite("http://www.realclimate.org")
+	c.crawl()
