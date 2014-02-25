a893488c6a8445df498e4e8e9396d2b5803b6609
Telmo Menezes <telmo@telmomenezes.net>
2014-02-25 14:54:08 +0100

    user actions working in clojure
    Former-commit-id: 9db50e061361171f43ba81b538856e714812fa6b

--- web/src/main/clojure/graphbrain/web/handlers/user.clj
+++ web/src/main/clojure/graphbrain/web/handlers/user.clj
@@ -1,2 +1,38 @@
-(ns graphbrain.web.handlers.user)
+(ns graphbrain.web.handlers.user
+  (:use (graphbrain.web common)))
 
+(defn handle-signup
+  [response]
+  (let
+    [name ((response :form-params) "name")
+     username ((response :form-params) "username")
+     email ((response :form-params) "email")
+     password ((response :form-params) "password")]
+    (. graph createUser username  name  email  password  "user")
+    "ok"))
+
+(defn handle-check-username
+  [response]
+  (let
+    [username ((response :form-params) "username")]
+    (if (. graph usernameExists username)
+      (str "exists " username)
+      (str "ok " username))))
+
+(defn handle-check-email
+  [response]
+  (let
+    [email ((response :form-params) "email")]
+    (if (. graph emailExists email)
+      (str "exists " email)
+      (str "ok " email))))
+
+(defn handle-login
+  [response]
+  (let
+    [login ((response :form-params) "login")
+     password ((response :form-params) "password")
+     user (. graph attemptLogin login password)]
+    (if user
+      (str (. user getUsername) " " (. user getSession))
+      "failed")))
--- web/src/main/clojure/graphbrain/web/server.clj
+++ web/src/main/clojure/graphbrain/web/server.clj
@@ -1,20 +1,25 @@
 (ns graphbrain.web.server
   (:use compojure.core
         ring.adapter.jetty
-        (ring.middleware resource file-info cookies)
-        (graphbrain.web.handlers landing node))
+        (ring.middleware resource file-info cookies params)
+        (graphbrain.web.handlers landing node user))
   (:require [compojure.handler :as handler]
             [compojure.route :as route]))
 
 (defroutes app-routes
   (GET "/" response (handle-landing response))
   (GET "/node/*" response (handle-node response))
+  (POST "/signup" response (handle-signup response))
+  (POST "/checkusername" response (handle-check-username response))
+  (POST "/checkemail" response (handle-check-email response))
+  (POST "/login" response (handle-login response))
   (route/not-found "<h1>Page not found</h1>"))
 
 (def app
   (-> app-routes
     (wrap-resource "")
     wrap-file-info
+    wrap-params
     wrap-cookies))
 
 (run-jetty app {:port 4567})
