77517420dc11bb25dfba692a5d6acfba10dc1c58
Telmo Menezes <telmo@telmomenezes.com>
2014-02-25 15:05:18 +0100

    redirect users to their page upon landing
    Former-commit-id: e0b247c371264ca55af420cfa33334ea2dc870cf

--- web/src/main/clojure/graphbrain/web/handlers/landing.clj
+++ web/src/main/clojure/graphbrain/web/handlers/landing.clj
@@ -1,5 +1,6 @@
 (ns graphbrain.web.handlers.landing
-  (:use (graphbrain.web common)
+  (:use (ring.util response)
+        (graphbrain.web common)
         (graphbrain.web.views page landing))
   (:import (com.graphbrain.web NavBar CssAndJs)))
 
@@ -7,9 +8,11 @@
   [response]
   (let
     [user (get-user response)]
-    (page
-      :title "Welcome"
-      :css-and-js (. (new CssAndJs) cssAndJs)
-      :navbar (. (new NavBar nil "home") html)
-      :body-fun landing-view
-      :js "")))
+    (if user
+      (redirect (str "/node/" (. user id)))
+      (page
+        :title "Welcome"
+        :css-and-js (. (new CssAndJs) cssAndJs)
+        :navbar (. (new NavBar nil "home") html)
+        :body-fun landing-view
+        :js ""))))
