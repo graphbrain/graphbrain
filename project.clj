028823d11df732d0987b7e91e5ac2ad1dfbe6904
Telmo Menezes <telmo@telmomenezes.com>
2014-03-05 14:55:19 +0100

    converting to leingen project
    Former-commit-id: 3a535c84d8a551163f672460c7c71ed702bd915d

---
+++ project.clj
@@ -0,0 +1,28 @@
+(defproject graphbrain "0.1.0-SNAPSHOT"
+  :description "GraphBrain project"
+  :url "http://graphbrain.com/"
+  :dependencies  [[org.clojure/clojure "1.5.1"]
+                 [commons-io/commons-io "2.4"]
+                 [org.apache.commons/commons-lang3 "3.1"]
+                 [commons-lang/commons-lang "2.6"]
+                 [mysql/mysql-connector-java "5.1.27"]
+                 [org.mindrot/jbcrypt "0.3m"]
+                 [net.sf.extjwnl/extjwnl "1.7.1"]
+                 [net.sf.extjwnl/extjwnl-data-wn31 "1.1"]
+                 [com.zaxxer/HikariCP "1.2.8"]
+                 [org.jsoup/jsoup "1.7.2"]
+                 [edu.stanford.nlp/stanford-corenlp "3.2.0"]
+                 [edu.stanford.nlp/stanford-corenlp "3.2.0" :classifier "models"]
+                 [edu.stanford.nlp/stanford-parser "3.2.0"]
+                 [edu.stanford.nlp/stanford-parser "3.2.0" :classifier "models"]
+                 [org.json/json "20131018"]
+                 [net.sourceforge.nekohtml/nekohtml "1.9.19"]
+                 [compojure/compojure "1.1.6"]
+                 [ring/ring-jetty-adapter "0.3.11"]
+                 [hiccup/hiccup "1.0.5"]
+                 [org.clojure/data.json "0.2.4"]]
+  :source-paths ["src" "src/main/clojure"]
+  :java-source-paths ["src/main/java"]
+  :test-paths ["test" "src/test/clojure"]
+  :resource-paths ["src/main/resources"]
+  :main graphbrain.web.server)
