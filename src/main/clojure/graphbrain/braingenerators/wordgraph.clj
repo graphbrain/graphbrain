7f73f30833ace9259a3f2ccf39fedea46895cc57
Telmo Menezes <telmo@telmomenezes.net>
2014-03-09 17:10:36 +0100

    working on wordgraph
    Former-commit-id: 94ce57fb10ea980f85fbfe82d1ac7cb55c0320d4

--- src/main/clojure/graphbrain/braingenerators/wordgraph.clj
+++ src/main/clojure/graphbrain/braingenerators/wordgraph.clj
@@ -1,11 +1,36 @@
-(ns graphbrain.braingenerators.wordgraph)
-
-(defn add-node
-  [graph word]
-  (assoc graph word #{}))
+(ns graphbrain.braingenerators.wordgraph
+  (:use graphbrain.braingenerators.nlptools)
+  (:require [clojure.math.combinatorics :as combo]))
 
 (defn add-edge
-  [graph orig targ]
-  (let [orig-links (graph orig)
-        targ-links (graph targ)]
-    (assoc graph orig (conj orig-links targ))))
+  [graph pair]
+  (let [orig (first pair)
+        targ (second pair)
+        orig-node (graph orig)
+        targ-node (graph targ)
+        orig-links (:links orig-node)
+        targ-links (:links targ-node)
+        g (assoc graph orig (assoc orig-node :links (conj orig-links targ)))
+        g (assoc g targ (assoc targ-node :links (conj targ-links orig)))]
+    g))
+
+(defn- add-window-to-graph
+  [window graph]
+  (let [pairs combo/combinations window 2]
+    (loop [g graph
+           p pairs]
+      (if (empty? p)
+        g
+        (recur (add-edge g p)
+               (rest p))))))
+
+(defn word-list->graph
+  [word-list window-size]
+  (loop [graph {}
+         windows (partition window-size 1 word-list)]
+    (if (empty? windows)
+      graph
+      (recur (add-edge graph (first windows))
+             (rest windows)))))
+
+(def words (url->word-list "http://www.realclimate.org/index.php/archives/2014/03/the-nenana-ice-classic-and-climate/"))
