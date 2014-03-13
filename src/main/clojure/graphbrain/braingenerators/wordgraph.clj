3bbdf55b24d8422039249bc0b92ba126c3b695c2
Telmo Menezes <telmo@telmomenezes.net>
2014-03-13 15:02:57 +0100

    ngram extraction working
    Former-commit-id: 1df7a6b2f7d0e4111276d26a0df5f2e099556d31

--- src/main/clojure/graphbrain/braingenerators/wordgraph.clj
+++ src/main/clojure/graphbrain/braingenerators/wordgraph.clj
@@ -7,6 +7,7 @@
         graphbrain.eco.words)
   (:require [clojure.math.combinatorics :as combo]))
 
+
 ;; words -> word graph
 
 (defn- relevant?
@@ -75,6 +76,10 @@
   [graph]
   (topwords graph))
 
+(defn graph->topwordso
+  [graph]
+  (prgraph->topwords (graph->prgraph graph)))
+
 (defn words->topwords
   [words]
   (prgraph->topwords (words->prgraph words)))
@@ -89,82 +94,70 @@
   (prn (map #(:word %) (url->topwords url-str))))
 
 
-;; build co-occurence topwords graph
-
-(defn- add-pair-to-topword-graph
-  [pair graph tw]
-  (let [w1 (first pair)
-        w2 (second pair)
-        tw1 (contains? tw w1)
-        tw2 (contains? tw w2)]
-    (if (and tw1 tw2)
-      (add-arc graph pair)
-      graph)))
-
-(defn- inc-occurrences
-  [word graph tw]
-  (if (contains? tw word)
-    (inc-field graph word :occ)
-    graph))
-
-(defn- topwords->graph
-  [word-list tw]
-  (loop [graph {}
-         windows (partition 2 1 word-list)
-         orphan  (first (first windows))]
-    (if (empty? windows)
-      (inc-occurrences orphan graph tw)
-      (let [pair (first windows)
-            word (second pair)]
-        (recur (inc-occurrences word
-                (add-pair-to-topword-graph pair graph tw) tw)
-              (rest windows) orphan)))))
-
-(defn url->topwords-graph
-  [url-str]
-  (let [words (url->word-list url-str)]
-    (topwords->graph words (words->topwords words))))
-
-
 ;; build ngrams
 
-(defn- add-ngrams
-  [graph ngrams node-id node-val ngram]
-  (let [new-ngram (conj ngram node-id)
-        in-arcs (keys (:in node-val))]
-    (if (empty? in-arcs)
-      (conj ngrams new-ngram)
-      (loop [ngs ngrams
-             inputs in-arcs]
-        (if (empty? inputs)
-          ngs
-          (recur (add-ngrams graph ngs
-                             (first inputs)
-                             (graph (first inputs)) new-ngram)
-                 (rest inputs)))))))
+(defn- topwords->ngrams
+  [twords words]
+  (loop [wlist words
+         ngram {:words []}
+         ngrams #{}]
+    (if (empty? wlist)
+      (if (empty? (:words ngram))
+        ngrams
+        (conj ngrams ngram))
+      (let [word (first wlist)]
+        (if (contains? twords word)
+          (recur (rest wlist)
+                 (assoc ngram :words (conj (:words ngram) word))
+                 ngrams)
+          (recur (rest wlist)
+                 {:words []}
+                 (if (empty? (:words ngram))
+                   ngrams
+                   (conj ngrams ngram))))))))
 
-(defn topwords-graph->ngrams
-  [twgraph]
-  (loop [graph twgraph
-         ngrams []]
-    (if (empty? graph) ngrams
-        (let [node (first graph)
-              node-id (first node)
-              node-val (second node)
-              out-arcs (:out node-val)]
-          (recur (rest graph)
-                 (if (empty? out-arcs)
-                   (add-ngrams twgraph ngrams node-id node-val nil)
-                   ngrams))))))
+(defn- word-with-pr
+  [word graph]
+  (let [pr (:pr (graph word))]
+    (assoc word :pr pr)))
+
+(defn- words-with-pr
+  [words graph]
+  (into [] (map #(word-with-pr % graph) words)))
+
+(defn- ngrams-with-pr
+  [ngrams graph]
+  (map #(assoc % :words (words-with-pr (:words %) graph)) ngrams))
+
+(defn- average [coll]
+  (/ (reduce + coll) (count coll)))
+
+(defn- ngram-score
+  [ngram]
+  (average (map #(:pr %) (:words ngram))))
+
+(defn- scored-ngrams
+  [ngrams]
+  (map #(assoc % :score (ngram-score %)) ngrams))
+
+(defn- sorted-ngrams
+  [ngrams]
+  (sort-by :score ngrams))
 
 (defn url->ngrams
   [url-str]
-  (topwords-graph->ngrams (url->topwords-graph url-str)))
+  (let [words (url->word-list url-str)
+        prgraph (words->prgraph words)
+        twords (prgraph->topwords prgraph)]
+    (sorted-ngrams
+     (scored-ngrams
+      (ngrams-with-pr (topwords->ngrams twords words) prgraph)))))
 
 (defn print-ngrams
   [url-str]
   (let [ngrams (url->ngrams url-str)]
     (doseq [ngram ngrams]
-      (prn (clojure.string/join " " (map #(:word %) ngram))))))
+      (prn (clojure.string/join " " (map #(:word %) (:words ngram)))
+           (:score ngram)))))
 
 #_(def words (url->word-list "http://www.realclimate.org/index.php/archives/2014/03/the-nenana-ice-classic-and-climate/"))
