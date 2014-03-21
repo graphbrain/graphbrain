2d87763d30511c4bdf5aef4f4ebda65a4ec730cf
Telmo Menezes <telmo@telmomenezes.com>
2014-03-21 13:56:49 +0100

    leaf ngrams working
    Former-commit-id: be7e66652da184347a06fbcd0a5f0a74a77b6d37

--- src/main/clojure/graphbrain/braingenerators/wordgraph.clj
+++ src/main/clojure/graphbrain/braingenerators/wordgraph.clj
@@ -45,7 +45,7 @@
 
 (defn url->wordgraph
   [url-str]
-  (words->graph (url->word-list url-str)))
+  (words->graph (url->words url-str)))
 
 
 ;; compute pagerank on word graph
@@ -96,9 +96,9 @@
 
 ;; build ngrams
 
-(defn- topwords->ngrams
-  [twords words]
-  (loop [wlist words
+(defn- sentence->ngrams
+  [twords sentence]
+  (loop [wlist sentence
          ngram {:words []}
          ngrams #{}]
     (if (empty? wlist)
@@ -115,6 +115,17 @@
                  (if (empty? (:words ngram))
                    ngrams
                    (conj ngrams ngram))))))))
+
+(defn- topwords->ngrams
+  [twords sentences]
+  (loop [slist sentences
+         ngrams #{}]
+    (if (empty? slist)
+      ngrams
+      (recur
+       (rest slist)
+       (clojure.set/union ngrams
+                          (sentence->ngrams twords (first slist)))))))
 
 (defn- word-with-pr
   [word graph]
@@ -146,12 +157,12 @@
 
 (defn url->ngrams
   [url-str]
-  (let [words (url->word-list url-str)
-        prgraph (words->prgraph words)
+  (let [sentences (url->sentences url-str)
+        prgraph (words->prgraph (sentences->words sentences))
         twords (prgraph->topwords prgraph)]
     (sorted-ngrams
      (scored-ngrams
-      (ngrams-with-pr (topwords->ngrams twords words) prgraph)))))
+      (ngrams-with-pr (topwords->ngrams twords sentences) prgraph)))))
 
 (defn print-ngrams
   [url-str]
@@ -160,4 +171,79 @@
       (prn (clojure.string/join " " (map #(:word %) (:words ngram)))
            (:score ngram)))))
 
+
+;; build ngram graph
+
+(defn- ngram->map
+  [ngram ngram-map]
+  (let [word (first (:words ngram))]
+    (assoc ngram-map word (into #{} (conj (ngram-map word) ngram)))))
+
+(defn- ngrams->map
+  [ngrams]
+  (loop [m {}
+         nglist ngrams]
+    (if (empty? nglist)
+      m
+      (recur (ngram->map (first nglist) m) (rest nglist)))))
+
+(defn- is-parent?
+  [ngram rest-words parent-words]
+  (if (< (count parent-words) (count (:words ngram)))
+    (= (take (count parent-words) rest-words) parent-words)))
+
+(defn- add-parents
+  [ngram parent-ngrams ngrams words]
+  (loop [parents parent-ngrams
+         candidates ngrams]
+    (if (empty? candidates)
+      parents
+      (let [candidate (first candidates)]
+        (recur (if (is-parent? ngram words (:words candidate))
+                 (conj parents candidate)
+                 parents)
+               (rest candidates))))))
+
+(defn- parent-ngrams
+  [ngram ngram-map]
+  (let [ngram-words (:words ngram)]
+    (loop [ngrams []
+           words ngram-words]
+      (if (empty? words)
+        ngrams
+        (recur (add-parents ngram ngrams (ngram-map (first words)) words)
+               (rest words))))))
+
+(defn- ngram->graph
+  [ngram graph ngram-map]
+  (let [parents (parent-ngrams ngram ngram-map)]
+    (loop [g graph
+           p parents]
+      (if (empty? p)
+        g
+        (recur (add-arc g (list ngram (first p))) (rest p))))))
+
+(defn ngrams->graph
+  [ngrams]
+  (let [ngram-map (ngrams->map ngrams)]
+    (loop [ngs ngrams
+          graph {}]
+     (if (empty? ngs)
+       graph
+       (recur (rest ngs) (ngram->graph (first ngs) graph ngram-map))))))
+
+(defn leaf-ngrams
+  [ngrams-graph]
+  (keys (filter #(empty? (:in (second %))) ngrams-graph)))
+
+(defn print-leaf-ngrams
+  [url-str]
+  (let [ngrams (url->ngrams url-str)
+        ngrams-graph (ngrams->graph ngrams)
+        leafs (sorted-ngrams (leaf-ngrams ngrams-graph))]
+    (doseq [ngram leafs]
+      (prn (clojure.string/join " " (map #(:word %) (:words ngram)))
+           (:score ngram)))))
+
 #_(def words (url->word-list "http://www.realclimate.org/index.php/archives/2014/03/the-nenana-ice-classic-and-climate/"))
+
