14f47461806db1a4ba59c36a05d33a0142237538
Telmo Menezes <telmo@telmomenezes.net>
2014-03-23 16:43:41 +0100

    created gbui clojrescript project
    Former-commit-id: 54a1c05e2891cb345f05f471d19b42d32e3eea07

--- src/main/clojure/graphbrain/braingenerators/pagereader.clj
+++ src/main/clojure/graphbrain/braingenerators/pagereader.clj
@@ -1,134 +1,26 @@
 (ns graphbrain.braingenerators.pagereader
-  (:use [clojure.string :only [join]])
-  (:import (java.net URL)
-           (java.io StringReader)
-           (de.l3s.boilerpipe.extractors CommonExtractors)
-           (de.l3s.boilerpipe.sax HTMLHighlighter)
-           (org.jsoup Jsoup)
-           (org.jsoup.nodes TextNode)
-           (edu.stanford.nlp.process DocumentPreprocessor)
-           (edu.stanford.nlp.ling HasWord)
-           (com.graphbrain.eco Prog POSTagger Words)
-           (com.graphbrain.db Graph)))
+  (:use graphbrain.braingenerators.ngrams
+        graphbrain.disambig.entityguesser
+        graphbrain.db.graph))
 
-; create graph
-(def graph (new Graph))
 
-; create prog
-(def prog
-  (Prog/fromString (slurp "eco/page.eco") graph))
+(def g (graph))
 
-(defn- has-word-list2sentence
-  [word-list]
-  (join " "
-    (map (fn [w] (. w toString)) word-list)))
+(defn ngram->entity
+  [graph ngram]
+  (let [ngram-str (ngram->str ngram)]
+    (prn ngram-str)
+    (guess graph ngram-str "xpto")))
 
-(defn extract-sentences
-  [text]
-  (let
-    [dp (new DocumentPreprocessor (new StringReader text))]
-    (map (fn [l] (has-word-list2sentence l)) dp)))
+(defn ngrams->entities
+  [graph ngrams]
+  (map #(ngram->entity graph %) ngrams))
 
-(defn extract-html
-  "Extract 'meat' from a page, returns html string"
+(defn print-leaf-entities
   [url-str]
-  (let [url (new URL url-str)
-        extractor CommonExtractors/ARTICLE_EXTRACTOR
-        hh (HTMLHighlighter/newExtractingInstance)]
-    (. hh process url extractor)))
-
-(defn deep-text
-  "Extract the text from a html node"
-  [node]
-  (let [cn  (. node childNode 0)]
-    (. cn text)))
-
-(defn non-white-count
-  "Count number of non-whitespace characters in a string"
-  [text]
-  (loop [rest-text text
-         count 0]
-    (if (empty? rest-text)
-      count
-      (if (Character/isWhitespace (first rest-text))
-        (recur (rest rest-text) count)
-        (recur (rest rest-text) (inc count))))))
-
-(defn parse-node
-  "Parse html node to a list of maps with info about text and certain tags"
-  [node char-count]
-  (flatten (let [children (. node childNodes)]
-    (loop [rest-chil children
-           res []
-           cc char-count]
-      (if (empty? rest-chil)
-        res
-        (let [n (first rest-chil)
-              elem (cond
-                (instance? TextNode n) {:text (. n text)}
-                (= "a" (. n nodeName)) {:text (deep-text n), :tag "a", :href (. n attr "abs:href")}
-                (= "strong" (. n nodeName)) {:text (deep-text n), :tag "strong"}
-                :else (parse-node n cc))
-              length (if (map? elem)
-                       (non-white-count (elem :text))
-                       (if (empty? elem)
-                         0
-                         (- ((last elem) :end) cc)))]
-          (recur
-            (rest rest-chil)
-            (conj res (if (map? elem)
-                        (merge elem {:start cc, :end (+ cc length)})
-                        elem))
-            (+ cc length))))))))
-
-(defn split-text-tags
-  "Split output of parse-node into a string with all the text
-  and list of maps with information about tags"
-  [text+tags]
-  (loop [list text+tags
-         text ""
-         tags []]
-    (if (empty? list)
-      {:text text, :tags tags}
-      (let [elem (first list)]
-        (recur
-          (rest list)
-          (str text " " (elem :text))
-          (if (nil? (elem :tag))
-            tags
-            (conj tags elem)))))))
-
-(defn sentence2words
-  "Convert sentence string to Words object"
-  [sentence]
-  (new Words
-    (POSTagger/annotate sentence)))
-
-(defn parse-words
-  "Use eco program to parse sequence of words to graphbrain vertex"
-  [words]
-  (let [ctxts-list (. prog wv words 0 nil)
-        ctxts (if (not (empty? ctxts-list))
-               (. (first ctxts-list) getCtxts))
-        ctxt (if (not (or (nil? ctxts) (empty? ctxts)))
-               (first ctxts))]
-    (if (not (nil? ctxt))
-      (. ctxt getTopRetVertex))))
-
-(defn parse-doc
-  [doc]
-  (map parse-words
-    (extract-sentences
-      ((split-text-tags (parse-node doc 0)) :text))))
-
-(defn read-page
-  [url-str]
-  (parse-doc (Jsoup/parse (extract-html url-str))))
-
-(defn print-words
-  [words]
-  (doseq [word words]
-    (print (str (. word getWord) "[" (. word getPos) "] ")))
-  (println ""))
-
-;(println (read-page "http://www.realclimate.org/index.php/archives/2014/02/going-with-the-wind/"))
+  (let [ngrams (url->ngrams url-str)
+        ngrams-graph (ngrams->graph ngrams)
+        leafs (sorted-ngrams (leaf-ngrams ngrams-graph))
+        entities (ngrams->entities g leafs)]
+    (doseq [e entities]
+      (prn e))))
