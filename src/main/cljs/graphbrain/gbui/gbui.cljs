0576ce480be0ac3d240d1d87ef64db385feed38d
Telmo Menezes <telmo@telmomenezes.net>
2014-04-04 16:42:18 +0200

    look at working
    Former-commit-id: 8c839e65cbbbc39094b75cd59a371f0d644543a1

--- src/main/cljs/graphbrain/gbui/gbui.cljs
+++ src/main/cljs/graphbrain/gbui/gbui.cljs
@@ -3,6 +3,7 @@
             [graphbrain.gbui.globals :as g]
             [graphbrain.gbui.graph :as graph]
             [graphbrain.gbui.animation :as anim]
+            [graphbrain.gbui.relations :as rels]
             seedrandom
             jquery.cookie
             jquery.mousewheel
@@ -14,7 +15,6 @@
             disambiguate
             undo
             user
-            relations
             aichat
             state))
 
@@ -26,12 +26,12 @@
 
   (intf/init-interface)
 
-;;  (if (not (or (undefined? js/data) (nil? js/data)))
-;;    (js/initRelations))
-
   (js/browserSpecificTweaks)
 
   (graph/init-graph!)
+
+  (if (not (or (undefined? js/data) (nil? js/data)))
+    (rels/init-relations!))
   
  (if (and (exists? js/data) (not (nil? js/data)))
     (if (:changedSNode @g/graph)
