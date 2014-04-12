e84dfca7a3fc0342a26bc45c58ab2862beb8f53c
Telmo Menezes <telmo@telmomenezes.net>
2014-04-12 23:30:55 +0200

    graphbrain/string
    Former-commit-id: bf0acc8fdbe75adcbbef773faf7665863282a106

---
+++ src/main/clojure/graphbrain/string.clj
@@ -0,0 +1,13 @@
+(ns graphbrain.string)
+
+(defn contains-char?
+  [s c]
+  (some #(= c %) s))
+
+(defn contains-space?
+  [s]
+  (contains-char? s \space))
+
+(defn no-spaces?
+  [s]
+  (not (contains-space? s)))
