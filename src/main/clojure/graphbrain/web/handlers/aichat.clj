7ba6c6cb0906b8ef66e3ec7a81d74f7c72021163
Telmo Menezes <telmo@telmomenezes.com>
2014-04-12 23:30:55 +0200

    graphbrain/string
    Former-commit-id: bf0acc8fdbe75adcbbef773faf7665863282a106

--- src/main/clojure/graphbrain/web/handlers/aichat.clj
+++ src/main/clojure/graphbrain/web/handlers/aichat.clj
@@ -1,25 +1,47 @@
 (ns graphbrain.web.handlers.aichat
-  (:use (graphbrain.web common))
+  (:use (graphbrain.web common)
+        (graphbrain.string))
   (:require [clojure.data.json :as json]
             [graphbrain.graph :as gb])
   (:import (com.graphbrain.eco Prog)
            (com.graphbrain.db VertexType)))
 
-(def prog
+(defonce prog
   (Prog/fromString
     (slurp "eco/chat.eco")
     graph))
 
 (defn- aichat-reply
-  [root-id vertex]
+  [root-id vertex sentence]
   (let
       [goto-id (if (gb/edge? vertex)
                (second (. vertex getIds))
                root-id)]
     (json/write-str
-     {:sentence (.id vertex)
+     {:sentence sentence
       :newedges (list (. vertex id))
       :gotoid goto-id})))
+
+(defn- sentence-type
+  [sentence]
+  (if (and (no-spaces? sentence)
+           (or (.startsWith sentence "http://") (.startsWith sentence "https://")))
+    :url :fact))
+
+(defn- process-fact
+  [user root sentence]
+  (. prog setVertex "$user" user)
+  (. prog setVertex "$root" root)
+  (let
+      [ctxts-list (. prog wv sentence 0)
+       vertex (. (first (. (first ctxts-list) getCtxts)) getTopRetVertex)]
+    (if (gb/edge? vertex)
+      (. graph put vertex (. user id)))
+    (aichat-reply (.-id root) vertex (.-id vertex))))
+
+(defn process-url
+  [user root sentence]
+  (aichat-reply (.-id root) root "url!"))
 
 (defn handle-aichat
   [request]
@@ -27,11 +49,6 @@
         root-id ((request :form-params) "rootId")
         root (if root-id (. graph get root-id))
         user (get-user request)]
-    (. prog setVertex "$user" user)
-    (. prog setVertex "$root" root)
-    (let
-      [ctxts-list (. prog wv sentence 0)
-       vertex (. (first (. (first ctxts-list) getCtxts)) getTopRetVertex)]
-      (if (gb/edge? vertex)
-        (. graph put vertex (. user id)))
-      (aichat-reply root-id vertex))))
+    (case (sentence-type sentence)
+      :fact (process-fact user root sentence)
+      :url (process-url user root sentence))))
