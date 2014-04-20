4418ae70201f832e5e474b88019653aee7138291
Telmo Menezes <telmo@telmomenezes.net>
2014-04-20 12:34:56 +0200

    raw visualization working again
    Former-commit-id: edd0393c6da9421257af1fe830eb083e1ccf8c58

--- src/main/clojure/graphbrain/web/handlers/raw.clj
+++ src/main/clojure/graphbrain/web/handlers/raw.clj
@@ -2,26 +2,24 @@
   (:use [clojure.string :only [join]]
         (graphbrain.web common)
         (graphbrain.web.views page raw))
-  (:require [graphbrain.web.cssandjs :as css+js]))
+  (:require [graphbrain.db.graph :as gb]
+            [graphbrain.web.cssandjs :as css+js]))
 
 (defn- raw-html
   [request user vertex]
   (let
-    [vertex-id (. vertex id)]
-    (str "<h2>Vertex: " vertex-id "</h2>"
-      (. vertex raw)
-      (let
-        [user-id (if user (. user id))
-         edges (. graph edges vertex-id user-id)]
-        (join
-          (map
-            (fn [x] (str (. x id) "<br />")) edges))))))
+    [vertex-id (:id vertex)]
+    (str "<h2>Vertex: " vertex-id "</h2>" (str vertex) "<br/><br/>"
+      (let [user-id (if user (:id user) "")
+            xxx (println (str "vertex-id: " vertex-id "; user-id: " user-id))
+            edges (gb/id->edges graph vertex-id user-id)]
+        (join (map (fn [x] (str (:id x) "<br />")) edges))))))
 
 (defn handle-raw
   [request]
   (let [user (get-user request)
-        vertex (. graph get (:* (:route-params request)))]
-    (page :title (. vertex label)
+        vertex (gb/getv graph (:* (:route-params request)))]
+    (page :title (:label vertex)
           :css-and-js (css+js/css+js)
           :user user
           :page :raw
