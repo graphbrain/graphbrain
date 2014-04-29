ae3811b49309cf88b7fa88ebd044867d4ec83f9f
Telmo Menezes <telmo@telmomenezes.com>
2014-04-29 16:15:38 +0200

    search working again
    Former-commit-id: e389e5ec99246d11972a774df5a1780c3ef80512

--- src/main/clojure/graphbrain/web/handlers/search.clj
+++ src/main/clojure/graphbrain/web/handlers/search.clj
@@ -1,13 +1,13 @@
 (ns graphbrain.web.handlers.search
   (:use (graphbrain.web common))
-  (:require [clojure.data.json :as json])
-  (:import (com.graphbrain.db SearchInterface)))
+  (:require [clojure.data.json :as json]
+            [graphbrain.db.graph :as gb]
+            [graphbrain.db.searchinterface :as si]))
 
 (defn handle-search
   [request]
   (let [q ((request :form-params) "q")
-        si (new SearchInterface graph)
-        results (. si query q)
-        results-list (map (fn [x] (list x (. graph description x))) results)]
+        results (si/query graph q)
+        results-list (map (fn [x] (list x (gb/description graph x))) results)]
     (json/write-str {:count (count results-list)
                      :results results-list})))
