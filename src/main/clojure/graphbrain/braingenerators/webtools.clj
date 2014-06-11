a9b1cadc22bf4c5beb1d62a7b00f4c7c935985a4
Telmo Menezes <telmo@telmomenezes.net>
2014-06-11 23:39:14 +0200

    spliting css generators
    Former-commit-id: e7273ef6958b2252e147ae7b5da77d5caa15c421

--- src/main/clojure/graphbrain/braingenerators/webtools.clj
+++ src/main/clojure/graphbrain/braingenerators/webtools.clj
@@ -3,6 +3,7 @@
   (:import (java.net URL)
            (java.io BufferedReader InputStreamReader)))
 
+(def http-agent "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36")
 
 (defn slurp-url[url-str]
-  (:body (client/get url-str)))
+  (:body (client/get url-str {:headers {"User-Agent" http-agent}})))
