300e0d9f5a1093a7f5b5028a945cd25c4be75d0f
Telmo Menezes <telmo@telmomenezes.com>
2014-07-15 11:14:39 +0200

    eid disambig
    Former-commit-id: a446412bbf9ed1d81e3494d1d5546cedd07a50ee

--- src/main/clojure/graphbrain/disambig/edgeguesser.clj
+++ src/main/clojure/graphbrain/disambig/edgeguesser.clj
@@ -2,9 +2,21 @@
   (:require [graphbrain.db.id :as id]
             [graphbrain.disambig.entityguesser :as eg]))
 
+(defn eid->guess-eid
+  [gbdb eid text ctxts]
+  (let [ids (id/id->ids eid)
+        guess (eg/guess-eid gbdb (second ids) text nil ctxts)]
+    (if (id/eid? guess) guess
+        (id/name+ids->eid
+         (first ids)
+         (second ids)
+         (map #(eg/guess-eid gbdb % text nil ctxts) (drop 2 ids))))))
+
 (defn guess
   [gbdb id text ctxts]
   (case (id/id->type id)
     :entity (eg/guess-eid gbdb id text nil ctxts)
-    :edge (id/ids->id (map #(guess gbdb % text ctxts) (id/id->ids id)))
+    :edge (if (id/eid? id)
+            (eid->guess-eid gbdb id text ctxts)
+            (id/ids->id (map #(guess gbdb % text ctxts) (id/id->ids id))))
     id))
