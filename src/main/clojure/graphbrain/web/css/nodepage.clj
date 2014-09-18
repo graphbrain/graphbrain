dca883315f3e3503f5213e15e260757ba460b1cd
Telmo Menezes <telmo@telmomenezes.net>
2014-09-18 19:04:18 +0200

    nodepage navbar improvements
    Former-commit-id: d477f06085984eadb432514e55277fd292c905a5

--- src/main/clojure/graphbrain/web/css/nodepage.clj
+++ src/main/clojure/graphbrain/web/css/nodepage.clj
@@ -9,7 +9,7 @@
    
    [:#nodepage
     {:max-width (px 900)
-     :margin-top (px 50)
+     :margin-top (px 60)
      :margin-left "auto"
      :margin-right "auto"
      :display "block"}]
@@ -24,4 +24,41 @@
 
    [:.np-desc
     {:font-size (px 20)
-     :margin-bottom (px 10)}]])
+     :margin-bottom (px 10)}]
+
+   [:#topbar
+    {:position "fixed"
+     :top (px 0)
+     :left (px 0)
+     :width "100%"
+     :height (px 60)
+     :background "#FFF"}]
+
+   [:.topbar-element
+    {:float "left"
+     :width (px 100)}]
+
+   [:.topbar-menu
+    {:float "right"
+     :width (px 100)}]
+
+   [:.topbar-input-area
+    {:margin "0 100px"}]
+
+   [:.topbar-center
+    {:position "relative"
+     :top "50%"
+     :transform "translateY(-50%)"
+     :text-align "center"}]
+   
+   [:.top-input
+    {}]
+
+   [:.top-input-field
+    {:width "100%"
+     :padding "10px 10px"
+     :line-height (px 30)
+     :font-size (px 24)
+     :border "none"
+     ;;:-webkit-appearance "none"
+     :outline "none"}]])
