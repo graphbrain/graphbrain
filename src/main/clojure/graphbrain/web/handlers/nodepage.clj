9bbb41ef66653432179772c022c5ecf5f3386f4f
Telmo Menezes <telmo@telmomenezes.com>
2014-10-26 12:54:45 +0100

    correct nodepage titles on urls
    Former-commit-id: 0660f72f77fa0074fe58d660128abdeb5cb933a5

--- src/main/clojure/graphbrain/web/handlers/nodepage.clj
+++ src/main/clojure/graphbrain/web/handlers/nodepage.clj
@@ -2,6 +2,8 @@
   (:use (graphbrain.web.views page))
   (:require [graphbrain.db.gbdb :as gb]
             [graphbrain.db.vertex :as vertex]
+            [graphbrain.db.entity :as entity]
+            [graphbrain.db.urlnode :as url]
             [graphbrain.db.user :as user]
             [graphbrain.web.common :as common]
             [graphbrain.web.contexts :as contexts]
@@ -26,10 +28,18 @@
        all-ctxts (user/user->ctxts user)
        vert (gb/getv common/gbdb
                      (:* (:route-params request))
-                     ctxts)]
-    (page :title (vertex/label vert)
+                     ctxts)
+       title (case (:type vert)
+               :url (url/title common/gbdb (:id vert) ctxts)
+               (vertex/label vert))
+       desc (case (:type vert)
+              :entity (entity/description vert)
+              :url "web page"
+              :user "GraphBrain user"
+              nil)]
+    (page :title title
           :css-and-js (css+js/css+js)
           :user user
           :page :node
-          :body-fun #(npview/view vert user)
+          :body-fun #(npview/view user title desc)
           :js (js vert user ctxts all-ctxts))))
