0284af8ee4af210663582dc0ab4dff3e5274c296
Telmo Menezes <telmo@telmomenezes.net>
2014-12-03 13:12:53 +0100

    showing rule trace on eco visualisation
    Former-commit-id: 0060235b34a5f700c5be13b44eec0135602d714a

--- src/main/clojure/graphbrain/eco/parsers/chat.clj
+++ src/main/clojure/graphbrain/eco/parsers/chat.clj
@@ -13,40 +13,40 @@
                rel (rel r)]
            (edge rel orig targ)))
 
-(pattern chat
-         [xwith "with", x1 "a", a ?, x2 ",", b ?, verb verb, c ?]
-         (let [x (! a)
-               y (! b)
-               z (! (concat b verb c))
-               k ["r/has" y x]]
-           (edge "r/+list" k z)))
+#_(pattern chat
+           [xwith "with", x1 "a", a ?, x2 ",", b ?, verb verb, c ?]
+           (let [x (! a)
+                 y (! b)
+                 z (! (concat b verb c))
+                 k ["r/has" y x]]
+             (edge "r/+list" k z)))
 
-(pattern chat
-         [a ?, verb1 verb, b ?, xand1 "and", verb2 verb, c ?]
-         (let [x (concat a verb1 b)
-               y (concat a verb2 c)]
-           (edge "r/+list" (! x) (! y))))
+#_(pattern chat
+           [a ?, verb1 verb, b ?, xand1 "and", verb2 verb, c ?]
+           (let [x (concat a verb1 b)
+                 y (concat a verb2 c)]
+             (edge "r/+list" (! x) (! y))))
 
-(pattern chat
-         [a ?, verb1 verb, b ?, xand2 "and", verb2 verb, c ?]
-         (let [x (concat a verb1 b)
-               y (concat a verb2 c)]
-           (edge "r/+list" x y)))
+#_(pattern chat
+           [a ?, verb1 verb, b ?, xand2 "and", verb2 verb, c ?]
+           (let [x (concat a verb1 b)
+                 y (concat a verb2 c)]
+             (edge "r/+list" x y)))
 
-(pattern chat
+#_(pattern chat
          [a ?, verb verb, b ?, in1 ind, c ?, xand3 "and", d ?, in2 ind, e ?]
          (if (not (ends-with (concat in1 c) (concat in2 e)))
            (let [x (! (concat a verb b in1 c))
                  y (! (concat a verb d in2 e))]
              (edge "r/+list" x y))))
 
-(pattern chat
+#_(pattern chat
          [a ?, verb verb, b ?, xand4 "and", c ?, in ind, d ?]
          (let [x (! (concat a verb b in d))
                y (! (concat a verb c in d))]
            (edge "r/+list" x y)))
 
-(pattern chat
+#_(pattern chat
          [a ?, verb verb, b ?, xand5 "and", c ?]
          (let [x (concat a verb b)
                y (concat a verb c)]
@@ -125,6 +125,10 @@
 (pattern chat
          [this "this"]
          (root env))
+
+(pattern chat
+         [obj !verb]
+         (entity obj))
 
 (pattern chat
          [obj ?]
