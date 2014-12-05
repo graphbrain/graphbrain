2b2fe8a51957ce98cdbc7cbcde1f5aa8175fbcce
Telmo Menezes <telmo@telmomenezes.com>
2014-12-05 16:54:44 +0100

    eco pattern descriptions
    Former-commit-id: 0023f81055fc8eaa580228e84cd2005ded682bbb

--- src/main/clojure/graphbrain/eco/parsers/chat.clj
+++ src/main/clojure/graphbrain/eco/parsers/chat.clj
@@ -6,6 +6,7 @@
 (ecoparser chat)
 
 (pattern chat
+         "(object) [rel] (object)"
          [x0 "-lcb-", a ?, x1 "-rcb-",
           x2 "-lsb-", r ?, x3 "-rsb-"
           x4 "-lcb-", b ?, x5 "-rcb-"]
@@ -14,7 +15,19 @@
                rel (rel r)]
            (edge rel orig targ)))
 
+(pattern chat
+         "have to -> task"
+         [actor !verb
+          have "have"
+          to "to"
+          task ?]
+         (let [r (id->vert "r/has_task")
+               actor (! actor)
+               task (! task)]
+           (edge r actor task)))
+
 #_(pattern chat
+           "with a"
            [xwith "with", x1 "a", a ?, x2 ",", b ?, verb verb, c ?]
            (let [x (! a)
                  y (! b)
@@ -23,37 +36,36 @@
              (edge "r/+list" k z)))
 
 #_(pattern chat
+           "something verb something AND verb something"
            [a ?, verb1 verb, b ?, xand1 "and", verb2 verb, c ?]
            (let [x (concat a verb1 b)
                  y (concat a verb2 c)]
              (edge "r/+list" (! x) (! y))))
 
 #_(pattern chat
-           [a ?, verb1 verb, b ?, xand2 "and", verb2 verb, c ?]
-           (let [x (concat a verb1 b)
-                 y (concat a verb2 c)]
+           "something verb something IND something AND something IND something"
+           [a ?, verb verb, b ?, in1 ind, c ?, xand3 "and", d ?, in2 ind, e ?]
+           (if (not (ends-with (concat in1 c) (concat in2 e)))
+             (let [x (! (concat a verb b in1 c))
+                   y (! (concat a verb d in2 e))]
+               (edge "r/+list" x y))))
+
+#_(pattern chat
+           "something verb something AND something IND something"
+           [a ?, verb verb, b ?, xand4 "and", c ?, in ind, d ?]
+           (let [x (! (concat a verb b in d))
+                 y (! (concat a verb c in d))]
              (edge "r/+list" x y)))
 
 #_(pattern chat
-         [a ?, verb verb, b ?, in1 ind, c ?, xand3 "and", d ?, in2 ind, e ?]
-         (if (not (ends-with (concat in1 c) (concat in2 e)))
-           (let [x (! (concat a verb b in1 c))
-                 y (! (concat a verb d in2 e))]
-             (edge "r/+list" x y))))
-
-#_(pattern chat
-         [a ?, verb verb, b ?, xand4 "and", c ?, in ind, d ?]
-         (let [x (! (concat a verb b in d))
-               y (! (concat a verb c in d))]
-           (edge "r/+list" x y)))
-
-#_(pattern chat
-         [a ?, verb verb, b ?, xand5 "and", c ?]
-         (let [x (concat a verb b)
-               y (concat a verb c)]
-           (edge "r/+list" x y)))
+           "something verb something and something"
+           [a ?, verb verb, b ?, xand5 "and", c ?]
+           (let [x (concat a verb b)
+                 y (concat a verb c)]
+             (edge "r/+list" x y)))
 
 (pattern chat
+         "something verb IND something"
          [a ?, verb verb, in ind, b ?]
          (let [orig (! a)
                rel (rel (concat verb in))
@@ -61,6 +73,7 @@
            (edge rel orig targ)))
 
 (pattern chat
+         "something verb TO something"
          [a ?, verb verb, to to, b ?]
          (let [orig (! a)
                rel (rel (concat verb to))
@@ -68,6 +81,7 @@
            (edge rel orig targ)))
 
 (pattern chat
+         "something verb COMPAR IND something"
          [a ?, verb verb, compar compar, ind ind c ?]
          (let [orig (! a)
                rel (rel (concat verb compar ind))
@@ -75,6 +89,7 @@
            (edge rel orig targ)))
 
 (pattern chat
+         "something verb something"
          [a ?, verb verb, c ?]
          (let [orig (! a)
                rel (rel verb)
@@ -82,18 +97,30 @@
            (edge rel orig targ)))
 
 (pattern chat
+         "of"
          [a ?, xof "of", b verb]
          (let [x (! a)
                y (! b)]
            (eid "r/+of" (words->str a xof b) x y)))
 
 (pattern chat
+         "possessive with 's"
          [a ?, xs "'s", b ?]
          (let [x (! a)
                y (! b)]
            (eid "r/+poss" (str (words->str a) "'s " (words->str b)) x y)))
 
 (pattern chat
+         "action"
+         [v verb, obj ?]
+         (let [v-vert (! v)
+               obj-vert (! obj)]
+           (eid "r/+action"
+                (str (words->str v) " " (words->str obj))
+                v-vert obj-vert)))
+
+(pattern chat
+         "something in something"
          [a ?, xin "in", b !verb]
          (let [x (! a)
                y (! b)
@@ -101,37 +128,45 @@
            (edge r x y)))
 
 (pattern chat
+         "property"
          [prop (| adj adv), obj ?]
          (let [x (! prop)
                y (! obj)]
            (eid "r/+prop" (words->str prop obj) x y)))
 
 (pattern chat
+         "remove stuff in parenthesis"
          [a ?,
           x0 "-lrb-", b ?, x1 "-rrb-"]
          (! a))
 
 (pattern chat
+         "remove determinate article"
          [a det, no-dt ?]
          (! no-dt))
 
 (pattern chat
+         "remove full stop"
          [no-stop ?, stop "."]
          (! no-stop))
 
 (pattern chat
+         "the user"
          [i "i"]
          (user env))
 
 (pattern chat
+         "the current entity"
          [this "this"]
          (root env))
 
 (pattern chat
+         "object with no verb"
          [obj !verb]
          (entity obj))
 
 (pattern chat
+         "object"
          [obj ?]
          (entity obj))
 
