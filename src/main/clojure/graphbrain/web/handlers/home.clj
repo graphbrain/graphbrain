d529c22754e47c34ad963b3ab81acfe29be43c71
Telmo Menezes <telmo@telmomenezes.net>
2014-12-17 14:29:40 +0100

    launch page
    Former-commit-id: 3ad57351a650de5e0dc2b5de345aff422d25cd6a

---
+++ src/main/clojure/graphbrain/web/handlers/home.clj
@@ -0,0 +1,24 @@
+(ns graphbrain.web.handlers.home
+  (:use (ring.util response)
+        (graphbrain.web common)
+        (graphbrain.web.views page))
+  (:require [graphbrain.web.cssandjs :as css+js]
+            [graphbrain.web.views.home :as home]))
+
+(defn- js
+  []
+  "var ptype='landing';")
+
+(defn handle
+  [request]
+  (let
+    [user (get-user request)]
+    (if user
+      (redirect (str "/v/" (:id user)))
+      (page
+       :title "Welcome"
+       :css-and-js (css+js/css+js)
+       :user nil
+       :page :home
+       :body-fun home/view
+       :js (js)))))
--- src/main/clojure/graphbrain/web/handlers/landing.clj
+++ src/main/clojure/graphbrain/web/handlers/landing.clj
@@ -1,8 +1,7 @@
 (ns graphbrain.web.handlers.landing
   (:use (ring.util response)
-        (graphbrain.web common)
-        (graphbrain.web.views page landing))
-  (:require [graphbrain.web.cssandjs :as css+js]))
+        (graphbrain.web common))
+  (:require [graphbrain.web.views.launchpage :as lp]))
 
 (defn- js
   []
@@ -14,10 +13,6 @@
     [user (get-user request)]
     (if user
       (redirect (str "/v/" (:id user)))
-      (page
-        :title "Welcome"
-        :css-and-js (css+js/css+js)
-        :user nil
-        :page :home
-        :body-fun landing-view
-        :js (js)))))
+      (if (= (:server request) "graphbrain.com")
+        (lp/page)
+        (redirect "/demo")))))
