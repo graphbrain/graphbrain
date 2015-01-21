cf72d253fbd7a01a4456b468129282c3100ae1c3
Telmo Menezes <telmo@telmomenezes.net>
2015-01-21 13:16:52 +0100

    admin-only permissions
    Former-commit-id: 63a04dc768af608c11eae1f83cf0f88ba0efa1a4

--- src/main/clojure/graphbrain/web/handlers/nodeactions.clj
+++ src/main/clojure/graphbrain/web/handlers/nodeactions.clj
@@ -7,16 +7,14 @@
             [graphbrain.web.common :as common]))
 
 (defn- remove-vertex!
-  [request targ-ctxt]
-  (let [edge-id ((request :form-params) "edge")]
-    (gb/remove! common/gbdb
-                (maps/id->edge edge-id)
-                targ-ctxt)))
+  [edge-id targ-ctxt]
+  (gb/remove! common/gbdb
+              (maps/id->edge edge-id)
+              targ-ctxt))
 
 (defn- new-meaning!
-  [request targ-ctxt]
+  [request edge-id targ-ctxt]
   (let [eid ((request :form-params) "eid")
-        edge-id ((request :form-params) "edge")
         score ((request :form-params) "score")
         edge (maps/id->edge edge-id score)
         new-eid (id/new-meaning eid targ-ctxt)]
@@ -26,11 +24,14 @@
   [request]
   (let [user (common/get-user request)
         vert-id (:* (:route-params request))
+        edge-id ((request :form-params) "edge")
         op ((request :form-params) "op")
         targ-ctxt ((request :form-params) "targ-ctxt")]
-    (if (perms/can-edit? common/gbdb (:id user) targ-ctxt)
+    (if (perms/can-edit? common/gbdb edge-id (:id user) targ-ctxt)
       (case op
-        "remove" (remove-vertex! request targ-ctxt)
-        "new-meaning" (new-meaning! request targ-ctxt)))
-    (redirect (str "/n/" targ-ctxt
-                   "/" (id/local->global vert-id)))))
+        "remove" (remove-vertex! edge-id targ-ctxt)
+        "new-meaning" (new-meaning! request edge-id targ-ctxt)))
+    (redirect (str "/n/" (if (= targ-ctxt vert-id)
+                           ""
+                           (str targ-ctxt "/"))
+                   (id/local->global vert-id)))))
