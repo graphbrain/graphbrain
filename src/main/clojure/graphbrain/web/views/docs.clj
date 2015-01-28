7edfc9f38dcb7f318d0dad1ec6df2851306af9fe
Telmo Menezes <telmo@telmomenezes.net>
2015-01-28 17:59:41 +0000

    working on help
    Former-commit-id: b401ed96d75b6638db8202029573ad10fb3dd39f

--- src/main/clojure/graphbrain/web/views/docs.clj
+++ src/main/clojure/graphbrain/web/views/docs.clj
@@ -2,9 +2,23 @@
   (:use hiccup.core)
   (:require [graphbrain.web.views.barpage :as bar]))
 
+(def style "
+<style>
+  body, html {
+    background-image:url('../images/bg.png')
+    #background-color: rgba(200, 200, 255, 0.85);
+    #background-attachment: fixed;
+  }
+  #nodeback {
+    background-color: transparent;    
+  }
+</style>
+")
+
 (defn view
   [title html-str ctxt]
   (html
+   style
    [:br] [:br] [:br] [:br] [:br]
    
    [:div {:class "container"}
