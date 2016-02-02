6446a0366ba993454c4c59a4cb25a24703951d04
Telmo Menezes <telmo@telmomenezes.net>
2016-02-02 16:43:50 +0100

    updating clojure version (1.8.0) and dependencies
    Former-commit-id: 6d20acd4a839fa14b02e0bb69d0e6d83f494dbe8

--- project.clj
+++ project.clj
@@ -2,49 +2,62 @@
   :description "GraphBrain project"
   :url "http://graphbrain.com/"
   :jvm-opts ["-Xmx750M" "-XX:-OmitStackTraceInFastThrow"]
-  :dependencies  [[org.clojure/clojure "1.6.0"]
-                  [org.clojure/tools.trace "0.7.8"]
-                  [org.clojure/tools.cli "0.3.1"]
+  :dependencies  [[org.clojure/clojure "1.8.0"]
+                  ;; Clojure tools
+                  [org.clojure/tools.trace "0.7.9"]
+                  [org.clojure/tools.cli "0.3.3"]
                   [org.clojure/tools.logging "0.3.1"]
+                  [org.clojure/tools.nrepl "0.2.11"]
+                  ;; Math
+                  [org.clojure/math.combinatorics "0.1.1"]
+                  ;; Apache commons
                   [commons-io/commons-io "2.4"]
-                  [org.apache.commons/commons-lang3 "3.1"]
+                  [org.apache.commons/commons-lang3 "3.4"]
                   [commons-lang/commons-lang "2.6"]
-                  [org.clojure/java.jdbc "0.3.3"]
-                  [mysql/mysql-connector-java "5.1.27"]
-                  [com.mchange/c3p0 "0.9.2.1"]
-                  [org.mindrot/jbcrypt "0.3m"]
+                  ;; DB
+                  [org.clojure/java.jdbc "0.4.2"]
+                  [mysql/mysql-connector-java "5.1.38"]
+                  [com.mchange/c3p0 "0.9.5.2"]
+                  [com.zaxxer/HikariCP "2.4.3"]
+                  ;; JSON
+                  [org.json/json "20151123"]
+                  [org.clojure/data.json "0.2.6"]
+                  ;; XML
                   [xml-apis/xml-apis "1.4.01"]
-                  [net.sf.extjwnl/extjwnl "1.7.1"]
-                  [net.sf.extjwnl/extjwnl-data-wn31 "1.1"]
-                  [com.zaxxer/HikariCP "1.2.8"]
-                  [org.jsoup/jsoup "1.7.2"]
-                  [edu.stanford.nlp/stanford-corenlp "3.2.0"]
-                  [edu.stanford.nlp/stanford-corenlp "3.2.0" :classifier "models"]
-                  [edu.stanford.nlp/stanford-parser "3.2.0"]
-                  [edu.stanford.nlp/stanford-parser "3.2.0" :classifier "models"]
-                  [org.json/json "20131018"]
-                  [net.sourceforge.nekohtml/nekohtml "1.9.19"]
-                  [compojure/compojure "1.1.6"]
-                  [ring/ring-core "1.3.2"]
-                  [ring/ring-jetty-adapter "1.3.2"]
-                  [org.clojure/data.json "0.2.4"]
-                  [org.clojure/tools.nrepl "0.2.3"]
-                  [org.clojure/math.combinatorics "0.0.7"]
-                  [clj-http "0.9.0"]
-                  [hiccup "1.0.5"]
-                  [garden "1.1.5" :exclusions [org.clojure/clojure]]
-                  [com.cemerick/url "0.1.1"]
-                  [markdown-clj "0.9.62"]
-                  ;; ClojureScript
-                  [org.clojure/clojurescript "0.0-2173"]
-                  [jayq "2.5.0"]
-                  [alandipert/storage-atom "1.2.2"]
-                  [com.cemerick/pprng "0.0.2"]
+                  ;; CSV
                   [clojure-csv/clojure-csv "2.0.1"]
+                  ;; HTML
+                  [org.jsoup/jsoup "1.8.3"]
+                  [net.sourceforge.nekohtml/nekohtml "1.9.21"]
+                  ;; NLP
+                  [edu.stanford.nlp/stanford-corenlp "3.6.0"]
+                  [edu.stanford.nlp/stanford-corenlp "3.6.0" :classifier "models"]
+                  [edu.stanford.nlp/stanford-parser "3.6.0"]
+                  [edu.stanford.nlp/stanford-parser "3.6.0" :classifier "models"]
+                  ;; WordNet
+                  [net.sf.extjwnl/extjwnl "1.9.1"]
+                  [net.sf.extjwnl/extjwnl-data-wn31 "1.2"]
+                  ;; Crypto
+                  [org.mindrot/jbcrypt "0.3m"]
+                  ;; Web App
+                  [javax.servlet/servlet-api "2.5"]
+                  [ring/ring-core "1.4.0"]
+                  [ring/ring-jetty-adapter "1.4.0"]
+                  [compojure "1.4.0"]
+                  [clj-http "2.0.1"]
+                  [hiccup "1.0.5"]
+                  [garden "1.3.0" :exclusions [org.clojure/clojure]]
+                  [com.cemerick/url "0.1.1"]
+                  [markdown-clj "0.9.85"]
+                  ;; ClojureScript
+                  [org.clojure/clojurescript "1.7.228"]
+                  [jayq "2.5.4"]
+                  [alandipert/storage-atom "1.2.4"]
+                  [com.cemerick/pprng "0.0.3"]
                   [hiccups "0.3.0"]]
-  :plugins [[lein-ring "0.8.13"]
-            [lein-cljsbuild "1.0.2"]
-            [lein-garden "0.1.8" :exclusions [org.clojure/clojure]]]
+  :plugins [[lein-ring "0.9.7"]
+            [lein-cljsbuild "1.1.2"]
+            [lein-garden "0.2.6" :exclusions [org.clojure/clojure]]]
   :source-paths ["src/main/clojure" "src/main/cljs"]
   :java-source-paths ["src/main/java"]
   :test-paths ["test" "src/test/clojure"]
@@ -62,6 +75,7 @@
                            :optimizations :whitespace
                            :pretty-print true}}]}
 
-  :garden {:builds [{:stylesheet graphbrain.web.css.main/main
+  :garden {:builds [{:source-paths ["src/main/clojure/graphbrain/web/css"]
+                     :stylesheet graphbrain.web.css.main/main
                      :compiler {:output-to "src/main/resources/css/gb.css"
                                 :pretty-print? false}}]})
