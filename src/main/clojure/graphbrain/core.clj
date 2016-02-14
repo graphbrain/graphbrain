63135d97d4484dd3c80668d0165a345d1bd3f271
telmomenezes <telmo@telmomenezes.com>
2016-02-14 16:08:39 +0100

    cli and wordnet improvements
    Former-commit-id: da216e7231810efff59215362b9404dd7428ce20

--- src/main/clojure/graphbrain/core.clj
+++ src/main/clojure/graphbrain/core.clj
@@ -1,10 +1,20 @@
 (ns graphbrain.core
   (:require [clojure.tools.cli :as cli]
+            [graphbrain.hg.constants :as const]
+            [graphbrain.hg.ops :as ops]
             [graphbrain.kr.wordnet :as wordnet]
             [graphbrain.web.server :as server]))
 
 (def cli-options
-  [["-p" "--port PORT" "Port number"
+  [["-s" "--storage STORAGE" "Storage type"
+    :default "mysql"]
+   ["-d" "--dbname" "Database name"
+    :default "gb"]
+   ["-u" "--dbuser" "Database user"
+    :default "gb"]
+   ["-p" "--dbpass" "Database password"
+    :default "gb"]
+   ["-P" "--port PORT" "Port number"
     :default 80
     :parse-fn #(Integer/parseInt %)
     :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
@@ -14,11 +24,18 @@
     :assoc-fn (fn [m k _] (update-in m [k] inc))]
    ["-h" "--help"]])
 
+(defn opts->hg
+  [opts]
+  (ops/hg (keyword (:storage opts)) (:dbname opts)))
+
 (defn -main
   [& args]
-  (let [opts (cli/parse-opts args cli-options)
-        arg (first (:arguments opts))]
-    (case arg
-      "wordnet" (wordnet/import!)
+  (let [cli-data (cli/parse-opts args cli-options)
+        opts (:options cli-data)
+        command (first (:arguments cli-data))]
+    (println const/ascii-logo)
+    (println)
+    (case command
+      "wordnet" (wordnet/import! (opts->hg opts))
       "webapp" (server/start!)
-      (prn (str "Unknown command: " arg)))))
+      (println (str "Unknown command: " command)))))
--- src/main/clojure/graphbrain/hg/constants.clj
+++ src/main/clojure/graphbrain/hg/constants.clj
@@ -1,7 +1,29 @@
 (ns graphbrain.hg.constants)
 
-(def ^:const degree "deg")
+;; Hypergraph
+(def ^:const degree "deg/1")
+(def ^:const index "ind/1")
+(def ^:const source "src/1")
 
-(def ^:const index "ind")
+;; Relations
+(def ^:const type-of "type_of/1")
+(def ^:const synonym "synonym/1")
+(def ^:const part-of "part_of/1")
+(def ^:const antonym "antonym/1")
+(def ^:const related "related/1")
+(def ^:const part-of-speech "pos/1")
 
-(def ^:const source "src")
+;; Parts-of-speech
+(def ^:const noun "noun/1")
+(def ^:const verb "verb/1")
+(def ^:const adjective "adjective/1")
+(def ^:const adverb "adverb/1")
+
+;; WordNet
+(def ^:const wordnet "wordnet/1")
+
+;; Logo
+(def ^:const ascii-logo
+  " __   __        __        __   __              
+/ _` |__)  /\\  |__) |__| |__) |__)  /\\  | |\\ | 
+\\__> |  \\ /~~\\ |    |  | |__) |  \\ /~~\\ | | \\| ")
