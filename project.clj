51fa044fd754a67c34fcc94f4bae6cfa7fb24b58
telmomenezes <telmo@telmomenezes.com>
2016-04-21 12:06:49 +0200

    adding more license headers

--- project.clj
+++ project.clj
@@ -1,12 +1,27 @@
+;   Copyright (c) 2016 CNRS - Centre national de la recherche scientifique.
+;   All rights reserved.
+;
+;   Written by Telmo Menezes <telmo@telmomenezes.com>
+;
+;   This file is part of GraphBrain.
+;
+;   GraphBrain is free software: you can redistribute it and/or modify
+;   it under the terms of the GNU Affero General Public License as published by
+;   the Free Software Foundation, either version 3 of the License, or
+;   (at your option) any later version.
+;
+;   GraphBrain is distributed in the hope that it will be useful,
+;   but WITHOUT ANY WARRANTY; without even the implied warranty of
+;   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;   GNU Affero General Public License for more details.
+;
+;   You should have received a copy of the GNU Affero General Public License
+;   along with GraphBrain.  If not, see <http://www.gnu.org/licenses/>.
+
 (defproject graphbrain "0.1.0-SNAPSHOT"
   :description "Open Knowledge Hypergraph"
   :url "http://graphbrain.org"
-  :jvm-opts ["-Xmx4000M" "-server" "-XX:-OmitStackTraceInFastThrow"
-;;             "-Dcom.sun.management.jmxremote"
-;;             "-Dcom.sun.management.jmxremote.ssl=false"
-;;             "-Dcom.sun.management.jmxremote.authenticate=false"
-;;             "-Dcom.sun.management.jmxremote.port=43210"
-             ]
+  :jvm-opts ["-Xmx4000M" "-server" "-XX:-OmitStackTraceInFastThrow"]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ;; Clojure tools
                  [org.clojure/tools.trace "0.7.9"]
