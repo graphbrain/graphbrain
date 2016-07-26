5aa681d7fa4c978585f64c4811c54b98a847b5c6
Telmo Menezes <telmo@telmomenezes.net>
2016-07-26 13:58:34 +0200

    converting hg.symbol to python

---
+++ python/setup.py
@@ -0,0 +1,41 @@
+#   Copyright (c) 2016 CNRS - Centre national de la recherche scientifique.
+#   All rights reserved.
+#
+#   Written by Telmo Menezes <telmo@telmomenezes.com>
+#
+#   This file is part of GraphBrain.
+#
+#   GraphBrain is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   GraphBrain is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with GraphBrain.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='graphbrain',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'click',
+        'mysqlclient',
+        'matplotlib',
+        'python-igraph'
+    ],
+    entry_points='''
+        [console_scripts]
+        gb=gb.main:cli
+    ''',
+)
