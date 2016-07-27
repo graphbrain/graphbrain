951e9e1c32820c49a304b4c6b10cb5e06523e4d3
telmomenezes <telmo@telmomenezes.com>
2016-07-28 01:56:09 +0200

    converting constants.sql to python

---
+++ python/gb/hg/constants.py
@@ -0,0 +1,45 @@
+#   Copyright (c) 2016 CNRS - Centre national de la recherche scientifique.
+#   All rights reserved.
+#
+#   Written by Telmo Menezes <telmo@telmomenezes.com>
+#
+#   This file is part of GraphBrain.
+#
+#   GraphBrain is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   GraphBrain is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with GraphBrain.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# Knowledge
+source = 'src/1'
+
+# Relations
+type_of = 'type_of/1'
+synonym = 'synonym/1'
+part_of = 'part_of/1'
+antonym = 'antonym/1'
+related = 'related/1'
+part_of_speech = 'pos/1'
+
+# Parts-of-speech
+noun = 'noun/1'
+verb = 'verb/1'
+adjective = 'adjective/1'
+adverb = 'adverb/1'
+
+# WordNet
+wordnet = 'wordnet/1'
+
+# Logo
+ascii_logo = """" __   __        __        __   __
+/ _` |__)  /\\  |__) |__| |__) |__)  /\\  | |\\ | 
+\\__> |  \\ /~~\\ |    |  | |__) |  \\ /~~\\ | | \\| """
