7973bb19328b9e016dc10dcec70139a42a331b95
Telmo Menezes <telmo@telmomenezes.comm>
2017-03-04 12:22:40 +0100

    removing obsolete code

--- src/test/clojure/graphbrain/hg/mysql_test.clj
+++
@@ -1,9 +0,0 @@
-(ns graphbrain.hg.mysql-test
-  (:use clojure.test
-        graphbrain.hg.mysql
-        graphbrain.hg.ops-test)
-  (:require [graphbrain.hg.ops :as ops]))
-
-(deftest mysql-tests
-  (ops-tests
-   (connection "gbtest")))
--- src/test/clojure/graphbrain/hg/sqlite_test.clj
+++
@@ -1,9 +0,0 @@
-(ns graphbrain.hg.sqlite-test
-  (:use clojure.test
-        graphbrain.hg.sqlite
-        graphbrain.hg.ops-test)
-  (:require [graphbrain.hg.ops :as ops]))
-
-(deftest sqlite-tests
-  (ops-tests
-   (connection "gbtest")))
