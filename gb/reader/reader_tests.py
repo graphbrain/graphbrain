d022afa4a2fb5f323bbe83583d5d2c9fe5bba9d7
Telmo Menezes <telmo@telmomenezes.net>
2018-05-11 14:46:37 +0200

    specify language and model file in reader_tests

--- gb/reader/reader_tests.py
+++ gb/reader/reader_tests.py
@@ -22,10 +22,10 @@
 from gb.reader.reader import Reader
 
 
-def reader_tests(hg, infile, show_namespaces):
-    extractor = Reader(hg, show_namespaces=show_namespaces)
+def reader_tests(hg, infile, show_namespaces, lang='en', model_file=None):
+    extractor = Reader(hg, show_namespaces=show_namespaces, lang=lang, model_file=model_file)
     extractor.debug = True
-    with open(infile, 'r') as f:
+    with open(infile, 'r', encoding='utf-8') as f:
         for line in f:
             extractor.read_text(line)
             print('\n\n')
