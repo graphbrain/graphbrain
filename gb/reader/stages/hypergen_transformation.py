47b54fe041a6d74e367c4774456ec1957a2ec936
Telmo Menezes <telmo@telmomenezes.net>
2018-07-24 18:42:06 +0200

    renaming hypergen ops

--- gb/reader/stages/hypergen_transformation.py
+++ gb/reader/stages/hypergen_transformation.py
@@ -22,39 +22,39 @@
 from gb.reader.semantic_tree import Position
 
 
-IGNORE, APPLY_NODE, NEST_NODE, APPLY_ROOT, NEST_ROOT, APPEND = range(6)
+IGNORE, APPLY_HYPEREDGE, NEST_HYPEREDGE, APPLY_TOKEN, NEST_TOKEN, HEAD = range(6)
 
 
 def apply(parent, root, child_id, pos, transf):
     if transf == IGNORE:
         pass
-    elif transf == APPLY_NODE:
+    elif transf == APPLY_HYPEREDGE:
         if pos == Position.LEFT:
             parent.apply_head(child_id)
         else:
             parent.apply_tail(child_id)
-    elif transf == NEST_NODE:
+    elif transf == NEST_HYPEREDGE:
         parent.nest(child_id)
-    elif transf == APPLY_ROOT:
+    elif transf == APPLY_TOKEN:
         root.apply_tail(child_id)
-    elif transf == NEST_ROOT:
+    elif transf == NEST_TOKEN:
         root.nest(child_id)
-    elif transf == APPEND:
+    elif transf == HEAD:
         parent.reverse_apply(child_id)
 
 
 def to_string(transf):
     if transf == IGNORE:
         return 'ignore'
-    elif transf == APPLY_NODE:
-        return 'apply node'
-    elif transf == NEST_NODE:
-        return 'next node'
-    elif transf == APPLY_ROOT:
-        return 'apply root'
-    elif transf == NEST_ROOT:
-        return 'nest root'
-    elif transf == APPEND:
-        return 'append'
+    elif transf == APPLY_HYPEREDGE:
+        return 'apply hyperedge'
+    elif transf == NEST_HYPEREDGE:
+        return 'nest hyperedge'
+    elif transf == APPLY_TOKEN:
+        return 'apply token'
+    elif transf == NEST_TOKEN:
+        return 'nest token'
+    elif transf == HEAD:
+        return 'head'
     else:
         return '?'
