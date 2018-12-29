cf15232ab330cbf6d3e9763d385be61f7799c52e
Telmo Menezes <telmo@telmomenezes.net>
2018-12-29 19:11:44 +0100

    v0.0.4

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='graphbrain',
-    version='0.0.3',
+    version='0.0.4',
     author='Telmo Menezes et al.',
     author_email='telmo@telmomenezes.net',
     description='Knowledge System + Natural Language Understanding',
