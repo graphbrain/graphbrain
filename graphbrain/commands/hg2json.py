b20bf08a432c548ec1291485013adf16298edbb6
Telmo Menezes <telmo@telmomenezes.net>
2019-01-04 18:45:02 +0100

    removing obsolete filters module

--- graphbrain/commands/hg2json.py
+++ graphbrain/commands/hg2json.py
@@ -1,9 +1,16 @@
+import json
 from graphbrain.hypergraph import HyperGraph
-from graphbrain.filters.filters import AllFilter
+
+
+def edge2dict(hg, edge):
+    return {'edge': edge2str(edge), 'text': hg.get_str_attribute(edge, 'text')}
 
 
 def run(params):
     hg = HyperGraph(params)
     outfile = params['outfile']
-    filt = AllFilter(hg)
-    filt.write_edges(outfile)
+
+    edge_data = [edge2dict(hg, edge) for edge in hg.all()]
+
+    with open(outfile, 'w') as json_file:
+        json.dump(edge_data, json_file)
