a8d38b701f128369624d80c02ecfdb9c41bda4d2
Telmo Menezes <telmo@telmomenezes.comm>
2019-01-26 19:33:38 +0100

    fixing init files

---
+++ graphbrain/parsers/stages/__init__.py
@@ -0,0 +1,7 @@
+from .hypergen import Hypergen
+from .disamb import Disamb
+from .disamb_simple import DisambSimple
+from .disamb_naive import DisambNaive
+from .merge import Merge
+from .shallow import Shallow
+from .concepts import Concepts
