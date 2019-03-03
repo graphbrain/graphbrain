02ac76823b76137452906e11c8ffa30fc0b19384
Telmo Menezes <telmo@telmomenezes.comm>
2019-03-03 16:04:13 +0100

    nlp tree visualisation

---
+++ graphbrain/parser/vis.py
@@ -0,0 +1,31 @@
+from collections import OrderedDict
+from asciitree import LeftAligned
+from termcolor import colored
+
+
+def token2str(token):
+    word = colored(token.text.strip(), 'cyan')
+    lemma = token.lemma_.strip()
+    pos = token.pos_
+    tag = colored(token.tag_, 'green')
+    dep = colored(token.dep_, 'yellow')
+    named_entity = token.ent_type_
+
+    if named_entity != '':
+        named_entity = colored('{%s}' % named_entity, 'magenta')
+
+    return '%s/%s/%s/%s (%s) %s' % (word, lemma, pos, tag, dep, named_entity)
+
+
+def token2label_tree(token, prefix='*'):
+    children = [token2label_tree(leaf, '<') for leaf in token.lefts] +\
+               [token2label_tree(leaf, '>') for leaf in token.rights]
+
+    label = '%s %s' % (colored(prefix, 'red'), token2str(token))
+    return label, OrderedDict(children)
+
+
+def print_tree(token):
+    label, children = token2label_tree(token)
+    tr = LeftAligned()
+    print(tr({label: children}))
