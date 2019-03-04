62b2f447350d45df4402ed6b5fec0048b42f200d
Telmo Menezes <telmo@telmomenezes.comm>
2019-03-05 00:14:30 +0100

    new parser passing 4 tests

--- graphbrain/parser/parser.py
+++ graphbrain/parser/parser.py
@@ -2,62 +2,12 @@ import spacy
 from graphbrain.funs import edge2str
 
 
-deps_types = {
-    'acl': 'p',  # concept-predicate? ###
-    'acomp': 'c',
-    'advcl': 'p',
-    'advmod': 'w',  # meta-modifier?
-    'agent': 'x',
-    'amod': 'm',
-    'appos': 'c',
-    'attr': 'c',
-    'aux': 'a',
-    'auxpass': 'a',
-    'case': 'b',  # ##
-    'cc': 'b',
-    'ccomp': 'p',
-    'compound': 'c',  # add +/b to form concept above
-    'conj': 'c',
-    'csubj': 'p',  # ?
-    'csubjpass': 'p',  # ?
-    'dative': 'c',
-    'dep': 'c',
-    'det': 'm',
-    'dobj': 'c',
-    'expl': 'a',
-    'intj': '',  # ignore
-    'mark': 'x',
-    'meta': 'c',  # ?
-    'neg': 'a',   # should be own type
-    'npadvmod': 'm',  # multiple words? ("a share")
-    'nsubj': 'c',
-    'nsubjpass': 'c',
-    'nummod': 'm',
-    'oprd': 'c',
-    'parataxis': 'p',
-    'pcomp': 'p',
-    'pobj': 'c',
-    'poss': 'c',
-    'preconj': 'm',
-    'predet': 'm',
-    'prep': {
-        'p': 'x',
-        '*': 'b'
-    },
-    'prt': 'a',  # could be in front of predicate, e.g. "shut down")
-    'punct': '',  # ignore
-    'quantmod': 'w',
-    'relcl': 'p',
-    'root': '',
-    'xcomp': 'c'
-}
-
-
 deps_arg_types = {
     'nsubj': 's',      # subject
     'nsubjpass': 'p',  # passive subject
     'agent': 'a',      # agent
     'acomp': 'c',      # subject complement
+    'attr': 'c',       # subject complement
     'dobj': 'o',       # direct object
     'dative': 'i',     # indirect object
     'advcl': 'x',      # specifier
@@ -112,6 +62,18 @@ def connector_type(entity):
         return entity_type(entity[0])
 
 
+def token_head_type(token):
+    head = token.head
+    if head:
+        return token_type(head)
+    else:
+        return ''
+
+
+def is_noun(token):
+    return token.tag_[:2] == 'NN'
+
+
 def token_type(token):
     dep = token.dep_
     if dep == 'ROOT':
@@ -119,17 +81,41 @@ def token_type(token):
             return 'p'
         else:
             return 'c'
-    else:
-        typ = deps_types.get(dep, '')
-        if type(typ) is str:
-            return typ
+    elif dep in {'acomp', 'appos', 'attr', 'compound', 'conj', 'dative', 'dep', 'dobj', 'nsubj', 'nsubjpass',
+                 'oprd', 'pobj', 'meta', 'xcomp'}:
+        return 'c'
+    elif dep in {'acl', 'advcl', 'ccomp', 'csubj', 'csubjpass', 'parataxis', 'pcomp', 'relcl'}:
+        return 'p'
+    elif dep in {'amod', 'det', 'npadvmod', 'nummod', 'preconj', 'predet'}:
+        return 'm'
+    elif dep in {'aux', 'auxpass', 'expl', 'neg', 'prt'}:
+        return 'a'
+    elif dep in {'case', 'cc'}:
+        return 'b'
+    elif dep in {'agent', 'mark'}:
+        return 'x'
+    elif dep == 'quantmod':
+        return 'w'
+    elif dep in {'intj', 'punct'}:
+        return ''
+    elif dep == 'advmod':
+        if token_head_type(token) == 'p':
+            return 'a'
         else:
-            head = token.head
-            if head:
-                parent_dep = token_type(head)
-            else:
-                parent_dep = '*'
-            return typ.get(parent_dep, typ['*'])
+            return 'w'
+    elif dep == 'poss':
+        if is_noun(token):
+            return 'c'
+        else:
+            return 'm'
+    elif dep == 'prep':
+        if token_head_type(token) == 'p':
+            return 'x'
+        else:
+            return 'b'
+    else:
+        #  error / warning
+        pass
 
 
 def arg_type(token):
@@ -146,22 +132,36 @@ def nest(inner, outer, before):
             return (outer[0], inner) + outer[1:]
 
 
+def nest_predicate(inner, outer, before):
+    if entity_type(inner) == 'p':
+        return nest(inner, outer, before)
+    else:
+        return (nest(inner[0], outer, before),) + inner[1:]
+
+
 def parens(entity):
     if is_atom(entity):
         return (entity,)
     else:
-        return entity
+        return tuple(entity)
 
 
 def connect(connector, arguments):
     return (connector,) + tuple(arguments)
 
 
-def apply_n(entity, arguments):
+def apply(entity, argument):
+    if is_atom(entity):
+        return (entity, argument)
+    else:
+        return (entity[0], argument) + entity[1:]
+
+
+def apply_n(entity, arguments, before=None):
     if len(arguments) == 0:
         return entity
     else:
-        return parens(entity) + tuple(arguments)
+        return parens(entity) + parens(arguments)
 
 
 def sequence(target, entity, before):
@@ -170,6 +170,26 @@ def sequence(target, entity, before):
     else:
         return parens(target) + parens(entity)
 
+
+def apply_fun_to_atom(fun, atom, target, entity, before):
+    if is_atom(target):
+        if target == atom:
+            return fun(target, entity, before)
+        else:
+            return target
+    else:
+        return tuple(apply_fun_to_atom(fun, atom, item, entity, before) for item in target)
+
+
+def post_process(entity):
+    if is_atom(entity):
+        return entity
+    else:
+        entity = tuple(post_process(item) for item in entity)
+        if connector_type(entity) == 'c':
+            return apply_n('+/b/.', entity)
+        else:
+            return entity
 
 class Parser(object):
     def __init__(self, lang):
@@ -181,14 +201,10 @@ class Parser(object):
             raise RuntimeError('unkown language: %s' % lang)
 
     def parse_token(self, token):
-        entities = []
-        modifiers = []
-        builders = []
-        auxiliaries = []
-        metas = []
-        subpredicates = []
         positions = {}
         tokens = {}
+        children = []
+        entities = []
 
         parent_type = token_type(token)
 
@@ -200,101 +216,52 @@ class Parser(object):
             tokens[child] = child_token
             if child:
                 child_type = entity_type(child)
-                if child_type in {'c', 'r', 'd'}:
-                    if parent_type == 'c' and connector_type(child) == 'b':
-                        builders.append(child)
-                    else:
+                if child_type:
+                    children.append(child)
+                    if child_type in {'c', 'r', 'd'}:
                         entities.append(child)
-                elif child_type == 'a':
-                    auxiliaries.append(child)
-                elif child_type == 'm':
-                    modifiers.append(child)
-                elif child_type == 'b':
-                    builders.append(child)
-                elif child_type == 'w':
-                    metas.append(child)
-                elif child_type == 'x':
-                    subpredicates.append(child)
-                else:
-                    # error
-                    pass
 
-        modifiers.reverse()
-        builders.reverse()
-        auxiliaries.reverse()
-        metas.reverse()
-        subpredicates.reverse()
+        children.reverse()
 
-        parent = '%s/%s' % (token.text.lower(), parent_type)
-
-        # print('')
-        # print('=> %s' % parent_type)
-        # print('entities: %s' % len(entities))
-        # print(edge2str(parent))
-
-        if parent_type == 'c':
-            for builder in builders:
-                parent = nest(parent, builder, positions[builder])
-
-            if is_atom(parent) or entity_type(parent[0]) == 'c':
-                for entity in entities:
-                    parent = sequence(parent, entity, positions[entity])
-            else:
-                parent = apply_n(parent, entities)
-
-            for modifier in modifiers:
-                parent = nest(parent, modifier, positions[modifier])
-
-            if not is_atom(parent) and connector_type(parent) == 'c':
-                parent = apply_n('+/b/gb', parent)
-
-        elif parent_type == 'p':
-            subtype = 'd'  # TODO
-            args_string = ''.join([arg_type(tokens[entity]) for entity in entities])
-            parent = '%s.%s.%s' % (parent, subtype, args_string)
-
-            for auxiliary in auxiliaries:
-                parent = nest(parent, auxiliary, positions[auxiliary])
-
-            parent = connect(parent, entities)
-
-            for subpredicate in subpredicates:
-                parent = nest(parent, subpredicate, positions[subpredicate])
-
-        elif parent_type == 'b':
-            for meta in metas:
-                parent = nest(parent, meta, positions[meta])
-
-            for modifier in modifiers:
-                parent = nest(parent, modifier, positions[modifier])
-
-            for builder in builders:
-                parent = nest(parent, builder, positions[builder])
-
-            parent = apply_n(parent, entities)
-
-        elif parent_type == 'm':
-            for meta in metas:
-                parent = nest(parent, meta, positions[meta])
-
-            for modifier in modifiers:
-                parent = nest(parent, modifier, positions[modifier])
-
-            for builder in builders:
-                parent = nest(parent, builder, positions[builder])
-
-        if parent_type == 'x':
-            parent = connect(parent, entities)
-
-        elif parent_type == '':
+        if parent_type == '':
             return None
 
-        else:
-            # error
-            pass
+        parent_atom = '%s/%s' % (token.text.lower(), parent_type)
 
-        # print(edge2str(parent))
-        return parent
+        if parent_type == 'p':
+            subtype = 'd'  # TODO
+            args_string = ''.join([arg_type(tokens[entity]) for entity in entities])
+            parent_atom = '%s.%s.%s' % (parent_atom, subtype, args_string)
+
+        parent = parent_atom
+
+        for child in children:
+            child_type = entity_type(child)
+            if child_type in {'c', 'r', 'd'}:
+                if parent_type == 'c':
+                    if connector_type(child) == 'b':
+                        parent = nest(parent, child, positions[child])
+                    else:
+                        if entity_type(parent_atom) == 'c' and connector_type(child) == 'c':
+                            parent = apply_fun_to_atom(sequence, parent_atom, parent, child, positions[child])
+                        else:
+                            parent = apply_fun_to_atom(apply_n, parent_atom, parent, (child,), positions[child])
+                else:
+                    parent = apply(parent, child)
+            elif child_type == 'b':
+                if connector_type(parent) == 'c':
+                    parent = apply_n(child, parent)
+                else:
+                    parent = nest(parent, child, positions[child])
+            elif child_type == 'm':
+                parent = nest(parent, child, positions[child])
+            elif child_type == 'a':
+                parent = nest_predicate(parent, child, positions[child])
+            elif child_type == 'w':
+                parent = nest(parent, child, positions[child])
+            parent_type = entity_type(parent)
+
+        return post_process(parent)
 
     def parse(self, text):
         doc = self.nlp(text)
