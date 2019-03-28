87185edc976e396f5f173f1c75287c366313fa14
Telmo Menezes <telmo@telmomenezes.net>
2019-03-28 19:19:07 +0100

    improving notebook entity visualization

--- graphbrain/notebook.py
+++ graphbrain/notebook.py
@@ -9,25 +9,43 @@ EDGE_COLORS = ['#a65628', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00',
                '#ffff33', '#e41a1c', '#f781bf']
 
 
-def _ent2html(entity, roots_only=True, compact=False, indent=False, close=True,
-              depth=0, color='#000', conn=False, close_html=''):
+def _ent2html(entity, roots_only=True, formatting='indented', indent=False,
+              close=True, depth=0, color='#000', conn=False, close_html=''):
     """Convert an entity to an html representation, internal implementation."""
+
+    assert(formatting in {'indented', 'compact', 'oneline'})
 
     # default value for close html string that is returned to caller
     ret_close_html = ''
 
     # font size
-    if compact:
-        font_size = 11
+    if formatting == 'indented':
+        fs = 14 - depth
+    elif formatting == 'compact':
+        fs = 12
+    elif formatting == 'oneline':
+        fs = 11
+    font_size = 'font-size:{}pt;'.format(fs)
+
+    # indent-dependent variables
+    if indent:
+        main_tag = 'div'
+        margin = 'margin-left:20px;'
     else:
-        font_size = 14 - depth
+        main_tag = 'span'
+        margin = ''
+
+    # color
+    color_html = 'color:{}'.format(color)
 
     # different renders depending on weather:
     # ... entity is an edge
     if is_edge(entity):
         et = entity_type(entity)[0]
         arity = len(entity)
+        contains_edges = any(is_edge(child) for child in entity)
         color = EDGE_COLORS[depth % len(EDGE_COLORS)]
+        color_html = 'color:{}'.format(color)
 
         # surroundings symbols depending on edge type
         if et == 'c':
@@ -42,14 +60,10 @@ def _ent2html(entity, roots_only=True, compact=False, indent=False, close=True,
 
         # render opening symbol
         html = """
-        <span style="font-weight:bold;font-size:{}pt">{}</span>
-        """.format(str(font_size), open_symbol).strip()
+        <span style="font-weight:bold;{}">{}</span>
+        """.format(font_size, open_symbol).strip()
 
-        # html that closes this edge render
-        if compact or (not indent):
-            close_html = '</span>'
-        else:
-            close_html = '</div>'
+        close_html = '</{}>'.format(main_tag)
 
         # render edge children
         for i in range(arity):
@@ -61,11 +75,17 @@ def _ent2html(entity, roots_only=True, compact=False, indent=False, close=True,
                 sep = ''
             # not connector
             else:
-                # edges with only two items are rendered in one line
-                if arity > 2:
-                    child_indent = True
-                else:
+                # indent depending on formatting
+                if formatting == 'indented':
+                    child_indent = contains_edges if is_atom(child) else True
+                elif formatting == 'compact':
+                    # edges with only two items are rendered in one line
+                    child_indent = arity > 2
+                    if child_indent and is_atom(child) and not contains_edges:
+                        child_indent = False
+                elif formatting == 'oneline':
                     child_indent = False
+
                 child_color = SYMBOL_COLOR
                 child_conn = conn
                 sep = ' '
@@ -80,7 +100,7 @@ def _ent2html(entity, roots_only=True, compact=False, indent=False, close=True,
 
                 child_html, child_close_html = _ent2html(child,
                                                          roots_only=roots_only,
-                                                         compact=compact,
+                                                         formatting=formatting,
                                                          indent=child_indent,
                                                          close=child_close,
                                                          depth=depth + 1,
@@ -90,16 +110,12 @@ def _ent2html(entity, roots_only=True, compact=False, indent=False, close=True,
             else:
                 child_html, _ = _ent2html(child,
                                           roots_only=roots_only,
-                                          compact=compact,
+                                          formatting=formatting,
+                                          indent=child_indent,
                                           depth=depth,
                                           color=child_color,
                                           conn=child_conn)
             html = '{}{}{}'.format(html, sep, child_html)
-
-        if indent:
-            margin = 20
-        else:
-            margin = 0
 
         # if closing html should not be rendered at the end o this edge
         # representation, then return it to the parent
@@ -109,32 +125,24 @@ def _ent2html(entity, roots_only=True, compact=False, indent=False, close=True,
 
         # render close symbol
         html = """
-        {}<span style="color:{}">
-            <span style="font-weight:bold;font-size:{}pt">{}</span>
+        {}<span style="{}">
+            <span style="font-weight:bold;{}">{}</span>
         </span>
-        """.format(html, color, str(font_size), close_symbol).strip()
+        """.format(html, color_html, font_size, close_symbol).strip()
 
         # render edge
-        if compact or (not indent):
-            html = """
-            <span style="color:{}">{}{}
-            """.format(color, html, close_html).strip()
-        else:
-            html = """
-            <div style="margin-left:{}px;color:{}">{}{}
-            """.format(str(margin), color, html, close_html).strip()
+        html = """
+        <{} style="{}{}">{}{}
+        """.format(main_tag, margin, color_html, html, close_html).strip()
     # ... entity is an atom
     else:
         # connectors are rendered in bold
-        if conn:
-            bold_style = 'font-weight:bold;'
-        else:
-            bold_style = ''
+        bold_style = 'font-weight:bold;' if conn else ''
 
         # render atom root
         html = """
-        <span style="{}color:{}">{}</span>
-        """.format(bold_style, color, str(root(entity)).strip()).strip()
+        <span style="{}{}">{}</span>
+        """.format(bold_style, color_html, str(root(entity)).strip()).strip()
 
         # render non-root part, if requested
         if not roots_only:
@@ -144,20 +152,20 @@ def _ent2html(entity, roots_only=True, compact=False, indent=False, close=True,
                        NAMESPACE_COLOR,
                        str('/'.join(atom_parts(entity)[1:])).strip()).strip()
 
-        # render atom with font size
+        # render atom
         html = """
-        <span style="font-size:{}pt">{}</span>
-        """.format(str(font_size), html).strip()
+        <{} style="{}{}">{}</{}>
+        """.format(main_tag, margin, font_size, html, main_tag).strip()
 
     return html, ret_close_html
 
 
-def ent2html(entity, roots_only=True, compact=False):
+def ent2html(entity, roots_only=True, formatting='indented'):
     """Convert an entity to an html representation."""
-    return _ent2html(entity, roots_only=roots_only, compact=compact)[0]
+    return _ent2html(entity, roots_only=roots_only, formatting=formatting)[0]
 
 
-def show(entity, roots_only=True, compact=False):
+def show(entity, roots_only=True, formatting='indented'):
     """Show a representation of the entity in the notebook."""
-    html = ent2html(entity, roots_only=roots_only, compact=compact)
+    html = ent2html(entity, roots_only=roots_only, formatting=formatting)
     display(HTML(html))
