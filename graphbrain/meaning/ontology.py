b83cbc4f032ae1dd41d3b28b00c367c1aed853da
Telmo Menezes <telmo@telmomenezes.net>
2019-07-23 14:33:01 +0200

    bug fix

--- graphbrain/meaning/ontology.py
+++ graphbrain/meaning/ontology.py
@@ -1,9 +1,10 @@
 import progressbar
+from graphbrain import *
 import graphbrain.constants as const
 
 
 def subtypes(hg, edge):
-    ont_edges = hg.pat2edges((const.type_of_pred, '*', edge))
+    ont_edges = hg.pat2edges(hedge((const.type_of_pred, '*', edge)))
     return tuple([ont_edge[1] for ont_edge in ont_edges])
 
 
