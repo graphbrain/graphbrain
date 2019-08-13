d16982dc3f382503bf3941f9bb06e147b8eb6884
Telmo Menezes <telmo@telmomenezes.net>
2019-08-13 16:28:48 +0200

    lemma()

--- graphbrain/meaning/lemmas.py
+++ graphbrain/meaning/lemmas.py
@@ -1,6 +1,13 @@
 import graphbrain.constants as const
 
 
+def lemma(hg, edge):
+    if edge.is_atom():
+        for lemma_edge in hg.search((const.lemma_pred, edge, '*')):
+            return lemma_edge[2]
+    return None
+
+
 def deep_lemma(hg, edge):
     if edge.is_atom():
         for lemma_edge in hg.search((const.lemma_pred, edge, '*')):
