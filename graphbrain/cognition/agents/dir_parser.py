a724c8a5d84f531c029fe3efd3d6d01b28880af1
Telmo Menezes <telmo@telmomenezes.net>
2020-11-15 18:20:30 +0100

    created dir_parser agent

---
+++ graphbrain/cognition/agents/dir_parser.py
@@ -0,0 +1,44 @@
+import os
+import re
+import logging
+from graphbrain.cognition.agent import Agent
+from graphbrain.cognition.agents.txt_parser import parse_text
+
+
+def build_sequence_name(path, name):
+    seq_name = '|'.join((path, name))
+    seq_name = seq_name.lower()
+    seq_name = seq_name.replace('/', '|')
+    seq_name = seq_name.replace(' ', '-')
+    seq_name = re.sub('[^a-z0-9\_\-|]+', '', seq_name)
+    return seq_name
+
+
+class DirParser(Agent):
+    def __init__(self, name, progress_bar=True, logging_level=logging.INFO):
+        super().__init__(
+            name, progress_bar=progress_bar, logging_level=logging_level)
+        self.sequences = []
+
+    def run(self):
+        indir = self.system.get_indir(self)
+        parser = self.system.get_parser(self)
+
+        for dirpath, _, filenames in os.walk(indir):
+            path = dirpath[len(indir) + 1:]
+            for filename in filenames:
+                name, extension = os.path.splitext(filename)
+                if extension == '.txt':
+                    while extension != '':
+                        name, extension = os.path.splitext(name)
+                    sequence = build_sequence_name(path, name)
+                    self.sequences.append(sequence)
+                    infile = os.path.join(dirpath, filename)
+                    print(sequence)
+                    for op in parse_text(infile, parser, sequence):
+                        yield op
+
+    def report(self):
+        sequences = '\n'.join(self.sequences)
+        return '{}\n\nSequences created:\n{}'.format(
+            super().report(), sequences)
--- graphbrain/cognition/agents/txt_parser.py
+++ graphbrain/cognition/agents/txt_parser.py
@@ -11,6 +11,32 @@ def paragraphs(file_name):
                 yield paragraph
 
 
+def parse_text(infile, parser, sequence):
+    pos = 0
+    for paragraph in paragraphs(infile):
+        parse_results = parser.parse(paragraph)
+        for parse in parse_results['parses']:
+            main_edge = parse['resolved_corefs']
+
+            # add main edge
+            if main_edge:
+                # attach text to edge
+                text = parse['text']
+                attr = {'text': text}
+
+                # print('main edge: {}'.format(main_edge.to_str()))
+                yield create_op(main_edge, sequence=sequence, position=pos,
+                                attributes=attr)
+                pos += 1
+
+                # add extra edges
+                for edge in parse['extra_edges']:
+                    yield create_op(edge)
+        for edge in parse_results['inferred_edges']:
+            # print('inferred edge: {}'.format(edge.to_str()))
+            yield create_op(edge, count=True)
+
+
 class TxtParser(Agent):
     def __init__(self, name, progress_bar=True, logging_level=logging.INFO):
         super().__init__(
@@ -26,30 +52,10 @@ class TxtParser(Agent):
         parser = self.system.get_parser(self)
         sequence = self.system.get_sequence(self)
 
-        pos = 0
-        for paragraph in paragraphs(infile):
-            parse_results = parser.parse(paragraph)
-            for parse in parse_results['parses']:
-                main_edge = parse['resolved_corefs']
-
-                # add main edge
-                if main_edge:
-                    # attach text to edge
-                    text = parse['text']
-                    attr = {'text': text}
-
-                    # print('main edge: {}'.format(main_edge.to_str()))
-                    yield create_op(main_edge, sequence=sequence, position=pos,
-                                    attributes=attr)
-                    self.edges += 1
-                    pos += 1
-
-                    # add extra edges
-                    for edge in parse['extra_edges']:
-                        yield create_op(edge)
-            for edge in parse_results['inferred_edges']:
-                # print('inferred edge: {}'.format(edge.to_str()))
-                yield create_op(edge, count=True)
+        for op in parse_text(infile, parser, sequence):
+            if op['sequence'] == sequence:
+                self.edges += 1
+            yield op
 
     def report(self):
         rep_str = ('edges found: {}'.format(self.edges))
