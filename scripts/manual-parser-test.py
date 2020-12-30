915fc5b7b7877e7311a7351c3609312a77e23deb
Telmo Menezes <telmo@telmomenezes.net>
2020-12-30 12:54:34 +0100

    manual parser test script

--- scripts/manual-parser-test.py
+++ scripts/manual-parser-test.py
@@ -1,5 +1,4 @@
-from collections import defaultdict
-import json
+import statistics
 from termcolor import colored
 from graphbrain import hedge
 from graphbrain.cli import wrapper
@@ -7,7 +6,7 @@ from graphbrain.colored import indented, colored_edge
 from graphbrain.parsers import create_parser
 
 
-class HumanEvaluation(object):
+class ManualEvaluation(object):
     def __init__(self):
         self.correct = 0
         self.defect = 0
@@ -38,22 +37,34 @@ class HumanEvaluation(object):
             self.defect += 1
             self.defects.append(defect)
             self.defective_edges.append(edge)
-            rds = float(len(defect.all_atoms()))
-            rds = rds / float(len(edge.all_atoms()))
+            rds = float(defect.size())
+            rds = rds / float(edge.size())
             self.rel_defect_sizes.append(rds)
         elif he == 'w':
             self.wrong += 1
 
     def __str__(self):
-        n = self.correct + self.minor + self.wrong
+        n = self.correct + self.defect + self.wrong
         ratio = float(self.correct) / float(n)
         correct_str = '{} ({}/{})'.format(ratio, self.correct, n)
-        ratio = float(self.minor) / float(n)
-        defect_str = '{} ({}/{})'.format(ratio, self.minor, n)
+        ratio = float(self.defect) / float(n)
+        defect_str = '{} ({}/{})'.format(ratio, self.defect, n)
         ratio = float(self.wrong) / float(n)
         wrong_str = '{} ({}/{})'.format(ratio, self.wrong, n)
-        return 'correct: {}; defect: {}; wrong: {}'.format(
+        line1 = 'correct: {}; defect: {}; wrong: {}'.format(
             correct_str, defect_str, wrong_str)
+        if len(self.defects) > 0:
+            mds = statistics.mean([edge.size() for edge in self.defects])
+            mdes = statistics.mean(
+                [edge.size() for edge in self.defective_edges])
+            mrds = statistics.mean(self.rel_defect_sizes)
+        else:
+            mds = 0.
+            mdes = 0.
+            mrds = 0.
+        line2 = 'mds: {}; mdes: {}; mrds: {}'.format(
+            mds, mdes, mrds)
+        return '{}\n{}'.format(line1, line2)
 
 
 def error_msg(msg):
@@ -89,46 +100,38 @@ def input_defect(sentence, edge):
     return defect
 
 
-def human_test(args):
+def manual_test(args):
     parser = create_parser(name=args.lang)
 
-    he = HumanEvaluation()
-    sources_he = defaultdict(HumanEvaluation)
+    he = ManualEvaluation()
 
     with open(args.infile, 'r') as f:
         for line in f:
-            case = json.loads(line)
-            if not case['ignore']:
-                sentence = case['sentence']
-                source = case['source'][:-1]
+            sentence = line.strip()
 
-                parser_output = parser.parse(sentence)
-                parsed_sentence = parser_output['parses'][0]
-                edge = parsed_sentence['main_edge']
+            parser_output = parser.parse(sentence)
+            parsed_sentence = parser_output['parses'][0]
+            edge = parsed_sentence['main_edge']
 
-                print('\n{}\n{}\n'.format(sentence, indented(edge)))
+            print('\n{}\n{}\n'.format(sentence, indented(edge)))
 
-                answer = he.input()
-                if answer == 'd':
-                    defect = input_defect(sentence, edge)
-                    defect_str = defect.to_str()
-                else:
-                    defect = None
-                    defect_str = ''
-                he.apply_evaluation(answer, defect)
-                sources_he[source].apply_evaluation(answer, edge, defect)
+            answer = he.input()
+            if answer == 'd':
+                defect = input_defect(sentence, edge)
+                defect_str = defect.to_str()
+            else:
+                defect = None
+                defect_str = ''
+            he.apply_evaluation(answer, edge, defect)
 
-                row_str = '\t'.join(
-                    (sentence, source, edge.to_str(), answer, defect_str))
-                with open(args.outfile, 'a') as of:
-                    of.write('{}\n'.format(row_str))
+            row_str = '\t'.join(
+                (sentence, edge.to_str(), answer, defect_str))
+            with open(args.outfile, 'a') as of:
+                of.write('{}\n'.format(row_str))
 
-                print('GLOBAL:')
-                print(colored(str(he), 'white'))
-                for source in sources_he:
-                    print('{}:'.format(source))
-                    print(str(sources_he[source]))
+            print('GLOBAL:')
+            print(colored(str(he), 'white'))
 
 
 if __name__ == '__main__':
-    wrapper(human_test, text='human test of parser')
+    wrapper(manual_test, text='manual test of parser')
