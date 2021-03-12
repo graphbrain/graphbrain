e94c433a7725312a9b66666b98a55c8789c146b1
Telmo Menezes <telmo@telmomenezes.net>
2021-03-12 11:26:43 +0100

    pattern counter

---
+++ graphbrain/patterns.py
@@ -0,0 +1,107 @@
+from collections import Counter
+from graphbrain import hedge, edge_matches_pattern
+
+
+argrole_order = {
+    'm': -1,
+    's': 0,
+    'p': 1,
+    'a': 2,
+    'c': 3,
+    'o': 4,
+    'i': 5,
+    't': 6,
+    'j': 7,
+    'x': 8,
+    'r': 9,
+    '?': 10
+}
+
+
+def normalize_edge(edge):
+    if edge.is_atom():
+        return edge
+    conn = edge[0]
+    ar = conn.argroles()
+    if ar != '':
+        roles_edges = zip(ar, edge[1:])
+        roles_edges = sorted(roles_edges,
+                             key=lambda role_edge: argrole_order[role_edge[0]])
+        ar = ''.join([role_edge[0] for role_edge in roles_edges])
+        pred = conn.atom()
+        new_pred = hedge('{}/{}.{}'.format(pred.root(), pred.type(), ar))
+        conn = conn.replace_atom(pred, new_pred)
+        edge = hedge([conn] + list(role_edge[1] for role_edge in roles_edges))
+    return hedge([normalize_edge(subedge) for subedge in edge])
+
+
+def edge2pattern(edge, root=False, subtype=False):
+    if root and edge.is_atom():
+        root_str = edge.root()
+    else:
+        root_str = '*'
+    if subtype:
+        et = edge.type()
+    else:
+        et = edge.type()[0]
+    pattern = '{}/{}'.format(root_str, et)
+    ar = edge.argroles()
+    if ar == '':
+        return hedge(pattern)
+    else:
+        return hedge('{}.{}'.format(pattern, ar))
+
+
+class PatternCounter:
+    def __init__(self,
+                 depth=2,
+                 count_subedges=True,
+                 expansions={'*'},
+                 match_roots=set(),
+                 match_subtypes={}):
+        self.patterns = Counter()
+        self.depth = depth
+        self.count_subedges = count_subedges
+        self.expansions = expansions
+        self.match_roots = match_roots
+        self.match_subtypes = match_subtypes
+
+    def _matches_expansions(self, edge):
+        for expansion in self.expansions:
+            if edge_matches_pattern(edge, expansion):
+                return True
+        return False
+
+    def _list2patterns(self, ledge, depth=1):
+        if depth > self.depth:
+            return []
+        first = ledge[0]
+        ft = first.type()[0]
+        subtype = ft in self.match_subtypes
+        hpats = [edge2pattern(first, subtype=subtype)]
+        if first.is_atom() and ft in self.match_roots:
+            hpats.append(edge2pattern(first, root=True, subtype=subtype))
+        if not first.is_atom() and self._matches_expansions(first):
+            hpats += self._list2patterns(list(first), depth + 1)
+        if len(ledge) == 1:
+            patterns = [[hpat] for hpat in hpats]
+        else:
+            patterns = []
+            for pattern in self._list2patterns(ledge[1:], depth=depth):
+                for hpat in hpats:
+                    patterns.append([hpat] + pattern)
+        return patterns
+
+    def _edge2patterns(self, edge):
+        return list(hedge(pattern)
+                    for pattern
+                    in self._list2patterns(list(normalize_edge(edge))))
+
+    def count(self, edge):
+        if not edge.is_atom():
+            if self._matches_expansions(edge):
+                for pattern in self._edge2patterns(edge):
+                    self.patterns[hedge(pattern)] += 1
+                if self.count_subedges:
+                    for subedge in edge:
+                        self.count(subedge)
---
+++ graphbrain/tests/test_patterns.py
@@ -0,0 +1,126 @@
+import unittest
+
+from graphbrain import hedge
+from graphbrain.patterns import PatternCounter
+
+
+class Testpatterns(unittest.TestCase):
+    def test_counter1(self):
+        pc = PatternCounter()
+        pc.count(hedge('((not/M is/P.sc) mary/C (not/M nice/C))'))
+
+        self.assertTrue(pc.patterns[hedge('(*/P.sc */C */C)')] == 1)
+        self.assertTrue(pc.patterns[hedge('((*/M */P.sc) */C */C)')] == 1)
+        self.assertTrue(pc.patterns[hedge('(*/P.sc */C (*/M */C))')] == 1)
+        self.assertTrue(
+            pc.patterns[hedge('((*/M */P.sc) */C (*/M */C))')] == 1)
+        self.assertTrue(pc.patterns[hedge('(*/M */P.sc)')] == 1)
+        self.assertFalse(pc.patterns[hedge('(*/M is/P.sc)')] == 1)
+        self.assertTrue(pc.patterns[hedge('(*/M */C)')] == 1)
+
+    def test_counter2(self):
+        pc = PatternCounter(count_subedges=False)
+        pc.count(hedge('((not/M is/P.sc) mary/C (not/M nice/C))'))
+
+        self.assertTrue(pc.patterns[hedge('(*/P.sc */C */C)')] == 1)
+        self.assertTrue(pc.patterns[hedge('((*/M */P.sc) */C */C)')] == 1)
+        self.assertTrue(pc.patterns[hedge('(*/P.sc */C (*/M */C))')] == 1)
+        self.assertTrue(
+            pc.patterns[hedge('((*/M */P.sc) */C (*/M */C))')] == 1)
+        self.assertFalse(pc.patterns[hedge('(*/M */P.sc)')] == 1)
+        self.assertFalse(pc.patterns[hedge('(*/M */C)')] == 1)
+
+    def test_counter3(self):
+        pc = PatternCounter(match_roots={'P'})
+        pc.count(hedge('((not/M is/P.sc) mary/C (not/M nice/C))'))
+
+        self.assertTrue(pc.patterns[hedge('(*/P.sc */C */C)')] == 1)
+        self.assertTrue(pc.patterns[hedge('((*/M */P.sc) */C */C)')] == 1)
+        self.assertTrue(pc.patterns[hedge('((*/M is/P.sc) */C */C)')] == 1)
+        self.assertTrue(pc.patterns[hedge('(*/P.sc */C (*/M */C))')] == 1)
+        self.assertTrue(
+            pc.patterns[hedge('((*/M */P.sc) */C (*/M */C))')] == 1)
+        self.assertTrue(pc.patterns[hedge('(*/M */P.sc)')] == 1)
+        self.assertTrue(pc.patterns[hedge('(*/M is/P.sc)')] == 1)
+        self.assertTrue(pc.patterns[hedge('(*/M */C)')] == 1)
+
+    def test_counter4(self):
+        pc = PatternCounter(count_subedges=False)
+        pc.count(hedge('((not/Mn is/Pd.sc) mary/Cp.s (very/M nice/Cc.s))'))
+
+        self.assertTrue(pc.patterns[hedge('(*/P.sc */C */C)')] == 1)
+        self.assertTrue(pc.patterns[hedge('((*/M */P.sc) */C */C)')] == 1)
+        self.assertTrue(pc.patterns[hedge('(*/P.sc */C (*/M */C))')] == 1)
+        self.assertTrue(
+            pc.patterns[hedge('((*/M */P.sc) */C (*/M */C))')] == 1)
+        self.assertFalse(pc.patterns[hedge('(*/M */P.sc)')] == 1)
+        self.assertFalse(pc.patterns[hedge('(*/M */C)')] == 1)
+
+    def test_counter5(self):
+        pc = PatternCounter(count_subedges=False, match_subtypes={'M'})
+        pc.count(hedge('((not/Mn is/Pd.sc) mary/Cp.s (very/M nice/Cc.s))'))
+
+        self.assertTrue(pc.patterns[hedge('(*/P.sc */C */C)')] == 1)
+        self.assertTrue(pc.patterns[hedge('((*/Mn */P.sc) */C */C)')] == 1)
+        self.assertTrue(pc.patterns[hedge('(*/P.sc */C (*/M */C))')] == 1)
+        self.assertTrue(
+            pc.patterns[hedge('((*/Mn */P.sc) */C (*/M */C))')] == 1)
+        self.assertFalse(pc.patterns[hedge('(*/Mn */P.sc)')] == 1)
+        self.assertFalse(pc.patterns[hedge('(*/M */C)')] == 1)
+
+    def test_counter6(self):
+        pc = PatternCounter(count_subedges=False,
+                            match_subtypes={'C', 'M', 'P'})
+        pc.count(hedge('((not/Mn is/Pd.sc) mary/Cp.s (very/M nice/Cc.s))'))
+
+        self.assertTrue(pc.patterns[hedge('(*/Pd.sc */Cp */Cc)')] == 1)
+        self.assertTrue(pc.patterns[hedge('((*/Mn */Pd.sc) */Cp */Cc)')] == 1)
+        self.assertTrue(pc.patterns[hedge('(*/Pd.sc */Cp (*/M */Cc))')] == 1)
+        self.assertTrue(
+            pc.patterns[hedge('((*/Mn */Pd.sc) */Cp (*/M */Cc))')] == 1)
+        self.assertFalse(pc.patterns[hedge('(*/Mn */P.sc)')] == 1)
+        self.assertFalse(pc.patterns[hedge('(*/M */Cc)')] == 1)
+
+    def test_counter7(self):
+        pc = PatternCounter()
+        pc.count(hedge('(a/M (b/M (c/M (d/M e/C))))'))
+
+        self.assertTrue(pc.patterns[hedge('(*/M */C)')] == 4)
+        self.assertTrue(pc.patterns[hedge('(*/M (*/M */C))')] == 3)
+        self.assertFalse(pc.patterns[hedge('(*/M (*/M (*/M */C)))')] == 2)
+        self.assertFalse(
+            pc.patterns[hedge('(*/M (*/M (*/M (*/M */C)))(')] == 1)
+
+    def test_counter8(self):
+        pc = PatternCounter(depth=3)
+        pc.count(hedge('(a/M (b/M (c/M (d/M e/C))))'))
+
+        self.assertTrue(pc.patterns[hedge('(*/M */C)')] == 4)
+        self.assertTrue(pc.patterns[hedge('(*/M (*/M */C))')] == 3)
+        self.assertTrue(pc.patterns[hedge('(*/M (*/M (*/M */C)))')] == 2)
+        self.assertFalse(
+            pc.patterns[hedge('(*/M (*/M (*/M (*/M */C)))(')] == 1)
+
+    def test_counter9(self):
+        pc = PatternCounter(depth=4)
+        pc.count(hedge('(a/M (b/M (c/M (d/M e/C))))'))
+
+        self.assertTrue(pc.patterns[hedge('(*/M */C)')] == 4)
+        self.assertTrue(pc.patterns[hedge('(*/M (*/M */C))')] == 3)
+        self.assertTrue(pc.patterns[hedge('(*/M (*/M (*/M */C)))')] == 2)
+        self.assertTrue(
+            pc.patterns[hedge('(*/M (*/M (*/M (*/M */C)))(')] == 1)
+
+    def test_counter10(self):
+        pc = PatternCounter(depth=1)
+        pc.count(hedge('(a/M (b/M (c/M (d/M e/C))))'))
+
+        self.assertTrue(pc.patterns[hedge('(*/M */C)')] == 4)
+        self.assertFalse(pc.patterns[hedge('(*/M (*/M */C))')] == 3)
+        self.assertFalse(pc.patterns[hedge('(*/M (*/M (*/M */C)))')] == 2)
+        self.assertFalse(
+            pc.patterns[hedge('(*/M (*/M (*/M (*/M */C)))(')] == 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
