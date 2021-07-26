49de6c0914265bbf6a5cafbc6ef4327608e6abc9
Telmo Menezes <telmo@telmomenezes.net>
2021-07-26 14:48:59 +0200

    automatic deploy

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.4-dev',
+    VERSION: '0.5.0-dev',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
