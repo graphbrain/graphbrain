bb6b69d619c085ebfa5e2283bd78ef56e0dec45b
Telmo Menezes <telmo@telmomenezes.net>
2022-02-23 19:00:26 +0100

    moved conunctions to utils

--- graphbrain/utils/conjunctions.py
+++ graphbrain/utils/conjunctions.py
@@ -1,8 +1,4 @@
-import logging
-
 from graphbrain import hedge
-from graphbrain.cognition.agent import Agent
-from graphbrain.op import create_op
 
 
 # TODO: make concepts decomposition configurable
@@ -67,14 +63,3 @@ def conjunctions_decomposition(edge, concepts=False):
 
     # no decomposition neeeded
     return [edge]
-
-
-class Conjunctions(Agent):
-    def __init__(self, name, progress_bar=True, logging_level=logging.INFO):
-        super().__init__(
-            name, progress_bar=progress_bar, logging_level=logging_level)
-        self.recursive = False
-
-    def process_edge(self, edge, depth):
-        for redge in conjunctions_decomposition(edge):
-            yield create_op(redge)
