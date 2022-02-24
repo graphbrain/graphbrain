dfbed0f79a7b2aa5434578cabb5df90c7e55e93b
Telmo Menezes <telmo@telmomenezes.net>
2022-02-24 09:59:45 +0100

    moving all actors functions to examples

--- examples/actors.py
+++ examples/actors.py
@@ -7,6 +7,30 @@ from graphbrain.utils.concepts import strip_concept
 from graphbrain.utils.lemmas import deep_lemma
 
 
+def is_actor(hg, edge):
+    """Checks if the edge is a coreference to an actor."""
+    if edge.type()[0] == 'C':
+        return hg.exists(('actor/P/.', main_coref(hg, edge)))
+    else:
+        return False
+
+
+def find_actors(hg, edge):
+    """Returns set of all coreferences to actors found in the edge."""
+    actors = set()
+    if is_actor(hg, edge):
+        actors.add(main_coref(hg, edge))
+    if not edge.is_atom():
+        for item in edge:
+            actors |= find_actors(hg, item)
+    return actors
+
+
+def actors(hg):
+    """"Returns an iterator over all actors."""
+    return [edge[1] for edge in hg.search('(actor/P/. *)')]
+
+
 ACTOR_PRED_LEMMAS = {'say', 'claim', 'warn', 'kill', 'accuse', 'condemn',
                      'slam', 'arrest', 'clash', 'blame', 'want', 'call',
                      'tell'}
