c94dae19061181d84a577efe8d50f9a6b4c4074f
Telmo Menezes <telmo@telmomenezes.net>
2022-02-24 09:56:12 +0100

    Processor class

---
+++ graphbrain/processor.py
@@ -0,0 +1,23 @@
+class Processor:
+    def __init__(self, hg, sequence=None):
+        self.hg = hg
+        self.sequence = sequence
+
+    def process_edge(self, edge):
+        pass
+
+    def on_end(self, edge):
+        pass
+
+    def report(self, edge):
+        return ''
+
+    def run(self):
+        if self.sequence is None:
+            for edge in self.hg.all():
+                self.process_edge(edge)
+        else:
+            for edge in self.hg.sequence(self.sequence):
+                self.process_edge(edge)
+        self.on_end()
+        print(self.report())
